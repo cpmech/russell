5e9bb8f13232610f218736555f6ce48bfb2aaf26
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-07-14 13:56:17 +1000

    Add constants

--- russell_tensor/src/lib.rs
+++ russell_tensor/src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Russell - Rust Scientific Library
+//!
+//! **tensor**: tensor analysis tools
+
+// errors ///////////////////////////////////////
+// #[macro_use]
+// extern crate error_chain;
+// mod err {
+//     error_chain! {}
+// }
+
+// modules //////////////////////////////////////
+mod constants;
+pub use crate::constants::*;

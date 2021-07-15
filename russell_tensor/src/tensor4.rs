4f33b96a7bef2ab4d57d701fb0c620cc17eb93fb
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-07-15 23:04:46 +1000

    [tensors] implement 4th order tensor symm

--- russell_tensor/src/tensor4.rs
+++ russell_tensor/src/tensor4.rs
@@ -19,41 +19,75 @@ impl Tensor4 {
     pub fn from_tensor(dd: &[[[[f64; 3]; 3]; 3]; 3], minor_symmetric: bool) -> Self {
         let size = if minor_symmetric { 6 } else { 9 };
         let mut dd_bar = vec![0.0; size * size];
-        for i in 0..3 {
-            for j in 0..3 {
-                for k in 0..3 {
-                    for l in 0..3 {
-                        let a = IJKL_TO_I[i][j][k][l];
-                        let b = IJKL_TO_J[i][j][k][l];
-                        let p = a + b * size; // col-major
-                        if i == j && k == l {
-                            dd_bar[p] = dd[i][j][k][l];
+        if minor_symmetric {
+            for i in 0..3 {
+                for j in 0..3 {
+                    for k in 0..3 {
+                        for l in 0..3 {
+                            // check minor-symmetry
+                            if i > j || k > l {
+                                if dd[i][j][k][l] != dd[j][i][k][l]
+                                    || dd[i][j][k][l] != dd[i][j][l][k]
+                                    || dd[i][j][k][l] != dd[j][i][l][k]
+                                {
+                                    panic!("the components of minor-symmetric tensor do not pass symmetry check");
+                                }
+                            } else {
+                                let a = IJKL_TO_I[i][j][k][l];
+                                let b = IJKL_TO_J[i][j][k][l];
+                                let p = a + b * size; // col-major
+                                if i == j && k == l {
+                                    dd_bar[p] = dd[i][j][k][l];
+                                }
+                                if i == j && k < l {
+                                    dd_bar[p] = SQRT_2 * dd[i][j][k][l];
+                                }
+                                if i < j && k == l {
+                                    dd_bar[p] = SQRT_2 * dd[i][j][k][l];
+                                }
+                                if i < j && k < l {
+                                    dd_bar[p] = 2.0 * dd[i][j][k][l];
+                                }
+                            }
                         }
-                        if i == j && k < l {
-                            dd_bar[p] = (dd[i][j][k][l] + dd[i][j][l][k]) / SQRT_2;
-                        }
-                        if i == j && k > l {
-                            dd_bar[p] = (dd[i][j][l][k] - dd[i][j][k][l]) / SQRT_2;
-                        }
-
-                        if i < j && k == l {
-                            dd_bar[p] = (dd[i][j][k][l] + dd[j][i][k][l]) / SQRT_2;
-                        }
-                        if i < j && k < l {
-                            dd_bar[p] = (dd[i][j][k][l] + dd[i][j][l][k] + dd[j][i][k][l] + dd[j][i][l][k]) / 2.0;
-                        }
-                        if i < j && k > l {
-                            dd_bar[p] = (dd[i][j][l][k] - dd[i][j][k][l] + dd[j][i][l][k] - dd[j][i][k][l]) / 2.0;
-                        }
-
-                        if i > j && k == l {
-                            dd_bar[p] = (dd[j][i][k][l] - dd[i][j][k][l]) / SQRT_2;
-                        }
-                        if i > j && k < l {
-                            dd_bar[p] = (dd[j][i][k][l] + dd[j][i][l][k] - dd[i][j][k][l] - dd[i][j][l][k]) / 2.0;
-                        }
-                        if i > j && k > l {
-                            dd_bar[p] = (dd[j][i][l][k] - dd[j][i][k][l] - dd[i][j][l][k] + dd[i][j][k][l]) / 2.0;
+                    }
+                }
+            }
+        } else {
+            for i in 0..3 {
+                for j in 0..3 {
+                    for k in 0..3 {
+                        for l in 0..3 {
+                            let a = IJKL_TO_I[i][j][k][l];
+                            let b = IJKL_TO_J[i][j][k][l];
+                            let p = a + b * size; // col-major
+                            if i == j && k == l {
+                                dd_bar[p] = dd[i][j][k][l];
+                            }
+                            if i == j && k < l {
+                                dd_bar[p] = (dd[i][j][k][l] + dd[i][j][l][k]) / SQRT_2;
+                            }
+                            if i == j && k > l {
+                                dd_bar[p] = (dd[i][j][l][k] - dd[i][j][k][l]) / SQRT_2;
+                            }
+                            if i < j && k == l {
+                                dd_bar[p] = (dd[i][j][k][l] + dd[j][i][k][l]) / SQRT_2;
+                            }
+                            if i < j && k < l {
+                                dd_bar[p] = (dd[i][j][k][l] + dd[i][j][l][k] + dd[j][i][k][l] + dd[j][i][l][k]) / 2.0;
+                            }
+                            if i < j && k > l {
+                                dd_bar[p] = (dd[i][j][l][k] - dd[i][j][k][l] + dd[j][i][l][k] - dd[j][i][k][l]) / 2.0;
+                            }
+                            if i > j && k == l {
+                                dd_bar[p] = (dd[j][i][k][l] - dd[i][j][k][l]) / SQRT_2;
+                            }
+                            if i > j && k < l {
+                                dd_bar[p] = (dd[j][i][k][l] + dd[j][i][l][k] - dd[i][j][k][l] - dd[i][j][l][k]) / 2.0;
+                            }
+                            if i > j && k > l {
+                                dd_bar[p] = (dd[j][i][l][k] - dd[j][i][k][l] - dd[i][j][l][k] + dd[i][j][k][l]) / 2.0;
+                            }
                         }
                     }
                 }
@@ -87,6 +121,17 @@ mod tests {
             for b in 0..9 {
                 let p = a + b * 9; // col-major
                 assert_eq!(t4.comps_mandel[p], Samples::TENSOR4_SAMPLE1_MANDEL_MATRIX[a][b]);
+            }
+        }
+    }
+
+    #[test]
+    fn from_tensor_sym_works() {
+        let t4 = Tensor4::from_tensor(&Samples::TENSOR4_SYM_SAMPLE1, true);
+        for a in 0..6 {
+            for b in 0..6 {
+                let p = a + b * 6; // col-major
+                assert_eq!(t4.comps_mandel[p], Samples::TENSOR4_SYM_SAMPLE1_MANDEL_MATRIX[a][b]);
             }
         }
     }

7713d15416a431edb417ff34330bab3e9e623e61
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-07-17 11:49:37 +1000

    [tensor] convert format_matrix to take vec of vec

--- russell_tensor/src/formatter.rs
+++ russell_tensor/src/formatter.rs
@@ -2,12 +2,12 @@ use super::*;
 use std::cmp;
 use std::fmt::Write;
 
-pub fn format_matrix(mat: &[f64], nrow: usize, ncol: usize) -> Result<String, Error> {
+pub fn format_matrix(mat: &Vec<Vec<f64>>, nrow: usize, ncol: usize) -> Result<String, Error> {
     let mut width = 0;
     let mut buf = String::new();
     for i in 0..nrow {
         for j in 0..ncol {
-            write!(&mut buf, "{}", mat[i + j * ncol])?;
+            write!(&mut buf, "{}", mat[i][j])?;
             width = cmp::max(buf.chars().count(), width);
             buf.clear();
         }
@@ -22,7 +22,7 @@ pub fn format_matrix(mat: &[f64], nrow: usize, ncol: usize) -> Result<String, Er
             if j == 0 {
                 write!(buf, "│")?;
             }
-            write!(buf, "{:>1$}", mat[i + j * ncol], width)?;
+            write!(buf, "{:>1$}", mat[i][j], width)?;
         }
     }
     write!(buf, " │\n")?;
@@ -38,8 +38,8 @@ mod tests {
 
     #[test]
     fn format_matrix_works() -> Result<(), Error> {
-        let mat = &[11.1, 44.4, 77.7, 22.2, 55.5, 88.8, 33.3, 66.6, 99.9];
-        let buf = format_matrix(mat, 3, 3)?;
+        let mat = vec![vec![11.1, 22.2, 33.3], vec![44.4, 55.5, 66.6], vec![77.7, 88.8, 99.9]];
+        let buf = format_matrix(&mat, 3, 3)?;
         println!("{}", buf);
         let correct = "┌                ┐\n\
                             │ 11.1 22.2 33.3 │\n\

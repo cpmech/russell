6c63b5c98dc6f0d3a059429e52fe4d3e0c396b89
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-16 21:01:43 +1000

    Remove error-chain

--- russell_lab/src/lib.rs
+++ russell_lab/src/lib.rs
@@ -2,13 +2,6 @@
 //!
 //! **lab**: matrix-vector "laboratory" tools
 
-// errors ///////////////////////////////////////
-#[macro_use]
-extern crate error_chain;
-mod err {
-    error_chain! {}
-}
-
 // modules //////////////////////////////////////
 mod linspace;
 pub use crate::linspace::*;

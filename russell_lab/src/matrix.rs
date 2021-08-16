a251deebda253b70898ce3ac6f64265e16edb656
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-16 21:55:38 +1000

    [lab] handle input matrix with wrong cols

--- russell_lab/src/matrix.rs
+++ russell_lab/src/matrix.rs
@@ -17,12 +17,19 @@ pub struct Matrix {
 ///   a[i+j*m] = A[i][j]
 ///
 impl Matrix {
+    /// Creates new (nrow x ncol) Matrix filled with zeros
     pub fn new(nrow: usize, ncol: usize) -> Self {
         Matrix {
             data: vec![0.0; nrow * ncol],
         }
     }
 
+    /// Creates new matrix from given data
+    ///
+    /// # Panics
+    ///
+    /// This function panics if there are rows with different number of columns
+    ///
     pub fn from(data: &[&[f64]]) -> Self {
         let nrow = data.len();
         if nrow == 0 {
@@ -33,6 +40,9 @@ impl Matrix {
             data: vec![0.0; nrow * ncol],
         };
         for i in 0..nrow {
+            if data[i].len() != ncol {
+                panic!("all rows must have the same number of columns");
+            }
             for j in 0..ncol {
                 matrix.data[i + j * nrow] = data[i][j];
             }
@@ -49,7 +59,7 @@ mod tests {
     use russell_chk::*;
 
     #[test]
-    fn new_matrix_works() {
+    fn new_works() {
         let u = Matrix::new(3, 3);
         let correct = &[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
         assert_vec_approx_eq!(u.data, correct, 1e-15);
@@ -65,5 +75,16 @@ mod tests {
         ]);
         let correct = &[1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 8.0];
         assert_vec_approx_eq!(aa.data, correct, 1e-15);
+    }
+
+    #[test]
+    #[should_panic(expected = "all rows must have the same number of columns")]
+    fn from_panics_on_wrong_columns() {
+        #[rustfmt::skip]
+         Matrix::from(&[
+            &[1.0, 2.0, 3.0],
+            &[4.0, 5.0],
+            &[7.0, 8.0, 8.0],
+        ]);
     }
 }

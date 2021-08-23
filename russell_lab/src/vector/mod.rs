299c09279b8dfaffb13b9fc252f570ceaeab1fca
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-23 12:27:15 +1000

    [lab] Make scale_vector a member of Vector

--- russell_lab/src/vector/mod.rs
+++ russell_lab/src/vector/mod.rs
@@ -1,10 +1,8 @@
 mod add_vectors;
 mod add_vectors_simd;
 mod inner;
-mod scale_vector;
 mod vector;
 pub use crate::vector::add_vectors::*;
 use crate::vector::add_vectors_simd::*;
 pub use crate::vector::inner::*;
-pub use crate::vector::scale_vector::*;
 pub use crate::vector::vector::*;

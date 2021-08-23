87b45d910a51dbdda05f27d4245eab51556f9fe7
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-23 23:07:05 +1000

    [lab] Implement copy vector

--- russell_lab/src/vector/copy_vector.rs
+++ russell_lab/src/vector/copy_vector.rs
@@ -2,33 +2,34 @@ use super::*;
 use russell_openblas::*;
 use std::convert::TryInto;
 
-/// Updates vector based on another vector (axpy)
+/// Copy vector into another vector
 ///
 /// ```text
-/// v += alpha * u
+/// v := u
 /// ```
 ///
 /// # Examples
 ///
 /// ```
 /// use russell_lab::*;
-/// let u = Vector::from(&[10.0, 20.0, 30.0]);
-/// let mut v = Vector::from(&[10.0, 20.0, 30.0]);
-/// update_vector(&mut v, 0.1, &u);
-/// let correct = "┌    ┐\n\
-///                │ 11 │\n\
-///                │ 22 │\n\
-///                │ 33 │\n\
-///                └    ┘";
+/// let u = Vector::from(&[1.0, 2.0, 3.0]);
+/// let mut v = Vector::from(&[-1.0, -2.0, -3.0]);
+/// copy_vector(&mut v, &u);
+/// let correct = "┌   ┐\n\
+///                │ 1 │\n\
+///                │ 2 │\n\
+///                │ 3 │\n\
+///                └   ┘";
 /// assert_eq!(format!("{}", v), correct);
 /// ```
-pub fn update_vector(v: &mut Vector, alpha: f64, u: &Vector) {
+/// v := u
+pub fn copy_vector(v: &mut Vector, u: &Vector) {
     let n = v.data.len();
     if u.data.len() != n {
         panic!("the vectors must have the same dimension");
     }
     let n_i32: i32 = n.try_into().unwrap();
-    daxpy(n_i32, alpha, &u.data, 1, &mut v.data, 1);
+    dcopy(n_i32, &u.data, 1, &mut v.data, 1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,11 +40,11 @@ mod tests {
     use russell_chk::*;
 
     #[test]
-    fn update_vectors_works() {
-        let u = Vector::from(&[10.0, 20.0, 30.0]);
+    fn copy_vectors_works() {
+        let u = Vector::from(&[1.0, 2.0, 3.0]);
         let mut v = Vector::from(&[100.0, 200.0, 300.0]);
-        update_vector(&mut v, 2.0, &u);
-        let correct = &[120.0, 240.0, 360.0];
+        copy_vector(&mut v, &u);
+        let correct = &[1.0, 2.0, 3.0];
         assert_vec_approx_eq!(v.data, correct, 1e-15);
     }
 }

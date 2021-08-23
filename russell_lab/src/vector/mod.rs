df0db4850a01e7aedd8cc724f49b03a6842cf23f
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-23 23:02:12 +1000

    [lab] Implement update_vector

--- russell_lab/src/vector/mod.rs
+++ russell_lab/src/vector/mod.rs
@@ -1,6 +1,8 @@
 mod add_vectors;
 mod inner;
+mod update_vector;
 mod vector;
 pub use crate::vector::add_vectors::*;
 pub use crate::vector::inner::*;
+pub use crate::vector::update_vector::*;
 pub use crate::vector::vector::*;

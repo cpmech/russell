7f5e03a4eac6c7b09323bb9f8d883ddba2d2f1d8
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-24 17:07:35 +1000

    Add tests

--- russell_lab/src/matrix/add_matrices.rs
+++ russell_lab/src/matrix/add_matrices.rs
@@ -33,19 +33,19 @@ const NATIVE_VERSUS_OPENBLAS_BOUNDARY: usize = 16;
 pub fn add_matrices(c: &mut Matrix, alpha: f64, a: &Matrix, beta: f64, b: &Matrix) {
     if a.nrow != c.nrow {
         #[rustfmt::skip]
-        panic!("the nrow of matrix [a] (={}) must equal the nrow of matrix [c] (={})", a.nrow, c.nrow);
+        panic!("nrow of matrix [a] (={}) must equal nrow of matrix [c] (={})", a.nrow, c.nrow);
     }
     if a.ncol != c.ncol {
         #[rustfmt::skip]
-        panic!("the ncol of matrix [a] (={}) must equal the ncol of matrix [c] (={})", a.ncol, c.ncol);
+        panic!("ncol of matrix [a] (={}) must equal ncol of matrix [c] (={})", a.ncol, c.ncol);
     }
     if b.nrow != c.nrow {
         #[rustfmt::skip]
-        panic!("the nrow of matrix [b] (={}) must equal the nrow of matrix [c] (={})", b.nrow, c.nrow);
+        panic!("nrow of matrix [b] (={}) must equal nrow of matrix [c] (={})", b.nrow, c.nrow);
     }
     if b.ncol != c.ncol {
         #[rustfmt::skip]
-        panic!("the ncol of matrix [b] (={}) must equal the ncol of matrix [c] (={})", b.ncol, c.ncol);
+        panic!("ncol of matrix [b] (={}) must equal ncol of matrix [c] (={})", b.ncol, c.ncol);
     }
     if c.nrow == 0 && c.ncol == 0 {
         return;
@@ -89,6 +89,52 @@ mod tests {
             &[-1.0, -2.0, -3.0, -4.0],
             &[-1.0, -2.0, -3.0, -4.0],
         ]);
+        assert_vec_approx_eq!(c.data, correct, 1e-15);
+    }
+
+    #[test]
+    #[should_panic(expected = "nrow of matrix [a] (=4) must equal nrow of matrix [c] (=3)")]
+    fn add_matrices_panic_1() {
+        let a = Matrix::new(4, 4);
+        let b = Matrix::new(3, 4);
+        let mut c = Matrix::new(3, 4);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+    }
+
+    #[test]
+    #[should_panic(expected = "ncol of matrix [a] (=3) must equal ncol of matrix [c] (=4)")]
+    fn add_matrices_panic_2() {
+        let a = Matrix::new(3, 3);
+        let b = Matrix::new(3, 4);
+        let mut c = Matrix::new(3, 4);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+    }
+
+    #[test]
+    #[should_panic(expected = "nrow of matrix [b] (=4) must equal nrow of matrix [c] (=3)")]
+    fn add_matrices_panic_3() {
+        let a = Matrix::new(3, 4);
+        let b = Matrix::new(4, 4);
+        let mut c = Matrix::new(3, 4);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+    }
+
+    #[test]
+    #[should_panic(expected = "ncol of matrix [b] (=3) must equal ncol of matrix [c] (=4)")]
+    fn add_matrices_panic_4() {
+        let a = Matrix::new(3, 4);
+        let b = Matrix::new(3, 3);
+        let mut c = Matrix::new(3, 4);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+    }
+
+    #[test]
+    fn add_matrices_skip() {
+        let a = Matrix::new(0, 0);
+        let b = Matrix::new(0, 0);
+        let mut c = Matrix::new(0, 0);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+        let correct: &[f64] = &[];
         assert_vec_approx_eq!(c.data, correct, 1e-15);
     }
 }

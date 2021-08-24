63702d2e5b0e3791a6736f54717a6a3cc085ee06
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-24 17:27:41 +1000

    Remove use of pycobertura

--- zscripts/coverage-tarpaulin.bash
+++ zscripts/coverage-tarpaulin.bash
@@ -6,7 +6,6 @@ if [[ $CI != "true" ]]; then
     echo "Install:"
     echo
     echo "cargo install cargo-tarpaulin"
-    echo "pip3 install pycobertura"
     echo
 fi
 
@@ -16,15 +15,5 @@ cargo +nightly tarpaulin \
     --all \
     --out Html \
     --out Xml
-
-if [[ $CI != "true" ]]; then
-    pycobertura show \
-        --format=html \
-        --output cobertura.html \
-        cobertura.xml
-
-    browse tarpaulin-report.html
-    browse cobertura.html
-fi
 
 cd ..

4ece566b95ec1c075c027795cc536c68d4d62a09
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-26 15:44:57 +1000

    [lab] Use Result in copy_matrix

--- russell_lab/src/matrix/copy_matrix.rs
+++ russell_lab/src/matrix/copy_matrix.rs
@@ -11,6 +11,7 @@ use std::convert::TryInto;
 /// # Examples
 ///
 /// ```
+/// # fn main() -> Result<(), &'static str> {
 /// use russell_lab::*;
 /// let a = Matrix::from(&[
 ///     &[1.0, 2.0, 3.0],
@@ -20,24 +21,22 @@ use std::convert::TryInto;
 ///     &[-1.0, -2.0, -3.0],
 ///     &[-4.0, -5.0, -6.0],
 /// ]);
-/// copy_matrix(&mut b, &a);
+/// copy_matrix(&mut b, &a)?;
 /// let correct = "┌       ┐\n\
 ///                │ 1 2 3 │\n\
 ///                │ 4 5 6 │\n\
 ///                └       ┘";
 /// assert_eq!(format!("{}", b), correct);
+/// # Ok(())
+/// # }
 /// ```
-pub fn copy_matrix(b: &mut Matrix, a: &Matrix) {
-    if a.nrow != b.nrow {
-        #[rustfmt::skip]
-        panic!("nrow of matrix [a] (={}) must equal nrow of matrix [b] (={})", a.nrow, b.nrow);
-    }
-    if a.ncol != b.ncol {
-        #[rustfmt::skip]
-        panic!("ncol of matrix [a] (={}) must equal ncol of matrix [b] (={})", a.ncol, b.ncol);
+pub fn copy_matrix(b: &mut Matrix, a: &Matrix) -> Result<(), &'static str> {
+    if a.nrow != b.nrow || a.ncol != b.ncol {
+        return Err("[a] and [b] matrices must have the same dimensions");
     }
     let n_i32: i32 = b.data.len().try_into().unwrap();
     dcopy(n_i32, &a.data, 1, &mut b.data, 1);
+    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -59,7 +58,7 @@ mod tests {
             &[100.0, 200.0, 300.0],
             &[400.0, 500.0, 600.0],
         ]);
-        copy_matrix(&mut b, &a);
+        copy_matrix(&mut b, &a)?;
         #[rustfmt::skip]
         let correct = slice_to_colmajor(&[
             &[10.0, 20.0, 30.0],
@@ -70,18 +69,28 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "nrow of matrix [a] (=4) must equal nrow of matrix [b] (=3)")]
     fn copy_matrix_panic_1() {
-        let a = Matrix::new(4, 4);
-        let mut b = Matrix::new(3, 4);
-        copy_matrix(&mut b, &a);
-    }
-
-    #[test]
-    #[should_panic(expected = "ncol of matrix [a] (=3) must equal ncol of matrix [b] (=4)")]
-    fn copy_matrix_panic_2() {
-        let a = Matrix::new(3, 3);
-        let mut b = Matrix::new(3, 4);
-        copy_matrix(&mut b, &a);
+        let a_2x2 = Matrix::new(2, 2);
+        let a_2x1 = Matrix::new(2, 1);
+        let a_1x2 = Matrix::new(1, 2);
+        let mut b_2x2 = Matrix::new(2, 2);
+        let mut b_2x1 = Matrix::new(2, 1);
+        let mut b_1x2 = Matrix::new(1, 2);
+        assert_eq!(
+            copy_matrix(&mut b_2x2, &a_2x1),
+            Err("[a] and [b] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            copy_matrix(&mut b_2x2, &a_1x2),
+            Err("[a] and [b] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            copy_matrix(&mut b_2x1, &a_2x2),
+            Err("[a] and [b] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            copy_matrix(&mut b_1x2, &a_2x2),
+            Err("[a] and [b] matrices must have the same dimensions")
+        );
     }
 }

57f4a8859baba1e3f3520b33cae945b5f485c487
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-26 16:43:09 +1000

    [lab] Remove panic from copy vectors

--- russell_lab/src/vector/copy_vector.rs
+++ russell_lab/src/vector/copy_vector.rs
@@ -11,6 +11,7 @@ use std::convert::TryInto;
 /// # Examples
 ///
 /// ```
+/// # fn main() -> Result<(), &'static str> {
 /// use russell_lab::*;
 /// let u = Vector::from(&[1.0, 2.0, 3.0]);
 /// let mut v = Vector::from(&[-1.0, -2.0, -3.0]);
@@ -21,15 +22,17 @@ use std::convert::TryInto;
 ///                │ 3 │\n\
 ///                └   ┘";
 /// assert_eq!(format!("{}", v), correct);
+/// # Ok(())
+/// # }
 /// ```
-pub fn copy_vector(v: &mut Vector, u: &Vector) {
+pub fn copy_vector(v: &mut Vector, u: &Vector) -> Result<(), &'static str> {
     let n = v.data.len();
     if u.data.len() != n {
-        #[rustfmt::skip]
-        panic!("dim of vector [u] (={}) must equal dim of vector [v] (={})", u.data.len(), n);
+        return Err("vectors have wrong dimensions");
     }
     let n_i32: i32 = n.try_into().unwrap();
     dcopy(n_i32, &u.data, 1, &mut v.data, 1);
+    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -40,19 +43,22 @@ mod tests {
     use russell_chk::*;
 
     #[test]
-    fn copy_vector_works() {
+    fn copy_vector_works() -> Result<(), &'static str> {
         let u = Vector::from(&[1.0, 2.0, 3.0]);
         let mut v = Vector::from(&[100.0, 200.0, 300.0]);
-        copy_vector(&mut v, &u);
+        copy_vector(&mut v, &u)?;
         let correct = &[1.0, 2.0, 3.0];
         assert_vec_approx_eq!(v.data, correct, 1e-15);
+        Ok(())
     }
 
     #[test]
-    #[should_panic(expected = "dim of vector [u] (=4) must equal dim of vector [v] (=3)")]
-    fn copy_vector_panic_1() {
+    fn copy_vector_fails_on_wrong_dimensions() {
         let u = Vector::new(4);
         let mut v = Vector::new(3);
-        copy_vector(&mut v, &u);
+        assert_eq!(
+            copy_vector(&mut v, &u),
+            Err("vectors have wrong dimensions")
+        );
     }
 }

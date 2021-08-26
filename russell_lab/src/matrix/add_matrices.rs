afec9a69fe10f3687c0e07762b996e233777e787
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-26 15:05:14 +1000

    [lab] Implement Result for add matrices

--- russell_lab/src/matrix/add_matrices.rs
+++ russell_lab/src/matrix/add_matrices.rs
@@ -12,6 +12,7 @@ const NATIVE_VERSUS_OPENBLAS_BOUNDARY: usize = 16;
 /// # Examples
 ///
 /// ```
+/// # fn main() -> Result<(), &'static str> {
 /// use russell_lab::*;
 /// let a = Matrix::from(&[
 ///     &[ 10.0,  20.0,  30.0,  40.0],
@@ -22,38 +23,37 @@ const NATIVE_VERSUS_OPENBLAS_BOUNDARY: usize = 16;
 ///     &[-2.0, -1.5, -1.0, -0.5],
 /// ]);
 /// let mut c = Matrix::new(2, 4);
-/// add_matrices(&mut c, 0.1, &a, 2.0, &b);
+/// add_matrices(&mut c, 0.1, &a, 2.0, &b)?;
 /// let correct = "┌             ┐\n\
 ///                │  5  5  5  5 │\n\
 ///                │ -5 -5 -5 -5 │\n\
 ///                └             ┘";
 /// assert_eq!(format!("{}", c), correct);
+/// # Ok(())
+/// # }
 /// ```
-pub fn add_matrices(c: &mut Matrix, alpha: f64, a: &Matrix, beta: f64, b: &Matrix) {
-    if a.nrow != c.nrow {
-        #[rustfmt::skip]
-        panic!("nrow of matrix [a] (={}) must equal nrow of matrix [c] (={})", a.nrow, c.nrow);
+pub fn add_matrices(
+    c: &mut Matrix,
+    alpha: f64,
+    a: &Matrix,
+    beta: f64,
+    b: &Matrix,
+) -> Result<(), &'static str> {
+    if a.nrow != c.nrow || a.ncol != c.ncol {
+        return Err("[a] and [c] matrices must have the same dimensions");
     }
-    if a.ncol != c.ncol {
-        #[rustfmt::skip]
-        panic!("ncol of matrix [a] (={}) must equal ncol of matrix [c] (={})", a.ncol, c.ncol);
-    }
-    if b.nrow != c.nrow {
-        #[rustfmt::skip]
-        panic!("nrow of matrix [b] (={}) must equal nrow of matrix [c] (={})", b.nrow, c.nrow);
-    }
-    if b.ncol != c.ncol {
-        #[rustfmt::skip]
-        panic!("ncol of matrix [b] (={}) must equal ncol of matrix [c] (={})", b.ncol, c.ncol);
+    if b.nrow != c.nrow || b.ncol != c.ncol {
+        return Err("[b] and [c] matrices must have the same dimensions");
     }
     if c.nrow == 0 && c.ncol == 0 {
-        return;
+        return Ok(());
     }
     if c.data.len() > NATIVE_VERSUS_OPENBLAS_BOUNDARY {
         add_vectors_oblas(&mut c.data, alpha, &a.data, beta, &b.data);
     } else {
         add_vectors_native(&mut c.data, alpha, &a.data, beta, &b.data);
     }
+    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,7 +64,7 @@ mod tests {
     use russell_chk::*;
 
     #[test]
-    fn add_matrices_works() {
+    fn add_matrices_works() -> Result<(), &'static str> {
         const NOISE: f64 = 1234.567;
         let a = Matrix::from(&[
             &[1.0, 2.0, 3.0, 4.0],
@@ -81,7 +81,7 @@ mod tests {
             &[NOISE, NOISE, NOISE, NOISE],
             &[NOISE, NOISE, NOISE, NOISE],
         ]);
-        add_matrices(&mut c, 1.0, &a, -4.0, &b);
+        add_matrices(&mut c, 1.0, &a, -4.0, &b)?;
         #[rustfmt::skip]
         let correct =slice_to_colmajor(&[
             &[-1.0, -2.0, -3.0, -4.0],
@@ -89,10 +89,11 @@ mod tests {
             &[-1.0, -2.0, -3.0, -4.0],
         ]);
         assert_vec_approx_eq!(c.data, correct, 1e-15);
+        Ok(())
     }
 
     #[test]
-    fn add_matrix_oblas_works() {
+    fn add_matrix_oblas_works() -> Result<(), &'static str> {
         const NOISE: f64 = 1234.567;
         let a = Matrix::from(&[
             &[1.0, 2.0, 3.0, 4.0, 5.0],
@@ -115,7 +116,7 @@ mod tests {
             &[NOISE, NOISE, NOISE, NOISE, NOISE],
             &[NOISE, NOISE, NOISE, NOISE, NOISE],
         ]);
-        add_matrices(&mut c, 1.0, &a, -4.0, &b);
+        add_matrices(&mut c, 1.0, &a, -4.0, &b)?;
         #[rustfmt::skip]
         let correct =slice_to_colmajor(&[
             &[-1.0, -2.0, -3.0, -4.0, -5.0],
@@ -125,51 +126,44 @@ mod tests {
             &[-1.0, -2.0, -3.0, -4.0, -5.0],
         ]);
         assert_vec_approx_eq!(c.data, correct, 1e-15);
+        Ok(())
     }
 
     #[test]
-    #[should_panic(expected = "nrow of matrix [a] (=4) must equal nrow of matrix [c] (=3)")]
-    fn add_matrices_panic_1() {
-        let a = Matrix::new(4, 4);
-        let b = Matrix::new(3, 4);
-        let mut c = Matrix::new(3, 4);
-        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+    fn add_matrices_handle_wrong_dimensions() {
+        let a_2x2 = Matrix::new(2, 2);
+        let a_2x3 = Matrix::new(2, 3);
+        let a_3x2 = Matrix::new(3, 2);
+        let b_2x2 = Matrix::new(2, 2);
+        let b_2x3 = Matrix::new(2, 3);
+        let b_3x2 = Matrix::new(3, 2);
+        let mut c_2x2 = Matrix::new(2, 2);
+        assert_eq!(
+            add_matrices(&mut c_2x2, 1.0, &a_2x3, 1.0, &b_2x2),
+            Err("[a] and [c] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            add_matrices(&mut c_2x2, 1.0, &a_3x2, 1.0, &b_2x2),
+            Err("[a] and [c] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            add_matrices(&mut c_2x2, 1.0, &a_2x2, 1.0, &b_2x3),
+            Err("[b] and [c] matrices must have the same dimensions")
+        );
+        assert_eq!(
+            add_matrices(&mut c_2x2, 1.0, &a_2x2, 1.0, &b_3x2),
+            Err("[b] and [c] matrices must have the same dimensions")
+        );
     }
 
     #[test]
-    #[should_panic(expected = "ncol of matrix [a] (=3) must equal ncol of matrix [c] (=4)")]
-    fn add_matrices_panic_2() {
-        let a = Matrix::new(3, 3);
-        let b = Matrix::new(3, 4);
-        let mut c = Matrix::new(3, 4);
-        add_matrices(&mut c, 1.0, &a, 1.0, &b);
-    }
-
-    #[test]
-    #[should_panic(expected = "nrow of matrix [b] (=4) must equal nrow of matrix [c] (=3)")]
-    fn add_matrices_panic_3() {
-        let a = Matrix::new(3, 4);
-        let b = Matrix::new(4, 4);
-        let mut c = Matrix::new(3, 4);
-        add_matrices(&mut c, 1.0, &a, 1.0, &b);
-    }
-
-    #[test]
-    #[should_panic(expected = "ncol of matrix [b] (=3) must equal ncol of matrix [c] (=4)")]
-    fn add_matrices_panic_4() {
-        let a = Matrix::new(3, 4);
-        let b = Matrix::new(3, 3);
-        let mut c = Matrix::new(3, 4);
-        add_matrices(&mut c, 1.0, &a, 1.0, &b);
-    }
-
-    #[test]
-    fn add_matrices_skip() {
+    fn add_matrices_skip() -> Result<(), &'static str> {
         let a = Matrix::new(0, 0);
         let b = Matrix::new(0, 0);
         let mut c = Matrix::new(0, 0);
-        add_matrices(&mut c, 1.0, &a, 1.0, &b);
+        add_matrices(&mut c, 1.0, &a, 1.0, &b)?;
         let correct: &[f64] = &[];
         assert_vec_approx_eq!(c.data, correct, 1e-15);
+        Ok(())
     }
 }

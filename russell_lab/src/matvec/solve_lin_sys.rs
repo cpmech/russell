f66d8b2c931d8581d193c93e8f6a6ba3b2f015cb
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-28 17:06:28 +1000

    [lab] Add test

--- russell_lab/src/matvec/solve_lin_sys.rs
+++ russell_lab/src/matvec/solve_lin_sys.rs
@@ -56,6 +56,9 @@ pub fn solve_lin_sys(b: &mut Vector, a: &mut Matrix) -> Result<(), &'static str>
     if m != n {
         return Err("matrix must be square");
     }
+    if b.data.len() != m {
+        return Err("vector has wrong dimension");
+    }
     let mut ipiv = vec![0; m];
     let m_i32 = to_i32(m);
     dgesv(m_i32, 1, &mut a.data, m_i32, &mut ipiv, &mut b.data, m_i32)?;
@@ -74,6 +77,16 @@ mod tests {
         let mut a = Matrix::new(2, 3);
         let mut b = Vector::new(3);
         assert_eq!(solve_lin_sys(&mut b, &mut a), Err("matrix must be square"));
+    }
+
+    #[test]
+    fn solve_lin_sys_fails_on_wrong_dims() {
+        let mut a = Matrix::new(2, 2);
+        let mut b = Vector::new(3);
+        assert_eq!(
+            solve_lin_sys(&mut b, &mut a),
+            Err("vector has wrong dimension")
+        );
     }
 
     #[test]
@@ -104,6 +117,35 @@ mod tests {
               23.0 / 14.0,
         ]);
         assert_vec_approx_eq!(b.data, x_correct.data, 1e-13);
+        Ok(())
+    }
+
+    #[test]
+    fn solve_lin_sys_1_works() -> Result<(), &'static str> {
+        // example from https://numericalalgorithmsgroup.github.io/LAPACK_Examples/examples/doc/dgesv_example.html
+        #[rustfmt::skip]
+        let mut a = Matrix::from(&[
+            &[ 1.80,  2.88,  2.05, -0.89],
+            &[ 5.25, -2.95, -0.95, -3.80],
+            &[ 1.58, -2.69, -2.90, -1.04],
+            &[-1.11, -0.66, -0.59,  0.80],
+        ])?;
+        #[rustfmt::skip]
+        let mut b = Vector::from(&[
+             9.52,
+            24.35,
+             0.77,
+            -6.22,
+        ]);
+        solve_lin_sys(&mut b, &mut a)?;
+        #[rustfmt::skip]
+        let x_correct = Vector::from(&[
+             1.0,
+            -1.0,
+             3.0,
+            -5.0,
+        ]);
+        assert_vec_approx_eq!(b.data, x_correct.data, 1e-14);
         Ok(())
     }
 }

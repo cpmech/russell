025ff0191611980f149b2611443f0e3e1f302f83
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-31 09:08:01 +1000

    [sparse] Improve mem check

--- russell_sparse/src/bin/mem_check_build.rs
+++ russell_sparse/src/bin/mem_check_build.rs
@@ -2,8 +2,22 @@ use russell_sparse::*;
 
 fn main() -> Result<(), &'static str> {
     println!("\nRunning Mem Check\n");
-    let trip = SparseTriplet::new(3, 3, 5)?;
-    let solver = SolverMumps::new(EnumMumpsSymmetry::No, true)?;
+    let mut trip = SparseTriplet::new(5, 5, 13)?;
+    trip.put(0, 0, 1.0)?; // << duplicated
+    trip.put(0, 0, 1.0)?; // << duplicated
+    trip.put(1, 0, 3.0)?;
+    trip.put(0, 1, 3.0)?;
+    trip.put(2, 1, -1.0)?;
+    trip.put(4, 1, 4.0)?;
+    trip.put(1, 2, 4.0)?;
+    trip.put(2, 2, -3.0)?;
+    trip.put(3, 2, 1.0)?;
+    trip.put(4, 2, 2.0)?;
+    trip.put(2, 3, 2.0)?;
+    trip.put(1, 4, 6.0)?;
+    trip.put(4, 4, 1.0)?;
+    let mut solver = SolverMumps::new(EnumMumpsSymmetry::No, true)?;
+    solver.analyze(&trip, true)?;
     println!("{}", trip);
     println!("\n{}", solver);
     println!("\nDone\n");

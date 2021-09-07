83506cdb3aa377e60e847b7f69ac805ee60db619
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-07 11:59:01 +1000

    Improve mem check

--- russell_sparse/src/bin/mem_check_build.rs
+++ russell_sparse/src/bin/mem_check_build.rs
@@ -1,8 +1,11 @@
 use russell_lab::*;
 use russell_sparse::*;
 
-fn test_solver_mmp() {
-    println!("Testing SolverMMP\n");
+fn test_solver(kind: EnumSolverKind) {
+    match kind {
+        EnumSolverKind::Mmp => println!("\nTesting MMP solver\n"),
+        EnumSolverKind::Umf => println!("\nTesting UMF solver\n"),
+    }
 
     let mut trip = match SparseTriplet::new(5, 5, 13, false) {
         Ok(v) => v,
@@ -12,8 +15,8 @@ fn test_solver_mmp() {
         }
     };
 
-    trip.put(0, 0, 1.0); // << duplicated
-    trip.put(0, 0, 1.0); // << duplicated
+    trip.put(0, 0, 1.0); // << (0, 0, a00/2)
+    trip.put(0, 0, 1.0); // << (0, 0, a00/2)
     trip.put(1, 0, 3.0);
     trip.put(0, 1, 3.0);
     trip.put(2, 1, -1.0);
@@ -27,8 +30,8 @@ fn test_solver_mmp() {
     trip.put(4, 4, 1.0);
 
     let mut config = ConfigSolver::new();
-    config.set_solver_kind(EnumSolverKind::Mmp);
-    let mut solver_mmp = match Solver::new(config) {
+    config.set_solver_kind(kind);
+    let mut solver = match Solver::new(config) {
         Ok(v) => v,
         Err(e) => {
             println!("FAIL(new solver): {}", e);
@@ -36,7 +39,7 @@ fn test_solver_mmp() {
         }
     };
 
-    match solver_mmp.initialize(&trip) {
+    match solver.initialize(&trip) {
         Err(e) => {
             println!("FAIL(initialize): {}", e);
             return;
@@ -44,7 +47,7 @@ fn test_solver_mmp() {
         _ => (),
     };
 
-    match solver_mmp.factorize() {
+    match solver.factorize() {
         Err(e) => {
             println!("FAIL(factorize): {}", e);
             return;
@@ -55,7 +58,7 @@ fn test_solver_mmp() {
     let mut x = Vector::new(5);
     let rhs = Vector::from(&[8.0, 45.0, -3.0, 3.0, 19.0]);
 
-    match solver_mmp.solve(&mut x, &rhs) {
+    match solver.solve(&mut x, &rhs) {
         Err(e) => {
             println!("FAIL(solve): {}", e);
             return;
@@ -63,7 +66,7 @@ fn test_solver_mmp() {
         _ => (),
     }
 
-    match solver_mmp.solve(&mut x, &rhs) {
+    match solver.solve(&mut x, &rhs) {
         Err(e) => {
             println!("FAIL(solve again): {}", e);
             return;
@@ -72,7 +75,7 @@ fn test_solver_mmp() {
     }
 
     println!("{}", trip);
-    println!("\n{}", solver_mmp);
+    println!("\n{}", solver);
     println!("\nx =\n{}", x);
 
     let mut trip_singular = match SparseTriplet::new(5, 5, 2, false) {
@@ -85,7 +88,7 @@ fn test_solver_mmp() {
 
     trip_singular.put(0, 0, 1.0);
     trip_singular.put(4, 4, 1.0);
-    match solver_mmp.initialize(&trip_singular) {
+    match solver.initialize(&trip_singular) {
         Err(e) => {
             println!("FAIL(initialize singular matrix): {}", e);
             return;
@@ -93,114 +96,15 @@ fn test_solver_mmp() {
         _ => (),
     };
 
-    match solver_mmp.factorize() {
+    match solver.factorize() {
         Err(e) => println!("\nOk(factorize singular matrix): {}", e),
         _ => (),
     };
-}
-
-fn test_solver_umf() {
-    println!("\nTesting SolverUMF\n");
-
-    let mut trip = match SparseTriplet::new(5, 5, 13, false) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("FAIL(new triplet): {}", e);
-            return;
-        }
-    };
-
-    trip.put(0, 0, 1.0); // << duplicated
-    trip.put(0, 0, 1.0); // << duplicated
-    trip.put(1, 0, 3.0);
-    trip.put(0, 1, 3.0);
-    trip.put(2, 1, -1.0);
-    trip.put(4, 1, 4.0);
-    trip.put(1, 2, 4.0);
-    trip.put(2, 2, -3.0);
-    trip.put(3, 2, 1.0);
-    trip.put(4, 2, 2.0);
-    trip.put(2, 3, 2.0);
-    trip.put(1, 4, 6.0);
-    trip.put(4, 4, 1.0);
-
-    let config = ConfigSolver::new();
-    let mut solver_umf = match Solver::new(config) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("FAIL(new solver): {}", e);
-            return;
-        }
-    };
-
-    match solver_umf.initialize(&trip) {
-        Err(e) => {
-            println!("FAIL(initialize): {}", e);
-            return;
-        }
-        _ => (),
-    };
-
-    match solver_umf.factorize() {
-        Err(e) => {
-            println!("FAIL(factorize): {}", e);
-            return;
-        }
-        _ => (),
-    };
-
-    let mut x = Vector::new(5);
-    let rhs = Vector::from(&[8.0, 45.0, -3.0, 3.0, 19.0]);
-
-    match solver_umf.solve(&mut x, &rhs) {
-        Err(e) => {
-            println!("FAIL(solve): {}", e);
-            return;
-        }
-        _ => (),
-    }
-
-    match solver_umf.solve(&mut x, &rhs) {
-        Err(e) => {
-            println!("FAIL(solve again): {}", e);
-            return;
-        }
-        _ => (),
-    }
-
-    println!("{}", trip);
-    println!("\n{}", solver_umf);
-    println!("\nx =\n{}", x);
-
-    let mut trip_singular = match SparseTriplet::new(5, 5, 2, false) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("FAIL(new singular matrix): {}", e);
-            return;
-        }
-    };
-
-    trip_singular.put(0, 0, 1.0);
-    trip_singular.put(4, 4, 1.0);
-    match solver_umf.initialize(&trip_singular) {
-        Err(e) => {
-            println!("FAIL(initialize singular matrix): {}", e);
-            return;
-        }
-        _ => (),
-    };
-
-    match solver_umf.factorize() {
-        Err(e) => println!("\nOk(factorize singular matrix): {}", e),
-        _ => (),
-    };
-
-    println!("\nDone\n");
 }
 
 fn main() {
     println!("\nRunning Mem Check\n");
-    test_solver_mmp();
-    test_solver_umf();
+    test_solver(EnumSolverKind::Mmp);
+    test_solver(EnumSolverKind::Umf);
     println!("\nDone\n");
 }

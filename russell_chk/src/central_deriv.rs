e8b58061f5e4c7553795918ec3a429ba3160102c
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-10-21 12:16:59 +1000

    Add error estimation to central_deriv

--- russell_chk/src/central_deriv.rs
+++ russell_chk/src/central_deriv.rs
@@ -14,27 +14,38 @@
 ///
 /// # Output
 ///
+/// Returns the triple (dfdx, abs_trunc_err, abs_round_err) where:
+///
 /// * `dfdx` -- numerical derivative of f(x) w.r.t x @ x
+/// * `abs_trunc_err` -- estimated truncation error O(h²)
+/// * `abs_round_err` -- rounding error due to cancellations
 ///
 /// # Notes
 ///
-/// * Thus function computes the derivative using the 5-point rule
+/// * The function computes the derivative using the 5-point rule
 ///   (x-h, x-h/2, x, x+h/2, x+h); but the central point is not used
+/// * The truncation error in the r5 approximation is O(h⁴); however,
+///   for safety, we estimate the error from r5-r3, which is O(h²).
+///   This allows the automatic scaling of h.
 ///
 /// # Example
 ///
 /// ```
 /// use russell_chk::central_deriv;
-/// use std::f64::consts::PI;
-/// let f = |x: f64| f64::sin(PI * x / 2.0);
-/// let g = |x: f64| f64::cos(PI * x / 2.0) * PI / 2.0;
-/// let d = central_deriv(f, 1.0, 1e-3);
-/// assert!(f64::abs(d - g(1.0)) < 1e-12);
+/// let f = |x: f64| f64::exp(-2.0 * x);
+/// let g = |x: f64| -2.0 * f64::exp(-2.0 * x);
+/// let h = 1e-3;
+/// let x = 1.0;
+/// let (d, err, rerr) = central_deriv(f, x, h);
+/// assert!(f64::abs(d - g(x)) < 1e-13);
+/// assert!(err < 1e-6);
+/// assert!(rerr < 1e-11);
 /// ```
-pub fn central_deriv<F>(f: F, x: f64, h: f64) -> f64
+pub fn central_deriv<F>(f: F, x: f64, h: f64) -> (f64, f64, f64)
 where
     F: Fn(f64) -> f64,
 {
+    // numerical derivative
     let fm1 = f(x - h);
     let fp1 = f(x + h);
     let fmh = f(x - h / 2.0);
@@ -42,7 +53,19 @@ where
     let r3 = 0.5 * (fp1 - fm1);
     let r5 = (4.0 / 3.0) * (fph - fmh) - (1.0 / 3.0) * r3;
     let dfdx = r5 / h;
-    dfdx
+
+    // smallest number satisfying 1.0 + EPS > 1.0
+    const EPS: f64 = 1.0e-15;
+
+    // error estimation
+    let e3 = (f64::abs(fp1) + f64::abs(fm1)) * EPS;
+    let e5 = 2.0 * (f64::abs(fph) + f64::abs(fmh)) * EPS + e3;
+    let dy = f64::max(f64::abs(r3 / h), f64::abs(r5 / h)) * (f64::abs(x) / h) * EPS;
+    let abs_trunc_err = f64::abs((r5 - r3) / h);
+    let abs_round_err = f64::abs(e5 / h) + dy;
+
+    // results
+    (dfdx, abs_trunc_err, abs_round_err)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -57,8 +80,10 @@ mod tests {
         let f = |x: f64| f64::cos(PI * x / 2.0);
         let g = |x: f64| -f64::sin(PI * x / 2.0) * PI / 2.0;
         let h = 1e-3;
-        let x1 = 1.0;
-        let d1 = central_deriv(f, x1, h);
-        assert!(f64::abs(d1 - g(x1)) < 1e-12);
+        let x = 1.0;
+        let (d, err, rerr) = central_deriv(f, x, h);
+        assert!(f64::abs(d - g(x)) < 1e-12);
+        assert!(err < 1e-6);
+        assert!(rerr < 1e-11);
     }
 }

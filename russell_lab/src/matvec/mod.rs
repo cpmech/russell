72ab58f04da5e1a0f8fc828457647fafd4753282
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-10-30 12:26:59 +1000

    Impl vec_mat_mul

--- russell_lab/src/matvec/mod.rs
+++ russell_lab/src/matvec/mod.rs
@@ -1,6 +1,8 @@
 mod mat_vec_mul;
 mod outer;
 mod solve_lin_sys;
+mod vec_mat_mul;
 pub use crate::matvec::mat_vec_mul::*;
 pub use crate::matvec::outer::*;
 pub use crate::matvec::solve_lin_sys::*;
+pub use crate::matvec::vec_mat_mul::*;
--- russell_lab/src/matvec/vec_mat_mul.rs
+++ russell_lab/src/matvec/vec_mat_mul.rs
@@ -2,17 +2,24 @@ use crate::matrix::Matrix;
 use crate::vector::Vector;
 use russell_openblas::{dgemv, to_i32};
 
-/// Performs the matrix-vector multiplication resulting in a vector
+/// Performs the vector-matrix multiplication resulting in a vector
 ///
 /// ```text
-///  v  :=  α ⋅  a   ⋅  u
-/// (m)        (m,n)   (n)
+///  v  :=  α ⋅  u  ⋅  a  
+/// (n)         (m)  (m,n)
+/// ```
+///
+/// or
+///
+/// ```text
+///  v  :=  α ⋅   aᵀ  ⋅  u
+/// (n)         (n,m)   (m)  
 /// ```
 ///
 /// # Note
 ///
-/// The length of vector `u` must equal the number of columns of matrix `a` and
-/// the length of vector `v` must equal the number of rows of matrix `a`.
+/// The length of vector `u` must equal the number of rows of matrix `a` and
+/// the length of vector `v` must equal the number of columns of matrix `a`.
 ///
 /// # Example
 ///
@@ -25,22 +32,21 @@ use russell_openblas::{dgemv, to_i32};
 ///     [15.0, -6.0, 0.0],
 ///     [ 3.0,  5.0, 1.0],
 /// ]);
-/// let u = Vector::from(&[1.0, 2.0, 3.0]);
-/// let mut v = Vector::new(a.nrow());
-/// mat_vec_mul(&mut v, 0.5, &a, &u)?;
+/// let u = Vector::from(&[1.0, 2.0, 3.0, 4.0]);
+/// let mut v = Vector::new(a.ncol());
+/// vec_mat_mul(&mut v, 0.5, &u, &a)?;
 /// let correct = "┌     ┐\n\
-///                │   2 │\n\
-///                │   1 │\n\
-///                │ 1.5 │\n\
-///                │   8 │\n\
+///                │  27 │\n\
+///                │   0 │\n\
+///                │ 4.5 │\n\
 ///                └     ┘";
 /// assert_eq!(format!("{}", v), correct);
 /// # Ok(())
 /// # }
 /// ```
-pub fn mat_vec_mul(v: &mut Vector, alpha: f64, a: &Matrix, u: &Vector) -> Result<(), &'static str> {
-    let m = v.dim();
-    let n = u.dim();
+pub fn vec_mat_mul(v: &mut Vector, alpha: f64, u: &Vector, a: &Matrix) -> Result<(), &'static str> {
+    let n = v.dim();
+    let m = u.dim();
     if m != a.nrow() || n != a.ncol() {
         return Err("matrix and vectors are incompatible");
     }
@@ -50,7 +56,7 @@ pub fn mat_vec_mul(v: &mut Vector, alpha: f64, a: &Matrix, u: &Vector) -> Result
     let m_i32: i32 = to_i32(m);
     let n_i32: i32 = to_i32(n);
     dgemv(
-        false,
+        true,
         m_i32,
         n_i32,
         alpha,
@@ -68,43 +74,43 @@ pub fn mat_vec_mul(v: &mut Vector, alpha: f64, a: &Matrix, u: &Vector) -> Result
 
 #[cfg(test)]
 mod tests {
-    use super::{mat_vec_mul, Matrix, Vector};
+    use super::{vec_mat_mul, Matrix, Vector};
     use russell_chk::*;
 
     #[test]
-    fn mat_vec_mul_fails_on_wrong_dims() {
+    fn vec_mat_mul_fails_on_wrong_dims() {
         let u = Vector::new(2);
         let a_1x2 = Matrix::new(1, 2);
         let a_3x1 = Matrix::new(3, 1);
         let mut v = Vector::new(3);
         assert_eq!(
-            mat_vec_mul(&mut v, 1.0, &a_1x2, &u),
+            vec_mat_mul(&mut v, 1.0, &u, &a_1x2),
             Err("matrix and vectors are incompatible")
         );
         assert_eq!(
-            mat_vec_mul(&mut v, 1.0, &a_3x1, &u),
+            vec_mat_mul(&mut v, 1.0, &u, &a_3x1),
             Err("matrix and vectors are incompatible")
         );
     }
 
     #[test]
-    fn mat_vec_mul_works() -> Result<(), &'static str> {
+    fn vec_mat_mul_works() -> Result<(), &'static str> {
         #[rustfmt::skip]
         let a = Matrix::from(&[
             [ 5.0, -2.0, 0.0, 1.0],
             [10.0, -4.0, 0.0, 2.0],
             [15.0, -6.0, 0.0, 3.0],
         ]);
-        let u = Vector::from(&[1.0, 3.0, 8.0, 5.0]);
-        let mut v = Vector::new(a.nrow());
-        mat_vec_mul(&mut v, 1.0, &a, &u)?;
-        let correct = &[4.0, 8.0, 12.0];
+        let u = Vector::from(&[1.0, 3.0, 8.0]);
+        let mut v = Vector::new(a.ncol());
+        vec_mat_mul(&mut v, 1.0, &u, &a)?;
+        let correct = &[155.0, -62.0, 0.0, 31.0];
         assert_vec_approx_eq!(v.as_data(), correct, 1e-15);
         Ok(())
     }
 
     #[test]
-    fn mat_vec_mul_zero_works() -> Result<(), &'static str> {
+    fn vec_mat_mul_zero_works() -> Result<(), &'static str> {
         let a_0x0 = Matrix::new(0, 0);
         let a_0x1 = Matrix::new(0, 1);
         let a_1x0 = Matrix::new(1, 0);
@@ -112,12 +118,12 @@ mod tests {
         let u1 = Vector::new(1);
         let mut v0 = Vector::new(0);
         let mut v1 = Vector::new(1);
-        mat_vec_mul(&mut v0, 1.0, &a_0x0, &u0)?;
+        vec_mat_mul(&mut v0, 1.0, &u0, &a_0x0)?;
         assert_eq!(v0.as_data(), &[]);
-        mat_vec_mul(&mut v0, 1.0, &a_0x1, &u1)?;
-        assert_eq!(v0.as_data(), &[]);
-        mat_vec_mul(&mut v1, 1.0, &a_1x0, &u0)?;
+        vec_mat_mul(&mut v1, 1.0, &u0, &a_0x1)?;
         assert_eq!(v1.as_data(), &[0.0]);
+        vec_mat_mul(&mut v0, 1.0, &u1, &a_1x0)?;
+        assert_eq!(v0.as_data(), &[]);
         Ok(())
     }
 }

c2024ea0c00e757bf1424aa58439d3acdf09cd3e
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-01-28 16:49:50 +1000

    Improve comments

--- russell_lab/src/matrix/aliases.rs
+++ russell_lab/src/matrix/aliases.rs
@@ -1,3 +1,4 @@
 use crate::GenericMatrix;
 
+/// Matrix is an alias to GenericMatrix<f64> and is used in most functions that call OpenBLAS
 pub type Matrix = GenericMatrix<f64>;
--- russell_lab/src/vector/aliases.rs
+++ russell_lab/src/vector/aliases.rs
@@ -1,3 +1,4 @@
 use crate::GenericVector;
 
+/// Vector is an alias to GenericVector<f64> and is used in most functions that call OpenBLAS
 pub type Vector = GenericVector<f64>;

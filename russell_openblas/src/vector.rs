1219ec7086cb7f63537d7b6628725b11fcdfbe57
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-28 09:54:57 +1000

    [openblas] Impl zcopy and zscal

--- russell_openblas/src/vector.rs
+++ russell_openblas/src/vector.rs
@@ -4,7 +4,9 @@ extern "C" {
     // from /usr/include/x86_64-linux-gnu/cblas.h
     fn cblas_ddot(n: i32, x: *const f64, incx: i32, y: *const f64, incy: i32) -> f64;
     fn cblas_dcopy(n: i32, x: *const f64, incx: i32, y: *mut f64, incy: i32);
+    fn cblas_zcopy(n: i32, x: *const Complex64, incx: i32, y: *mut Complex64, incy: i32);
     fn cblas_dscal(n: i32, alpha: f64, x: *const f64, incx: i32);
+    fn cblas_zscal(n: i32, alpha: *const Complex64, x: *const Complex64, incx: i32);
     fn cblas_daxpy(n: i32, alpha: f64, x: *const f64, incx: i32, y: *mut f64, incy: i32);
     fn cblas_zaxpy(n: i32, alpha: *const Complex64, x: *const Complex64, incx: i32, y: *mut Complex64, incy: i32);
     fn cblas_dnrm2(n: i32, x: *const f64, incx: i32) -> f64;
@@ -12,7 +14,7 @@ extern "C" {
     fn cblas_idamax(n: i32, x: *const f64, incx: i32) -> i32;
 }
 
-/// Calculates the dot product of two vectors.
+/// Calculates the dot product of two vectors
 ///
 /// ```text
 /// x dot y
@@ -31,7 +33,7 @@ pub fn ddot(n: i32, x: &[f64], incx: i32, y: &[f64], incy: i32) -> f64 {
     unsafe { cblas_ddot(n, x.as_ptr(), incx, y.as_ptr(), incy) }
 }
 
-/// Copies a vector into another.
+/// Copies a vector into another
 ///
 /// ```text
 /// y := x
@@ -52,7 +54,24 @@ pub fn dcopy(n: i32, x: &[f64], incx: i32, y: &mut [f64], incy: i32) {
     }
 }
 
-/// Scales a vector by a constant.
+/// Copies a vector into another (complex version)
+///
+/// ```text
+/// y := x
+/// ```
+///
+/// # Reference
+///
+/// <https://www.netlib.org/lapack/explore-html/d6/d53/zcopy_8f.html>
+///
+#[inline]
+pub fn zcopy(n: i32, x: &[Complex64], incx: i32, y: &mut [Complex64], incy: i32) {
+    unsafe {
+        cblas_zcopy(n, x.as_ptr(), incx, y.as_mut_ptr(), incy);
+    }
+}
+
+/// Scales a vector by a constant
 ///
 /// ```text
 /// x := alpha * x
@@ -73,6 +92,23 @@ pub fn dscal(n: i32, alpha: f64, x: &mut [f64], incx: i32) {
     }
 }
 
+/// Scales a vector by a constant (complex version)
+///
+/// ```text
+/// x := alpha * x
+/// ```
+///
+/// # Reference
+///
+/// <http://www.netlib.org/lapack/explore-html/d2/d74/zscal_8f.html>
+///
+#[inline]
+pub fn zscal(n: i32, alpha: Complex64, x: &mut [Complex64], incx: i32) {
+    unsafe {
+        cblas_zscal(n, &alpha, x.as_ptr(), incx);
+    }
+}
+
 /// Computes constant times a vector plus a vector.
 ///
 /// ```text
@@ -90,7 +126,7 @@ pub fn daxpy(n: i32, alpha: f64, x: &[f64], incx: i32, y: &mut [f64], incy: i32)
     }
 }
 
-/// Computes constant times a vector plus a vector (Complex version).
+/// Computes constant times a vector plus a vector (Complex version)
 ///
 /// ```text
 /// y := alpha*x + y
@@ -156,7 +192,7 @@ pub fn idamax(n: i32, x: &[f64], incx: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use super::{dasum, daxpy, dcopy, ddot, dnrm2, dscal, idamax, zaxpy};
+    use super::{dasum, daxpy, dcopy, ddot, dnrm2, dscal, idamax, zaxpy, zcopy, zscal};
     use crate::to_i32;
     use num_complex::Complex64;
     use russell_chk::{assert_approx_eq, assert_complex_vec_approx_eq, assert_vec_approx_eq};
@@ -181,6 +217,38 @@ mod tests {
     }
 
     #[test]
+    fn zcopy_works() {
+        const IGNORED: Complex64 = Complex64::new(100000.0, 200000.0);
+        let x = [
+            Complex64::new(20.0, 21.0),
+            Complex64::new(10.0, 11.0),
+            Complex64::new(-30.0, -31.0),
+            IGNORED,
+            IGNORED,
+        ];
+        let mut y = [
+            Complex64::new(200.0, 201.0),
+            Complex64::new(100.0, 101.0),
+            Complex64::new(-300.0, -301.0),
+            IGNORED,
+            IGNORED,
+        ];
+        let (n, incx, incy) = (3, 1, 1);
+        zcopy(n, &x, incx, &mut y, incy);
+        assert_complex_vec_approx_eq!(
+            x,
+            &[
+                Complex64::new(20.0, 21.0),
+                Complex64::new(10.0, 11.0),
+                Complex64::new(-30.0, -31.0),
+                IGNORED,
+                IGNORED
+            ],
+            1e-15
+        );
+    }
+
+    #[test]
     fn dscal_works() {
         const IGNORED: f64 = 100000.0;
         let alpha = 0.5;
@@ -188,6 +256,54 @@ mod tests {
         let (n, incx) = (3, 1);
         dscal(n, alpha, &mut x, incx);
         assert_vec_approx_eq!(x, &[10.0, 5.0, -15.0, IGNORED, IGNORED], 1e-15);
+    }
+
+    #[test]
+    fn zscal_works() {
+        const IGNORED: Complex64 = Complex64::new(100000.0, 200000.0);
+        let alpha = Complex64::new(0.5, 0.0);
+        let mut x = [
+            Complex64::new(20.0, 21.0),
+            Complex64::new(10.0, 11.0),
+            Complex64::new(-30.0, -31.0),
+            IGNORED,
+            IGNORED,
+        ];
+        let (n, incx) = (3, 1);
+        zscal(n, alpha, &mut x, incx);
+        assert_complex_vec_approx_eq!(
+            x,
+            &[
+                Complex64::new(10.0, 10.5),
+                Complex64::new(5.0, 5.5),
+                Complex64::new(-15.0, -15.5),
+                IGNORED,
+                IGNORED
+            ],
+            1e-15
+        );
+
+        let alpha = Complex64::new(0.5, 1.0);
+        let mut x = [
+            Complex64::new(20.0, 21.0),
+            Complex64::new(10.0, 11.0),
+            Complex64::new(-30.0, -31.0),
+            IGNORED,
+            IGNORED,
+        ];
+        let (n, incx) = (3, 1);
+        zscal(n, alpha, &mut x, incx);
+        assert_complex_vec_approx_eq!(
+            x,
+            &[
+                Complex64::new(-11.0, 30.5),
+                Complex64::new(-6.0, 15.5),
+                Complex64::new(16.0, -45.5),
+                IGNORED,
+                IGNORED
+            ],
+            1e-15
+        );
     }
 
     #[test]

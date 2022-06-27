47c3ab4aac6de7b173c330e97f5146ccc44104e2
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-28 09:56:59 +1000

    Impl complex add vectors

--- russell_openblas/src/highlevel/mod.rs
+++ russell_openblas/src/highlevel/mod.rs
@@ -1,4 +1,8 @@
 mod add_vectors_native;
 mod add_vectors_oblas;
+mod complex_add_vectors_native;
+mod complex_add_vectors_oblas;
 pub use crate::highlevel::add_vectors_native::*;
 pub use crate::highlevel::add_vectors_oblas::*;
+pub use crate::highlevel::complex_add_vectors_native::*;
+pub use crate::highlevel::complex_add_vectors_oblas::*;

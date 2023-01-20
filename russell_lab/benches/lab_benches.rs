d8f38ad80de3c43537757de839ad877528577a5c
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-01-20 13:20:42 +1000

    Add benchmark (eigen)

--- russell_lab/benches/lab_benches.rs
+++ russell_lab/benches/lab_benches.rs
@@ -2,11 +2,11 @@ use criterion::BenchmarkId;
 use criterion::Criterion;
 use criterion::Throughput;
 use criterion::{criterion_group, criterion_main};
-use russell_lab::{vec_add, Vector};
+use russell_lab::{mat_eigen_sym, mat_eigen_sym_jacobi, vec_add, Matrix, Vector};
 
-fn bench_vec_add(c: &mut Criterion) {
+fn _bench_vec_add(c: &mut Criterion) {
     let sizes = &[1, 4, 16, 32, 64, 128];
-    let mut group = c.benchmark_group("russell_lab::vec_add");
+    let mut group = c.benchmark_group("lab_vec_add");
     for size in sizes {
         group.throughput(Throughput::Elements(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
@@ -19,5 +19,26 @@ fn bench_vec_add(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_vec_add);
+fn bench_mat_eigen_sym(c: &mut Criterion) {
+    let sizes: Vec<usize> = (1..16).collect();
+    let mut group = c.benchmark_group("lab_mat_eigen_sym");
+    for size in &sizes {
+        group.throughput(Throughput::Elements(*size as u64));
+        group.bench_with_input(BenchmarkId::new("JacobiRotation", size), size, |b, &size| {
+            let mut a = Matrix::filled(size, size, 2.0);
+            let mut v = Matrix::new(size, size);
+            let mut l = Vector::new(size);
+            b.iter(|| mat_eigen_sym_jacobi(&mut l, &mut v, &mut a));
+        });
+        group.bench_with_input(BenchmarkId::new("OpenBLAS", size), size, |b, &size| {
+            let mut a = Matrix::filled(size, size, 2.0);
+            let mut l = Vector::new(size);
+            b.iter(|| mat_eigen_sym(&mut l, &mut a));
+        });
+    }
+    group.finish();
+}
+
+// criterion_group!(benches, bench_vec_add, bench_mat_eigen_sym);
+criterion_group!(benches, bench_mat_eigen_sym);
 criterion_main!(benches);

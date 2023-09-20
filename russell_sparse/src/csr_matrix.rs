ffcc4a46ddb224cf8424e8f2d3ae5d2ebbb3f6cc
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-20 17:21:43 +1000

    Impl CSR to dense conversion

--- russell_sparse/src/csr_matrix.rs
+++ russell_sparse/src/csr_matrix.rs
@@ -1,5 +1,6 @@
-use super::Layout;
-use crate::CooMatrix;
+use super::{CooMatrix, Layout};
+use crate::StrError;
+use russell_lab::Matrix;
 use russell_openblas::to_i32;
 
 /// Holds the arrays needed for a CSR (compressed sparse row) matrix
@@ -162,6 +163,147 @@ impl CsrMatrix {
 
         // results
         csr
+    }
+
+    /// Converts this CsrMatrix to a dense matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use russell_lab::Matrix;
+    /// use russell_sparse::prelude::*;
+    /// use russell_sparse::StrError;
+    ///
+    /// fn main() -> Result<(), StrError> {
+    ///     // allocate a square matrix and store as CSR matrix
+    ///     // ┌                ┐
+    ///     // │  2  3  0  0  0 │
+    ///     // │  3  0  4  0  6 │
+    ///     // │  0 -1 -3  2  0 │
+    ///     // │  0  0  1  0  0 │
+    ///     // │  0  4  2  0  1 │
+    ///     // └                ┘
+    ///     let csr = CsrMatrix {
+    ///         layout: Layout::Full,
+    ///         nrow: 5,
+    ///         ncol: 5,
+    ///         row_pointers: vec![0, 2, 5, 8, 9, 12],
+    ///         col_indices: vec![
+    ///             //                         p
+    ///             0, 1, //    i = 0, count = 0, 1
+    ///             0, 2, 4, // i = 1, count = 2, 3, 4
+    ///             1, 2, 3, // i = 2, count = 5, 6, 7
+    ///             2, //       i = 3, count = 8
+    ///             1, 2, 4, // i = 4, count = 9, 10, 11
+    ///                //              count = 12
+    ///         ],
+    ///         values: vec![
+    ///             //                                 p
+    ///             2.0, 3.0, //        i = 0, count = 0, 1
+    ///             3.0, 4.0, 6.0, //   i = 1, count = 2, 3, 4
+    ///             -1.0, -3.0, 2.0, // i = 2, count = 5, 6, 7
+    ///             1.0, //             i = 3, count = 8
+    ///             4.0, 2.0, 1.0, //   i = 4, count = 9, 10, 11
+    ///                  //                    count = 12
+    ///         ],
+    ///     };
+    ///
+    ///     // covert to dense
+    ///     let a = csr.as_matrix()?;
+    ///     let correct = "┌                ┐\n\
+    ///                    │  2  3  0  0  0 │\n\
+    ///                    │  3  0  4  0  6 │\n\
+    ///                    │  0 -1 -3  2  0 │\n\
+    ///                    │  0  0  1  0  0 │\n\
+    ///                    │  0  4  2  0  1 │\n\
+    ///                    └                ┘";
+    ///     assert_eq!(format!("{}", a), correct);
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn as_matrix(&self) -> Result<Matrix, StrError> {
+        let mut a = Matrix::new(self.nrow, self.ncol);
+        self.to_matrix(&mut a).unwrap();
+        Ok(a)
+    }
+
+    /// Converts this CsrMatrix to a dense matrix
+    ///
+    /// # Input
+    ///
+    /// * `a` -- where to store the dense matrix; must be (nrow, ncol)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use russell_lab::Matrix;
+    /// use russell_sparse::prelude::*;
+    /// use russell_sparse::StrError;
+    ///
+    /// fn main() -> Result<(), StrError> {
+    ///     // allocate a square matrix and store as CSR matrix
+    ///     // ┌                ┐
+    ///     // │  2  3  0  0  0 │
+    ///     // │  3  0  4  0  6 │
+    ///     // │  0 -1 -3  2  0 │
+    ///     // │  0  0  1  0  0 │
+    ///     // │  0  4  2  0  1 │
+    ///     // └                ┘
+    ///     let csr = CsrMatrix {
+    ///         layout: Layout::Full,
+    ///         nrow: 5,
+    ///         ncol: 5,
+    ///         row_pointers: vec![0, 2, 5, 8, 9, 12],
+    ///         col_indices: vec![
+    ///             //                         p
+    ///             0, 1, //    i = 0, count = 0, 1
+    ///             0, 2, 4, // i = 1, count = 2, 3, 4
+    ///             1, 2, 3, // i = 2, count = 5, 6, 7
+    ///             2, //       i = 3, count = 8
+    ///             1, 2, 4, // i = 4, count = 9, 10, 11
+    ///                //              count = 12
+    ///         ],
+    ///         values: vec![
+    ///             //                                 p
+    ///             2.0, 3.0, //        i = 0, count = 0, 1
+    ///             3.0, 4.0, 6.0, //   i = 1, count = 2, 3, 4
+    ///             -1.0, -3.0, 2.0, // i = 2, count = 5, 6, 7
+    ///             1.0, //             i = 3, count = 8
+    ///             4.0, 2.0, 1.0, //   i = 4, count = 9, 10, 11
+    ///                  //                    count = 12
+    ///         ],
+    ///     };
+    ///
+    ///     // covert to dense
+    ///     let mut a = Matrix::new(5, 5);
+    ///     csr.to_matrix(&mut a)?;
+    ///     let correct = "┌                ┐\n\
+    ///                    │  2  3  0  0  0 │\n\
+    ///                    │  3  0  4  0  6 │\n\
+    ///                    │  0 -1 -3  2  0 │\n\
+    ///                    │  0  0  1  0  0 │\n\
+    ///                    │  0  4  2  0  1 │\n\
+    ///                    └                ┘";
+    ///     assert_eq!(format!("{}", a), correct);
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn to_matrix(&self, a: &mut Matrix) -> Result<(), StrError> {
+        let (m, n) = a.dims();
+        if m != self.nrow || n != self.ncol {
+            return Err("wrong matrix dimensions");
+        }
+        a.fill(0.0);
+        for i in 0..self.nrow {
+            for p in self.row_pointers[i]..self.row_pointers[i + 1] {
+                let j = self.col_indices[p as usize] as usize;
+                a.add(i, j, self.values[p as usize]);
+                if self.layout != Layout::Full && i != j {
+                    a.add(j, i, self.values[p as usize]);
+                }
+            }
+        }
+        Ok(())
     }
 }
 

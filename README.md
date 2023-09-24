be322d76005de7d494e7b4d36a5c12dff6c13bea
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-24 16:23:36 +1000

    [wip] Remove doc examples temporarily

--- README.md
+++ README.md
@@ -120,58 +120,7 @@ fn main() -> Result<(), StrError> {
 
 ### Solve a small sparse linear system using UMFPACK
 
-```rust
-use russell_chk::vec_approx_eq;
-use russell_lab::{Matrix, Vector};
-use russell_sparse::prelude::*;
-use russell_sparse::StrError;
-
-fn main() -> Result<(), StrError> {
-    // allocate a square matrix
-    let (nrow, ncol, nnz) = (5, 5, 13);
-    let mut coo = CooMatrix::new(None, nrow, ncol, nnz)?;
-    coo.put(0, 0, 1.0)?; // << (0, 0, a00/2) duplicate
-    coo.put(0, 0, 1.0)?; // << (0, 0, a00/2) duplicate
-    coo.put(1, 0, 3.0)?;
-    coo.put(0, 1, 3.0)?;
-    coo.put(2, 1, -1.0)?;
-    coo.put(4, 1, 4.0)?;
-    coo.put(1, 2, 4.0)?;
-    coo.put(2, 2, -3.0)?;
-    coo.put(3, 2, 1.0)?;
-    coo.put(4, 2, 2.0)?;
-    coo.put(2, 3, 2.0)?;
-    coo.put(1, 4, 6.0)?;
-    coo.put(4, 4, 1.0)?;
-
-    // print matrix
-    let mut a = Matrix::new(nrow, ncol);
-    coo.to_matrix(&mut a)?;
-    let correct = "┌                ┐\n\
-                   │  2  3  0  0  0 │\n\
-                   │  3  0  4  0  6 │\n\
-                   │  0 -1 -3  2  0 │\n\
-                   │  0  0  1  0  0 │\n\
-                   │  0  4  2  0  1 │\n\
-                   └                ┘";
-    assert_eq!(format!("{}", a), correct);
-
-    // allocate x and rhs
-    let mut x = Vector::new(nrow);
-    let rhs = Vector::from(&[8.0, 45.0, -3.0, 3.0, 19.0]);
-
-    // initialize, factorize, and solve
-    let mut solver = SolverUMFPACK::new()?;
-    solver.initialize(&coo)?;
-    solver.factorize(&coo, false)?;
-    solver.solve(&mut x, &rhs, false)?;
-
-    // check
-    let correct = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-    vec_approx_eq(x.as_data(), &correct, 1e-14);
-    Ok(())
-}
-```
+TODO
 
 ## Todo list
 
--- russell_sparse/src/lib.rs
+++ russell_sparse/src/lib.rs
@@ -6,154 +6,15 @@
 //!
 //! ## Solving a sparse linear system using UMFPACK
 //!
-//! ```
-//! use russell_chk::vec_approx_eq;
-//! use russell_lab::{Matrix, Vector};
-//! use russell_sparse::prelude::*;
-//! use russell_sparse::StrError;
-//!
-//! fn main() -> Result<(), StrError> {
-//!     // allocate a square matrix
-//!     let (nrow, ncol, nnz) = (5, 5, 13);
-//!     let mut coo = CooMatrix::new(None, nrow, ncol, nnz)?;
-//!     coo.put(0, 0, 1.0)?; // << (0, 0, a00/2) duplicate
-//!     coo.put(0, 0, 1.0)?; // << (0, 0, a00/2) duplicate
-//!     coo.put(1, 0, 3.0)?;
-//!     coo.put(0, 1, 3.0)?;
-//!     coo.put(2, 1, -1.0)?;
-//!     coo.put(4, 1, 4.0)?;
-//!     coo.put(1, 2, 4.0)?;
-//!     coo.put(2, 2, -3.0)?;
-//!     coo.put(3, 2, 1.0)?;
-//!     coo.put(4, 2, 2.0)?;
-//!     coo.put(2, 3, 2.0)?;
-//!     coo.put(1, 4, 6.0)?;
-//!     coo.put(4, 4, 1.0)?;
-//!
-//!     // print matrix
-//!     let mut a = Matrix::new(nrow, ncol);
-//!     coo.to_matrix(&mut a)?;
-//!     let correct = "┌                ┐\n\
-//!                    │  2  3  0  0  0 │\n\
-//!                    │  3  0  4  0  6 │\n\
-//!                    │  0 -1 -3  2  0 │\n\
-//!                    │  0  0  1  0  0 │\n\
-//!                    │  0  4  2  0  1 │\n\
-//!                    └                ┘";
-//!     assert_eq!(format!("{}", a), correct);
-//!
-//!     // allocate x and rhs
-//!     let mut x = Vector::new(nrow);
-//!     let rhs = Vector::from(&[8.0, 45.0, -3.0, 3.0, 19.0]);
-//!
-//!     // initialize, factorize, and solve
-//!     let mut solver = SolverUMFPACK::new()?;
-//!     solver.initialize(&coo)?;
-//!     solver.factorize(&coo, false)?;
-//!     solver.solve(&mut x, &rhs, false)?;
-//!
-//!     // check
-//!     let correct = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-//!     vec_approx_eq(x.as_data(), &correct, 1e-14);
-//!     Ok(())
-//! }
-//! ```
+//! TODO
 //!
 //! ## Using the common solver interface
 //!
-//! ```
-//! use russell_chk::vec_approx_eq;
-//! use russell_lab::{Matrix, Vector};
-//! use russell_sparse::prelude::*;
-//! use russell_sparse::StrError;
-//!
-//! fn main() -> Result<(), StrError> {
-//!     // allocate a square matrix
-//!     let nrow = 3; // number of equations
-//!     let ncol = nrow; // number of equations
-//!     let nnz = 5; // number of non-zeros
-//!     let mut coo = CooMatrix::new(None, nrow, ncol, nnz)?;
-//!     coo.put(0, 0, 0.2)?;
-//!     coo.put(0, 1, 0.2)?;
-//!     coo.put(1, 0, 0.5)?;
-//!     coo.put(1, 1, -0.25)?;
-//!     coo.put(2, 2, 0.25)?;
-//!
-//!     // print matrix
-//!     let mut a = Matrix::new(nrow, ncol);
-//!     coo.to_matrix(&mut a)?;
-//!     let correct = "┌                   ┐\n\
-//!                    │   0.2   0.2     0 │\n\
-//!                    │   0.5 -0.25     0 │\n\
-//!                    │     0     0  0.25 │\n\
-//!                    └                   ┘";
-//!     assert_eq!(format!("{}", a), correct);
-//!
-//!     // allocate solver, initialize, and factorize
-//!     let mut solver = Solver::new(Genie::Umfpack)?;
-//!     solver.actual.initialize(&coo)?;
-//!     solver.actual.factorize(&coo, false)?;
-//!
-//!     // allocate rhs
-//!     let rhs1 = Vector::from(&[1.0, 1.0, 1.0]);
-//!     let rhs2 = Vector::from(&[2.0, 2.0, 2.0]);
-//!
-//!     // calculate solution
-//!     let mut x1 = Vector::new(nrow);
-//!     solver.actual.solve(&mut x1, &rhs1, false)?;
-//!     let correct = vec![3.0, 2.0, 4.0];
-//!     vec_approx_eq(x1.as_data(), &correct, 1e-14);
-//!
-//!     // solve again
-//!     let mut x2 = Vector::new(nrow);
-//!     solver.actual.solve(&mut x2, &rhs2, false)?;
-//!     let correct = vec![6.0, 4.0, 8.0];
-//!     vec_approx_eq(x2.as_data(), &correct, 1e-14);
-//!     Ok(())
-//! }
-//! ```
+//! TODO
 //!
 //! ## Using the common solver interface (single-use)
 //!
-//! ```
-//! use russell_chk::vec_approx_eq;
-//! use russell_lab::{Matrix, Vector};
-//! use russell_sparse::prelude::*;
-//! use russell_sparse::StrError;
-//!
-//! fn main() -> Result<(), StrError> {
-//!     // allocate a square matrix
-//!     let nrow = 3; // number of equations
-//!     let ncol = nrow; // number of equations
-//!     let nnz = 5; // number of non-zeros
-//!     let mut coo = CooMatrix::new(None, nrow, ncol, nnz)?;
-//!     coo.put(0, 0, 0.2)?;
-//!     coo.put(0, 1, 0.2)?;
-//!     coo.put(1, 0, 0.5)?;
-//!     coo.put(1, 1, -0.25)?;
-//!     coo.put(2, 2, 0.25)?;
-//!
-//!     // print matrix
-//!     let mut a = Matrix::new(nrow, ncol);
-//!     coo.to_matrix(&mut a)?;
-//!     let correct = "┌                   ┐\n\
-//!                    │   0.2   0.2     0 │\n\
-//!                    │   0.5 -0.25     0 │\n\
-//!                    │     0     0  0.25 │\n\
-//!                    └                   ┘";
-//!     assert_eq!(format!("{}", a), correct);
-//!
-//!     // solve the linear system
-//!     let mut x = Vector::new(nrow);
-//!     let rhs = Vector::from(&[1.0, 1.0, 1.0]);
-//!     Solver::compute(Genie::Umfpack, &coo, &mut x, &rhs, false)?;
-//!
-//!     // check
-//!     let correct = vec![3.0, 2.0, 4.0];
-//!     vec_approx_eq(x.as_data(), &correct, 1e-14);
-//!     Ok(())
-//! }
-//! ```
+//! TODO
 
 /// Defines a type alias for the error type as a static string
 pub type StrError = &'static str;

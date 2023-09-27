af4fc6f34922dbad9bcefc0f33d7926a3449e4bf
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-27 22:49:32 +1000

    Add memcheck log

--- russell_sparse/zscripts/memcheck.bash
+++ russell_sparse/zscripts/memcheck.bash
@@ -11,10 +11,14 @@ cargo build
 cargo valgrind run --bin mem_check
 cargo valgrind run --bin solve_matrix_market -- data/matrix_market/bfwb62.mtx
 cargo valgrind run --bin solve_matrix_market -- data/matrix_market/bfwb62.mtx --genie mumps -n 1
+
+if [[ "$WITH_DSS" == "1" ]]; then
+    cargo valgrind run --bin solve_matrix_market -- data/matrix_market/bfwb62.mtx --genie dss
+fi
+
 cargo valgrind run --example nonlinear_system_4eqs --
 cargo valgrind run --example nonlinear_system_4eqs -- -g mumps
 
 if [[ "$WITH_DSS" == "1" ]]; then
-    cargo valgrind run --bin solve_matrix_market -- data/matrix_market/bfwb62.mtx --genie dss
     cargo valgrind run --example nonlinear_system_4eqs -- -g dss
 fi

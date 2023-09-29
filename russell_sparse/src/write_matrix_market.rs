2ba52f4f17b63542363c6529f195a03d8809c6d6
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-29 21:40:36 +1000

    Add tests

--- russell_sparse/src/write_matrix_market.rs
+++ russell_sparse/src/write_matrix_market.rs
@@ -27,7 +27,7 @@ use std::path::Path;
 /// use russell_sparse::prelude::*;
 /// use russell_sparse::StrError;
 ///
-/// const SAVE_FILE: bool = true;
+/// const SAVE_FILE: bool = false;
 ///
 /// fn main() -> Result<(), StrError> {
 ///     // allocate a square matrix and store as CSR matrix
@@ -97,11 +97,12 @@ where
             None => write!(&mut buffer, "%%MatrixMarket matrix coordinate real general\n").unwrap(),
         };
     }
-    write!(&mut buffer, "{} {} {}\n", nrow, ncol, nnz).unwrap();
 
     // write data
     match mat.get_csc() {
         Ok(csc) => {
+            let (nrow, ncol, nnz, _) = csc.get_info(); // to use final_nnz
+            write!(&mut buffer, "{} {} {}\n", nrow, ncol, nnz).unwrap();
             for j in 0..csc.ncol {
                 for p in csc.col_pointers[j]..csc.col_pointers[j + 1] {
                     let i = csc.row_indices[p as usize] as usize;
@@ -112,6 +113,8 @@ where
         }
         Err(_) => match mat.get_csr() {
             Ok(csr) => {
+                let (nrow, ncol, nnz, _) = csr.get_info(); // to use final_nnz
+                write!(&mut buffer, "{} {} {}\n", nrow, ncol, nnz).unwrap();
                 for i in 0..csr.nrow {
                     for p in csr.row_pointers[i]..csr.row_pointers[i + 1] {
                         let j = csr.col_indices[p as usize] as usize;
@@ -122,6 +125,8 @@ where
             }
             Err(_) => {
                 let csc = mat.get_csc_or_from_coo()?;
+                let (nrow, ncol, nnz, _) = csc.get_info(); // to use final_nnz
+                write!(&mut buffer, "{} {} {}\n", nrow, ncol, nnz).unwrap();
                 for j in 0..csc.ncol {
                     for p in csc.col_pointers[j]..csc.col_pointers[j + 1] {
                         let i = csc.row_indices[p as usize] as usize;
@@ -153,22 +158,88 @@ where
 #[cfg(test)]
 mod tests {
     use super::write_matrix_market;
-    use crate::{Samples, SparseMatrix};
+    use crate::{CooMatrix, Samples, SparseMatrix};
     use std::fs;
 
     #[test]
-    fn write_matrix_market_works() {
-        // ┌                ┐
-        // │  2  3  0  0  0 │
-        // │  3  0  4  0  6 │
-        // │  0 -1 -3  2  0 │
-        // │  0  0  1  0  0 │
-        // │  0  4  2  0  1 │
-        // └                ┘
-        let (_, _, csr, _) = Samples::umfpack_unsymmetric_5x5(false);
-        let mut mat = SparseMatrix::from_csr(csr);
+    fn write_matrix_market_captures_errors() {
+        let wrong = CooMatrix {
+            symmetry: None,
+            nrow: 0,
+            ncol: 0,
+            nnz: 0,
+            max_nnz: 0,
+            indices_i: vec![],
+            indices_j: vec![],
+            values: vec![],
+            one_based: false,
+        };
+        let mut mat = &mut SparseMatrix::from_coo(wrong);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_wrong.mtx";
+        assert_eq!(
+            write_matrix_market(&mut mat, full_path, false).err(),
+            Some("nrow, ncol, and nnz must be greater than zero")
+        )
+    }
 
-        let full_path = "/tmp/russell_sparse/test_write_matrix_market.mtx";
+    #[test]
+    fn write_matrix_market_works() {
+        //  2  3  .  .  .
+        //  3  .  4  .  6
+        //  . -1 -3  2  .
+        //  .  .  1  .  .
+        //  .  4  2  .  1
+        let (coo, csc, csr, _) = Samples::umfpack_unsymmetric_5x5(false);
+
+        // COO
+        let mut mat = SparseMatrix::from_coo(coo);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_coo.mtx";
+        write_matrix_market(&mut mat, full_path, false).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(
+            contents,
+            "%%MatrixMarket matrix coordinate real general\n\
+             5 5 12\n\
+             1 1 2.0\n\
+             2 1 3.0\n\
+             1 2 3.0\n\
+             3 2 -1.0\n\
+             5 2 4.0\n\
+             2 3 4.0\n\
+             3 3 -3.0\n\
+             4 3 1.0\n\
+             5 3 2.0\n\
+             3 4 2.0\n\
+             2 5 6.0\n\
+             5 5 1.0\n"
+        );
+
+        // CSC
+        let mut mat = SparseMatrix::from_csc(csc);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc.mtx";
+        write_matrix_market(&mut mat, full_path, false).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(
+            contents,
+            "%%MatrixMarket matrix coordinate real general\n\
+             5 5 12\n\
+             1 1 2.0\n\
+             2 1 3.0\n\
+             1 2 3.0\n\
+             3 2 -1.0\n\
+             5 2 4.0\n\
+             2 3 4.0\n\
+             3 3 -3.0\n\
+             4 3 1.0\n\
+             5 3 2.0\n\
+             3 4 2.0\n\
+             2 5 6.0\n\
+             5 5 1.0\n"
+        );
+
+        // CSR
+        let mut mat = SparseMatrix::from_csr(csr);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csr.mtx";
         write_matrix_market(&mut mat, full_path, false).unwrap();
         let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
         assert_eq!(
@@ -191,18 +262,68 @@ mod tests {
     }
 
     #[test]
-    fn write_matrix_market_vismatrix_works() {
-        // ┌                ┐
-        // │  2  3  0  0  0 │
-        // │  3  0  4  0  6 │
-        // │  0 -1 -3  2  0 │
-        // │  0  0  1  0  0 │
-        // │  0  4  2  0  1 │
-        // └                ┘
-        let (_, _, csr, _) = Samples::umfpack_unsymmetric_5x5(false);
-        let mut mat = SparseMatrix::from_csr(csr);
+    fn write_matrix_market_lower_works() {
+        //     9   1.5     6  0.75     3
+        //   1.5   0.5     .     .     .
+        //     6     .    12     .     .
+        //  0.75     .     . 0.625     .
+        //     3     .     .     .    16
+        let (_, csc, _, _) = Samples::mkl_symmetric_5x5_lower(false, false, false);
 
-        let full_path = "/tmp/russell_sparse/test_write_matrix_market_vismatrix.smat";
+        let mut mat = SparseMatrix::from_csc(csc);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_lower_csc.smat";
+        write_matrix_market(&mut mat, full_path, false).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(
+            contents,
+            "%%MatrixMarket matrix coordinate real symmetric\n\
+             5 5 9\n\
+             1 1 9.0\n\
+             2 1 1.5\n\
+             3 1 6.0\n\
+             4 1 0.75\n\
+             5 1 3.0\n\
+             2 2 0.5\n\
+             3 3 12.0\n\
+             4 4 0.625\n\
+             5 5 16.0\n"
+        );
+    }
+
+    #[test]
+    fn write_matrix_market_vismatrix_works() {
+        //  2  3  .  .  .
+        //  3  .  4  .  6
+        //  . -1 -3  2  .
+        //  .  .  1  .  .
+        //  .  4  2  .  1
+        let (_, csc, csr, _) = Samples::umfpack_unsymmetric_5x5(false);
+
+        // CSC
+        let mut mat = SparseMatrix::from_csc(csc);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_vismatrix_csc.smat";
+        write_matrix_market(&mut mat, full_path, true).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(
+            contents,
+            "5 5 12\n\
+             0 0 2.0\n\
+             1 0 3.0\n\
+             0 1 3.0\n\
+             2 1 -1.0\n\
+             4 1 4.0\n\
+             1 2 4.0\n\
+             2 2 -3.0\n\
+             3 2 1.0\n\
+             4 2 2.0\n\
+             2 3 2.0\n\
+             1 4 6.0\n\
+             4 4 1.0\n"
+        );
+
+        // CSR
+        let mut mat = SparseMatrix::from_csr(csr);
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_vismatrix_csr.smat";
         write_matrix_market(&mut mat, full_path, true).unwrap();
         let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
         assert_eq!(

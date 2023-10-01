44f952f9603c19a104dc99c48bbc5f77031e6a7c
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-01 11:46:18 +1000

    [wip] Improve stats

--- russell_sparse/src/lin_sol_stats.rs
+++ russell_sparse/src/lin_sol_stats.rs
@@ -1,4 +1,4 @@
-use super::{LinSolStatsMUMPS, LinSolver};
+use super::{LinSolParams, LinSolStatsMUMPS, LinSolver, SparseMatrix};
 use serde::{Deserialize, Serialize};
 use serde_json;
 
@@ -21,7 +21,6 @@ pub struct LinSolStatsMatrix {
 pub struct LinSolStatsSolver {
     pub name: String,
     pub version: String,
-    pub documentation: String,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -87,125 +86,84 @@ pub struct LinSolStats {
     pub error: LinSolStatsError,
     pub time_human: LinSolStatsTimeHuman,
     pub time_nanoseconds: LinSolStatsTimeNanoseconds,
-    // pub mumps_stats: LinSolStatsMUMPS,
-}
-
-impl LinSolStatsMain {
-    pub fn new() -> Self {
-        LinSolStatsMain {
-            platform: "Russell".to_string(),
-            blas_lib: "OpenBLAS".to_string(),
-        }
-    }
-}
-
-impl LinSolStatsMatrix {
-    pub fn new() -> Self {
-        LinSolStatsMatrix {
-            name: String::new(),
-            nrow: 0,
-            ncol: 0,
-            nnz: 0,
-            symmetry: String::new(),
-        }
-    }
-}
-
-impl LinSolStatsSolver {
-    pub fn new() -> Self {
-        LinSolStatsSolver {
-            name: String::new(),
-            version: String::new(),
-            documentation: String::new(),
-        }
-    }
-}
-
-impl LinSolStatsRequests {
-    pub fn new() -> Self {
-        LinSolStatsRequests {
-            ordering: String::new(),
-            scaling: String::new(),
-            mumps_openmp_num_threads: 0,
-        }
-    }
-}
-
-impl LinSolStatsOutput {
-    pub fn new() -> Self {
-        LinSolStatsOutput {
-            effective_ordering: String::new(),
-            effective_scaling: String::new(),
-            openmp_num_threads: 0,
-            umfpack_strategy: String::new(),
-            umfpack_rcond: 0.0,
-        }
-    }
-}
-
-impl LinSolStatsDeterminant {
-    pub fn new() -> Self {
-        LinSolStatsDeterminant {
-            computed: false,
-            mantissa: 0.0,
-            base: 0.0,
-            exponent: 0.0,
-        }
-    }
-}
-
-impl LinSolStatsError {
-    pub fn new() -> Self {
-        LinSolStatsError {
-            computed: false,
-            max_abs_a: 0.0,
-            max_abs_a_times_x: 0.0,
-            relative_error: 0.0,
-        }
-    }
-}
-
-impl LinSolStatsTimeHuman {
-    pub fn new() -> Self {
-        LinSolStatsTimeHuman {
-            read_matrix_market: String::new(),
-            factorize: String::new(),
-            solve: String::new(),
-            total: String::new(),
-            verify: String::new(),
-        }
-    }
-}
-
-impl LinSolStatsTimeNanoseconds {
-    pub fn new() -> Self {
-        LinSolStatsTimeNanoseconds {
-            read_matrix_market: 0,
-            factorize: 0,
-            solve: 0,
-            total: 0,
-            verify: 0,
-        }
-    }
+    pub mumps_stats: LinSolStatsMUMPS,
 }
 
 impl LinSolStats {
-    pub fn new(solver: &LinSolver) -> Self {
-        LinSolStats {
-            main: LinSolStatsMain::new(),
-            matrix: LinSolStatsMatrix::new(),
-            solver: LinSolStatsSolver::new(),
-            requests: LinSolStatsRequests::new(),
-            output: LinSolStatsOutput::new(),
-            determinant: LinSolStatsDeterminant::new(),
-            error: LinSolStatsError::new(),
-            time_human: LinSolStatsTimeHuman::new(),
-            time_nanoseconds: LinSolStatsTimeNanoseconds::new(),
-            // mumps_stats: LinSolStatsMUMPS::new(),
-        }
-    }
-
     pub fn get_json(&self) -> String {
         serde_json::to_string_pretty(&self).unwrap()
+    }
+
+    pub fn new(matrix_name: String, matrix: &SparseMatrix, solver: &LinSolver, params: Option<LinSolParams>) -> Self {
+        let (nrow, ncol, nnz, symmetry) = matrix.get_info();
+        let par = if let Some(p) = params { p } else { LinSolParams::new() };
+        let sol = &solver.actual;
+        let umfpack_rcond = 0.0;
+        let (mantissa, base, exponent) = sol.get_determinant();
+        LinSolStats {
+            main: LinSolStatsMain {
+                platform: "Russell".to_string(),
+                blas_lib: "OpenBLAS".to_string(),
+            },
+            matrix: LinSolStatsMatrix {
+                name: matrix_name,
+                nrow,
+                ncol,
+                nnz,
+                symmetry: format!("{:?}", symmetry),
+            },
+            solver: LinSolStatsSolver {
+                name: sol.get_name(),
+                version: String::new(), //solver.actual.get_version(),
+            },
+            requests: LinSolStatsRequests {
+                ordering: format!("{:?}", par.ordering),
+                scaling: format!("{:?}", par.scaling),
+                mumps_openmp_num_threads: par.mumps_openmp_num_threads,
+            },
+            output: LinSolStatsOutput {
+                effective_ordering: sol.get_effective_ordering(),
+                effective_scaling: sol.get_effective_scaling(),
+                openmp_num_threads: 0,
+                umfpack_strategy: sol.get_effective_strategy(),
+                umfpack_rcond,
+            },
+            determinant: LinSolStatsDeterminant {
+                computed: par.compute_determinant,
+                mantissa,
+                base,
+                exponent,
+            },
+            error: LinSolStatsError {
+                computed: false,
+                max_abs_a: 0.0,
+                max_abs_a_times_x: 0.0,
+                relative_error: 0.0,
+            },
+            time_human: LinSolStatsTimeHuman {
+                read_matrix_market: String::new(),
+                factorize: String::new(),
+                solve: String::new(),
+                total: String::new(),
+                verify: String::new(),
+            },
+            time_nanoseconds: LinSolStatsTimeNanoseconds {
+                read_matrix_market: 0,
+                factorize: 0,
+                solve: 0,
+                total: 0,
+                verify: 0,
+            },
+            mumps_stats: LinSolStatsMUMPS {
+                inf_norm_a: 0.0,
+                inf_norm_x: 0.0,
+                scaled_residual: 0.0,
+                backward_error_omega1: 0.0,
+                backward_error_omega2: 0.0,
+                normalized_delta_x: 0.0,
+                condition_number1: 0.0,
+                condition_number2: 0.0,
+            },
+        }
     }
 }

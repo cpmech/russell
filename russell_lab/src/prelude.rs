2af4230be66a6f3de476f4862ad649eba54c7a66
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-06 20:36:04 +1000

    Add more functions to prelude

--- russell_lab/src/prelude.rs
+++ russell_lab/src/prelude.rs
@@ -3,11 +3,14 @@
 //! You may write `use russell_lab::prelude::*` in your code and obtain
 //! access to commonly used functionality.
 
+pub use crate::auxiliary_and_constants::*;
 pub use crate::enums::*;
+pub use crate::formatters::*;
 pub use crate::generators::*;
 pub use crate::matrix::*;
 pub use crate::matvec::*;
 pub use crate::read_table::*;
 pub use crate::sort::*;
+pub use crate::sort_vec_mat::*;
 pub use crate::stopwatch::*;
 pub use crate::vector::*;

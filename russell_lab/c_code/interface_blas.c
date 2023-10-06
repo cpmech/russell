a6c064277a331688f89431f60645b282b6077c24
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-06 14:41:44 +1000

    Remove comments about functions in interface_blas

--- russell_lab/c_code/interface_blas.c
+++ russell_lab/c_code/interface_blas.c
@@ -22,6 +22,27 @@
 
 #include "constants.h"
 
+// Here, we assume that:
+//
+// 1. OPENBLAS_CONST = const
+// 2. CBLAS_INDEX = int
+// 3. blasint = int
+// 4. MKL_INT = int
+//
+// i.e., this will only work with i32 and f64
+//
+// Note: CBLAS uses "pointer to void" to represent complex numbers.
+//
+// OpenBLAS include files:
+//
+// 1. /usr/include/x86_64-linux-gnu/cblas.h
+// 2. /usr/include/lapack.h
+//
+// Intel MKL include files:
+//
+// 1. /opt/intel/oneapi/mkl/latest/include/mkl_cblas.h
+// 2. /opt/intel/oneapi/mkl/latest/include/mkl_lapack.h
+
 int32_t c_using_intel_mkl() {
 #ifdef USE_INTEL_MKL
     return 1;
@@ -125,187 +146,3 @@ void c_dsyev(C_BOOL calc_v,
         }
     }
 }
-
-// Here, we assume that (SUBSTITUTIONS):
-//
-// 1. OPENBLAS_CONST = const
-// 2. blasint = int
-// 3. MKL_INT = int
-// 4. CBLAS_INDEX = int
-//
-// i.e., this will only work with i32 and f64
-//
-// Note: CBLAS uses "pointer to void" to represent complex numbers.
-//
-// vector //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// --- OpenBLAS --------------------------------------------------------------------------------------------------------
-//
-// From: /usr/include/x86_64-linux-gnu/cblas.h
-//
-// double cblas_ddot(OPENBLAS_CONST blasint n, OPENBLAS_CONST double *x, OPENBLAS_CONST blasint incx, OPENBLAS_CONST double *y, OPENBLAS_CONST blasint incy);
-// void cblas_dcopy(OPENBLAS_CONST blasint n, OPENBLAS_CONST double *x, OPENBLAS_CONST blasint incx, double *y, OPENBLAS_CONST blasint incy);
-// void cblas_zcopy(OPENBLAS_CONST blasint n, OPENBLAS_CONST void *x, OPENBLAS_CONST blasint incx, void *y, OPENBLAS_CONST blasint incy);
-// void cblas_dscal(OPENBLAS_CONST blasint N, OPENBLAS_CONST double alpha, double *X, OPENBLAS_CONST blasint incX);
-// void cblas_zscal(OPENBLAS_CONST blasint N, OPENBLAS_CONST void *alpha, void *X, OPENBLAS_CONST blasint incX);
-// void cblas_daxpy(OPENBLAS_CONST blasint n, OPENBLAS_CONST double alpha, OPENBLAS_CONST double *x, OPENBLAS_CONST blasint incx, double *y, OPENBLAS_CONST blasint incy);
-// void cblas_zaxpy(OPENBLAS_CONST blasint n, OPENBLAS_CONST void *alpha, OPENBLAS_CONST void *x, OPENBLAS_CONST blasint incx, void *y, OPENBLAS_CONST blasint incy);
-// double cblas_dnrm2 (OPENBLAS_CONST blasint N, OPENBLAS_CONST double *X, OPENBLAS_CONST blasint incX);
-// double cblas_dasum (OPENBLAS_CONST blasint n, OPENBLAS_CONST double *x, OPENBLAS_CONST blasint incx);
-// CBLAS_INDEX cblas_idamax(OPENBLAS_CONST blasint n, OPENBLAS_CONST double *x, OPENBLAS_CONST blasint incx);
-//
-// --- Intel MKL -------------------------------------------------------------------------------------------------------
-//
-// From: /opt/intel/oneapi/mkl/latest/include/mkl_cblas.h
-//
-// double cblas_ddot(const MKL_INT N, const double *X, const MKL_INT incX, const double *Y, const MKL_INT incY) NOTHROW;
-// void cblas_dcopy(const MKL_INT N, const double *X, const MKL_INT incX, double *Y, const MKL_INT incY) NOTHROW;
-// void cblas_zcopy(const MKL_INT N, const void *X, const MKL_INT incX, void *Y, const MKL_INT incY) NOTHROW;
-// void cblas_dscal(const MKL_INT N, const double alpha, double *X, const MKL_INT incX) NOTHROW;
-// void cblas_zscal(const MKL_INT N, const void *alpha, void *X, const MKL_INT incX) NOTHROW;
-// void cblas_daxpy(const MKL_INT N, const double alpha, const double *X, const MKL_INT incX, double *Y, const MKL_INT incY) NOTHROW;
-// void cblas_zaxpy(const MKL_INT N, const void *alpha, const void *X, const MKL_INT incX, void *Y, const MKL_INT incY) NOTHROW;
-// double cblas_dnrm2(const MKL_INT N, const double *X, const MKL_INT incX) NOTHROW;
-// double cblas_dasum(const MKL_INT N, const double *X, const MKL_INT incX) NOTHROW;
-// CBLAS_INDEX cblas_idamax(const MKL_INT N, const double *X, const MKL_INT incX) NOTHROW;
-//
-// matrix //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// --- OpenBLAS --------------------------------------------------------------------------------------------------------
-//
-// From: /usr/include/x86_64-linux-gnu/cblas.h
-//
-// typedef enum CBLAS_ORDER     {CblasRowMajor=101, CblasColMajor=102} CBLAS_ORDER;
-// typedef enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113, CblasConjNoTrans=114} CBLAS_TRANSPOSE;
-// typedef enum CBLAS_UPLO      {CblasUpper=121, CblasLower=122} CBLAS_UPLO;
-// typedef enum CBLAS_DIAG      {CblasNonUnit=131, CblasUnit=132} CBLAS_DIAG;
-// typedef enum CBLAS_SIDE      {CblasLeft=141, CblasRight=142} CBLAS_SIDE;
-// typedef CBLAS_ORDER CBLAS_LAYOUT;
-//
-// void cblas_dgemm(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N,
-//                  OPENBLAS_CONST blasint K, OPENBLAS_CONST double alpha, OPENBLAS_CONST double *A,
-//                  OPENBLAS_CONST blasint lda, OPENBLAS_CONST double *B, OPENBLAS_CONST blasint ldb,
-//                  OPENBLAS_CONST double beta, double *C, OPENBLAS_CONST blasint ldc);
-// void cblas_zgemm(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N,
-//                  OPENBLAS_CONST blasint K, OPENBLAS_CONST void *alpha, OPENBLAS_CONST void *A,
-//                  OPENBLAS_CONST blasint lda, OPENBLAS_CONST void *B, OPENBLAS_CONST blasint ldb,
-//                  OPENBLAS_CONST void *beta, void *C, OPENBLAS_CONST blasint ldc);
-// void cblas_dsyrk(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_UPLO Uplo,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE Trans, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-//                  OPENBLAS_CONST double alpha, OPENBLAS_CONST double *A, OPENBLAS_CONST blasint lda,
-//                  OPENBLAS_CONST double beta, double *C, OPENBLAS_CONST blasint ldc);
-// void cblas_zsyrk(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_UPLO Uplo,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE Trans, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-//                  OPENBLAS_CONST void *alpha, OPENBLAS_CONST void *A, OPENBLAS_CONST blasint lda,
-//                  OPENBLAS_CONST void *beta, void *C, OPENBLAS_CONST blasint ldc);
-// void cblas_zherk(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_UPLO Uplo,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE Trans, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-//                  OPENBLAS_CONST double alpha, OPENBLAS_CONST void *A, OPENBLAS_CONST blasint lda,
-//                  OPENBLAS_CONST double beta, void *C, OPENBLAS_CONST blasint ldc);
-//
-// --- Intel MKL -------------------------------------------------------------------------------------------------------
-//
-// From: /opt/intel/oneapi/mkl/latest/include/mkl_cblas.h
-//
-// enum CBLAS_LAYOUT {CblasRowMajor=101, CblasColMajor=102};
-// enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
-// enum CBLAS_UPLO {CblasUpper=121, CblasLower=122};
-// enum CBLAS_DIAG {CblasNonUnit=131, CblasUnit=132};
-// enum CBLAS_SIDE {CblasLeft=141, CblasRight=142};
-// enum CBLAS_STORAGE {CblasPacked=151};
-// enum CBLAS_IDENTIFIER {CblasAMatrix=161, CblasBMatrix=162};
-// enum CBLAS_OFFSET {CblasRowOffset=171, CblasColOffset=172, CblasFixOffset=173};
-// typedef CBLAS_LAYOUT CBLAS_ORDER; /* this for backward compatibility with CBLAS_ORDER */
-//
-// void cblas_dgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE TransA,
-//                  const CBLAS_TRANSPOSE TransB, const MKL_INT M, const MKL_INT N,
-//                  const MKL_INT K, const double alpha, const double *A,
-//                  const MKL_INT lda, const double *B, const MKL_INT ldb,
-//                  const double beta, double *C, const MKL_INT ldc) NOTHROW;
-// void cblas_zgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE TransA,
-//                  const CBLAS_TRANSPOSE TransB, const MKL_INT M, const MKL_INT N,
-//                  const MKL_INT K, const void *alpha, const void *A,
-//                  const MKL_INT lda, const void *B, const MKL_INT ldb,
-//                  const void *beta, void *C, const MKL_INT ldc) NOTHROW;
-// void cblas_dsyrk(const CBLAS_LAYOUT Layout, const CBLAS_UPLO Uplo,
-//                  const CBLAS_TRANSPOSE Trans, const MKL_INT N, const MKL_INT K,
-//                  const double alpha, const double *A, const MKL_INT lda,
-//                  const double beta, double *C, const MKL_INT ldc) NOTHROW;
-// void cblas_zsyrk(const CBLAS_LAYOUT Layout, const CBLAS_UPLO Uplo,
-//                  const CBLAS_TRANSPOSE Trans, const MKL_INT N, const MKL_INT K,
-//                  const void *alpha, const void *A, const MKL_INT lda,
-//                  const void *beta, void *C, const MKL_INT ldc) NOTHROW;
-// void cblas_zherk(const CBLAS_LAYOUT Layout, const CBLAS_UPLO Uplo,
-//                  const CBLAS_TRANSPOSE Trans, const MKL_INT N, const MKL_INT K,
-//                  const double alpha, const void *A, const MKL_INT lda,
-//                  const double beta, void *C, const MKL_INT ldc) NOTHROW;
-//
-// matrix-vector ///////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// --- OpenBLAS --------------------------------------------------------------------------------------------------------
-//
-// From: /usr/include/x86_64-linux-gnu/cblas.h
-//
-// void cblas_dgemv(OPENBLAS_CONST enum CBLAS_ORDER order,
-//                  OPENBLAS_CONST enum CBLAS_TRANSPOSE trans,  OPENBLAS_CONST blasint m, OPENBLAS_CONST blasint n,
-//                  OPENBLAS_CONST double alpha, OPENBLAS_CONST double  *a, OPENBLAS_CONST blasint lda,
-//                  OPENBLAS_CONST double  *x, OPENBLAS_CONST blasint incx,  OPENBLAS_CONST double beta,
-//                  double  *y, OPENBLAS_CONST blasint incy);
-// void cblas_dger (OPENBLAS_CONST enum CBLAS_ORDER order, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N,
-//                  OPENBLAS_CONST double  alpha, OPENBLAS_CONST double *X, OPENBLAS_CONST blasint incX,
-//                  OPENBLAS_CONST double *Y, OPENBLAS_CONST blasint incY, double *A, OPENBLAS_CONST blasint lda);
-//
-// --- Intel MKL -------------------------------------------------------------------------------------------------------
-//
-// From: /opt/intel/oneapi/mkl/latest/include/mkl_cblas.h
-//
-// void cblas_dgemv(const CBLAS_LAYOUT Layout,
-//                  const CBLAS_TRANSPOSE TransA, const MKL_INT M, const MKL_INT N,
-//                  const double alpha, const double *A, const MKL_INT lda,
-//                  const double *X, const MKL_INT incX, const double beta,
-//                  double *Y, const MKL_INT incY) NOTHROW;
-// void cblas_dger(const CBLAS_LAYOUT Layout, const MKL_INT M, const MKL_INT N,
-//                 const double alpha, const double *X, const MKL_INT incX,
-//                 const double *Y, const MKL_INT incY, double *A, const MKL_INT lda) NOTHROW;
-//
-// LAPACK //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// --- OpenBLAS --------------------------------------------------------------------------------------------------------
-//
-// From: /usr/include/lapack.h
-//
-// NOTE: The functions with char arguments will have to be wrapped and called with the LAPACK_ prefix.
-//
-// void LAPACK_dgesv(lapack_int const* n, lapack_int const* nrhs, double* A,
-//                   lapack_int const* lda, lapack_int* ipiv, double* B, lapack_int const* ldb,
-//                   lapack_int* info );
-// double LAPACK_dlange(char const *norm, lapack_int const *m, lapack_int const *n,
-//                           double const *A, lapack_int const *lda, double *work);
-// double LAPACK_zlange(char const *norm, lapack_int const *m, lapack_int const *n,
-//                           lapack_complex_double const *A, lapack_int const *lda, double *work);
-// void LAPACK_dpotrf(char const *uplo, lapack_int const *n, double *A,
-//                    lapack_int const *lda, lapack_int *info);
-// void LAPACK_dsyev_base(char const *jobz, char const *uplo, lapack_int const *n, double *A,
-//                        lapack_int const *lda, double *W, double *work, lapack_int const *lwork,
-//                        lapack_int *info);
-//
-// --- Intel MKL -------------------------------------------------------------------------------------------------------
-//
-// From: /opt/intel/oneapi/mkl/latest/include/mkl_lapack.h
-// (note the trailing underscore!)
-//
-// void dgesv_( const MKL_INT* n, const MKL_INT* nrhs, double* a,
-//             const MKL_INT* lda, MKL_INT* ipiv, double* b, const MKL_INT* ldb,
-//             MKL_INT* info ) NOTHROW;
-// double dlange_( const char* norm, const MKL_INT* m, const MKL_INT* n,
-//                 const double* a, const MKL_INT* lda, double* work ) NOTHROW;
-// double zlange_( const char* norm, const MKL_INT* m, const MKL_INT* n,
-//                 const MKL_Complex16* a, const MKL_INT* lda, double* work ) NOTHROW;
-// void dpotrf_( const char* uplo, const MKL_INT* n, double* a,
-//               const MKL_INT* lda, MKL_INT* info ) NOTHROW;
-// void dsyev_( const char* jobz, const char* uplo, const MKL_INT* n, double* a,
-//              const MKL_INT* lda, double* w, double* work, const MKL_INT* lwork,
-//              MKL_INT* info ) NOTHROW;
-//

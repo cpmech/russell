25262695e404c48f8ba0441c860c980eb155f424
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-08 07:56:24 +1000

    Add examples

--- russell_lab/examples/ex_matrix_cholesky.rs
+++ russell_lab/examples/ex_matrix_cholesky.rs
@@ -1,40 +1,62 @@
 use russell_lab::*;
 
 fn main() -> Result<(), StrError> {
-    // TODO
+    // set matrix (full)
+    #[rustfmt::skip]
+    let a_full = Matrix::from(&[
+        [ 3.0, 0.0,-3.0, 0.0],
+        [ 0.0, 3.0, 1.0, 2.0],
+        [-3.0, 1.0, 4.0, 1.0],
+        [ 0.0, 2.0, 1.0, 3.0],
+    ]);
 
-    // matrix a
-    // #[rustfmt::skip]
-    // let mut a_up = Matrix::from(&[
-    //     [3.0, 0.0, -3.0, 0.0],
-    //     [0.0, 3.0,  1.0, 2.0],
-    //     [0.0, 0.0,  4.0, 1.0],
-    //     [0.0, 0.0,  0.0, 3.0],
-    // ]);
-    // #[rustfmt::skip]
-    // let mut a_lo = Matrix::from(&[
-    //     [ 3.0, 0.0, 0.0, 0.0],
-    //     [ 0.0, 3.0, 0.0, 0.0],
-    //     [-3.0, 1.0, 4.0, 0.0],
-    //     [ 0.0, 2.0, 1.0, 3.0],
-    // ]);
+    // set matrix (lower)
+    #[rustfmt::skip]
+    let mut a_lower = Matrix::from(&[
+        [ 3.0, 0.0, 0.0, 0.0],
+        [ 0.0, 3.0, 0.0, 0.0],
+        [-3.0, 1.0, 4.0, 0.0],
+        [ 0.0, 2.0, 1.0, 3.0],
+    ]);
 
-    // // perform Cholesky factorization
-    // let m = a_up.nrow();
-    // let mut l = Matrix::new(m, m);
-    // mat_cholesky(&mut l, &a_up)?;
-    // TODO
+    // set matrix (upper)
+    #[rustfmt::skip]
+    let mut a_upper = Matrix::from(&[
+        [3.0, 0.0,-3.0, 0.0],
+        [0.0, 3.0, 1.0, 2.0],
+        [0.0, 0.0, 4.0, 1.0],
+        [0.0, 0.0, 0.0, 3.0],
+    ]);
 
-    // let mut l_lt = Matrix::new(m, m);
-    // for i in 0..m {
-    //     for j in 0..m {
-    //         for k in 0..m {
-    //             l_lt.add(i, j, l.get(i, k) * l.get(j, k));
-    //         }
-    //     }
-    // }
+    // perform Cholesky factorization (lower)
+    mat_cholesky(&mut a_lower, false)?;
+    let l = &a_lower;
 
-    // println!("{}", l_lt);
+    // perform Cholesky factorization (upper)
+    mat_cholesky(&mut a_upper, true)?;
+    let u = &a_upper;
 
+    // check:  l ⋅ lᵀ = a
+    let m = a_full.nrow();
+    let mut l_lt = Matrix::new(m, m);
+    for i in 0..m {
+        for j in 0..m {
+            for k in 0..m {
+                l_lt.add(i, j, l.get(i, k) * l.get(j, k));
+            }
+        }
+    }
+    mat_approx_eq(&l_lt, &a_full, 1e-14);
+
+    // check:   uᵀ ⋅ u = a
+    let mut ut_u = Matrix::new(m, m);
+    for i in 0..m {
+        for j in 0..m {
+            for k in 0..m {
+                ut_u.add(i, j, u.get(k, i) * u.get(k, j));
+            }
+        }
+    }
+    mat_approx_eq(&ut_u, &a_full, 1e-14);
     Ok(())
 }

6106aab69ece1f3ee35a7168fc56c9bdf2eda050
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-07 17:26:03 +1000

    Impl complex_mat_vec_mul

--- russell_lab/src/matvec/mod.rs
+++ russell_lab/src/matvec/mod.rs
@@ -1,5 +1,6 @@
 //! This module contains functions for calculations with matrices and vectors
 
+mod complex_mat_vec_mul;
 mod complex_solve_lin_sys;
 mod complex_vec_mat_mul;
 mod mat_sum_cols;
@@ -8,6 +9,7 @@ mod mat_vec_mul;
 mod solve_lin_sys;
 mod vec_mat_mul;
 mod vec_outer;
+pub use crate::matvec::complex_mat_vec_mul::*;
 pub use crate::matvec::complex_solve_lin_sys::*;
 pub use crate::matvec::complex_vec_mat_mul::*;
 pub use crate::matvec::mat_sum_cols::*;

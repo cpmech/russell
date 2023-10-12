9481a57ebf80502009bb43a97d6916c6fc7738a2
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-12 11:20:39 +1000

    Try pip again

--- zscripts/windows-with-intel-mkl.bash
+++ zscripts/windows-with-intel-mkl.bash
@@ -2,8 +2,7 @@
 
 set -e
 
-pip install --upgrade pip
-python3 -m pip debug --verbose
-# pip install mkl
+#python3 -m pip debug --verbose
+pip3 install mkl
 #pip install intel-fortran-rt
 #pip install mkl-devel

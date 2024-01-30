1b0078aaf927cc1236bf339155531354b32d8fad
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-01-30 10:58:23 +1000

    Add test

--- russell_ode/tests/test_bweuler.rs
+++ russell_ode/tests/test_bweuler.rs
@@ -72,3 +72,40 @@ fn test_bweuler_hairer_wanner_eq1_num_jac() {
     assert_eq!(b.n_iterations_max, 2);
     assert_eq!(b.h_optimal, control.h_equal.unwrap());
 }
+
+#[test]
+fn test_bweuler_hairer_wanner_eq1_modified_newton() {
+    let (system, mut control, mut args) = Samples::hairer_wanner_eq1();
+    let ndim = system.get_ndim();
+    let mut params = OdeParams::new(Method::BwEuler, None, None);
+    params.CteTg = true;
+    let mut solver = OdeSolver::new(&params, system).unwrap();
+    solver
+        .solve(
+            &mut control.y0,
+            control.x0,
+            control.x1,
+            control.h_equal,
+            &mut args,
+            no_step_output,
+            no_dense_output,
+        )
+        .unwrap();
+    let mut analytical = control.y_analytical.unwrap();
+    let mut y1_correct = Vector::new(ndim);
+    analytical(&mut y1_correct, control.x1);
+
+    approx_eq(control.y0[0], 0.09060476604187756, 1e-15);
+    approx_eq(control.y0[0], y1_correct[0], 1e-4);
+
+    let b = solver.bench();
+    println!("{}", solver.bench());
+    assert_eq!(b.n_function_eval, 80);
+    assert_eq!(b.n_jacobian_eval, 1);
+    assert_eq!(b.n_performed_steps, 40);
+    assert_eq!(b.n_accepted_steps, 40);
+    assert_eq!(b.n_rejected_steps, 0);
+    assert_eq!(b.n_iterations_last, 2);
+    assert_eq!(b.n_iterations_max, 2);
+    assert_eq!(b.h_optimal, control.h_equal.unwrap());
+}

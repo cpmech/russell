9013aeec3f1a7696c9e4e5a3b07b834086031698
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-15 11:47:46 +1000

    [sparse] Improve memcheck

--- russell_sparse/src/bin/mem_check.rs
+++ russell_sparse/src/bin/mem_check.rs
@@ -1,14 +1,16 @@
-use russell_lab::Vector;
+use num_complex::Complex64;
+use russell_lab::vec_approx_eq;
+use russell_lab::{complex_vec_approx_eq, cpx, ComplexVector, Vector};
 use russell_sparse::prelude::*;
+use russell_sparse::Samples;
 
 fn test_solver(genie: Genie) {
+    println!("----------------------------------------------------------------------\n");
     match genie {
         Genie::Mumps => println!("Testing MUMPS solver\n"),
         Genie::Umfpack => println!("Testing UMFPACK solver\n"),
         Genie::IntelDss => println!("Testing Intel DSS solver\n"),
     }
-
-    let (ndim, nnz) = (5, 13);
 
     let mut solver = match LinSolver::new(genie) {
         Ok(v) => v,
@@ -18,30 +20,11 @@ fn test_solver(genie: Genie) {
         }
     };
 
-    let one_based = if genie == Genie::Mumps { true } else { false };
-    let mut coo = match SparseMatrix::new_coo(ndim, ndim, nnz, None, one_based) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("FAIL(new CooMatrix): {}", e);
-            return;
-        }
-    };
+    let one_based = genie == Genie::Mumps;
+    let (coo, _, _, _) = Samples::umfpack_unsymmetric_5x5(one_based);
+    let mut mat = SparseMatrix::from_coo(coo);
 
-    coo.put(0, 0, 1.0).unwrap(); // << (0, 0, a00/2)
-    coo.put(0, 0, 1.0).unwrap(); // << (0, 0, a00/2)
-    coo.put(1, 0, 3.0).unwrap();
-    coo.put(0, 1, 3.0).unwrap();
-    coo.put(2, 1, -1.0).unwrap();
-    coo.put(4, 1, 4.0).unwrap();
-    coo.put(1, 2, 4.0).unwrap();
-    coo.put(2, 2, -3.0).unwrap();
-    coo.put(3, 2, 1.0).unwrap();
-    coo.put(4, 2, 2.0).unwrap();
-    coo.put(2, 3, 2.0).unwrap();
-    coo.put(1, 4, 6.0).unwrap();
-    coo.put(4, 4, 1.0).unwrap();
-
-    match solver.actual.factorize(&mut coo, None) {
+    match solver.actual.factorize(&mut mat, None) {
         Err(e) => {
             println!("FAIL(factorize): {}", e);
             return;
@@ -52,7 +35,7 @@ fn test_solver(genie: Genie) {
     let mut x = Vector::new(5);
     let rhs = Vector::from(&[8.0, 45.0, -3.0, 3.0, 19.0]);
 
-    match solver.actual.solve(&mut x, &mut coo, &rhs, false) {
+    match solver.actual.solve(&mut x, &mut mat, &rhs, false) {
         Err(e) => {
             println!("FAIL(solve): {}", e);
             return;
@@ -60,7 +43,7 @@ fn test_solver(genie: Genie) {
         _ => (),
     }
 
-    match solver.actual.solve(&mut x, &coo, &rhs, false) {
+    match solver.actual.solve(&mut x, &mat, &rhs, false) {
         Err(e) => {
             println!("FAIL(solve again): {}", e);
             return;
@@ -69,13 +52,71 @@ fn test_solver(genie: Genie) {
     }
 
     println!("x =\n{}", x);
+    let x_correct = &[1.0, 2.0, 3.0, 4.0, 5.0];
+    vec_approx_eq(x.as_data(), x_correct, 1e-14);
+}
+
+fn test_complex_solver(genie: Genie) {
+    println!("----------------------------------------------------------------------\n");
+    match genie {
+        Genie::Mumps => println!("Testing Complex MUMPS solver\n"),
+        Genie::Umfpack => println!("Testing Complex UMFPACK solver\n"),
+        Genie::IntelDss => println!("Testing Complex Intel DSS solver\n"),
+    }
+
+    let mut solver = match ComplexLinSolver::new(genie) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("FAIL(new solver): {}", e);
+            return;
+        }
+    };
+
+    let coo = match genie {
+        Genie::Mumps => Samples::complex_symmetric_3x3_lower(true).0,
+        Genie::Umfpack => Samples::complex_symmetric_3x3_full().0,
+        Genie::IntelDss => panic!("TODO"),
+    };
+    let mut mat = ComplexSparseMatrix::from_coo(coo);
+
+    match solver.actual.factorize(&mut mat, None) {
+        Err(e) => {
+            println!("FAIL(factorize): {}", e);
+            return;
+        }
+        _ => (),
+    };
+
+    let mut x = ComplexVector::new(3);
+    let rhs = ComplexVector::from(&[cpx!(-3.0, 3.0), cpx!(2.0, -2.0), cpx!(9.0, 7.0)]);
+
+    match solver.actual.solve(&mut x, &mut mat, &rhs, false) {
+        Err(e) => {
+            println!("FAIL(solve): {}", e);
+            return;
+        }
+        _ => (),
+    }
+
+    match solver.actual.solve(&mut x, &mat, &rhs, false) {
+        Err(e) => {
+            println!("FAIL(solve again): {}", e);
+            return;
+        }
+        _ => (),
+    }
+
+    println!("x =\n{}", x);
+    let x_correct = &[cpx!(1.0, 1.0), cpx!(2.0, -2.0), cpx!(3.0, 3.0)];
+    complex_vec_approx_eq(x.as_data(), x_correct, 1e-14);
 }
 
 fn test_solver_singular(genie: Genie) {
+    println!("----------------------------------------------------------------------\n");
     match genie {
-        Genie::Mumps => println!("Testing MUMPS solver\n"),
-        Genie::Umfpack => println!("Testing UMFPACK solver\n"),
-        Genie::IntelDss => println!("Testing Intel DSS solver\n"),
+        Genie::Mumps => println!("Testing MUMPS solver (singular matrix)\n"),
+        Genie::Umfpack => println!("Testing UMFPACK solver (singular matrix)\n"),
+        Genie::IntelDss => println!("Testing Intel DSS solver (singular matrix)\n"),
     }
 
     let (ndim, nnz) = (2, 2);
@@ -106,19 +147,21 @@ fn test_solver_singular(genie: Genie) {
 }
 
 fn main() {
-    println!("Running Mem Check\n");
-    test_solver(Genie::Umfpack);
-    println!("");
-    test_solver_singular(Genie::Umfpack);
-    println!("----------------------------------------------------------------------\n");
+    // real
     test_solver(Genie::Mumps);
-    println!("");
-    test_solver_singular(Genie::Mumps);
+    test_solver(Genie::Umfpack);
     if cfg!(with_intel_dss) {
-        println!("----------------------------------------------------------------------\n");
         test_solver(Genie::IntelDss);
-        // Intel DSS cannot handle singular matrices
-        // test_solver_singular(Genie::IntelDss);
     }
-    println!("Done\n");
+
+    // complex
+    test_complex_solver(Genie::Mumps);
+    test_complex_solver(Genie::Umfpack);
+
+    // singular real
+    test_solver_singular(Genie::Mumps);
+    test_solver_singular(Genie::Umfpack);
+    // Note: Intel DSS cannot handle singular matrices
+
+    println!("----------------------------------------------------------------------\n");
 }

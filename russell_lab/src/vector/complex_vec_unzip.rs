73e6900578138e10a6d0d7b4d948488ca5ce5ec4
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-16 20:09:54 +1000

    [lab] Impl complex_vec_unzip

---
+++ russell_lab/src/vector/complex_vec_unzip.rs
@@ -0,0 +1,72 @@
+use crate::ComplexVector;
+use crate::StrError;
+use crate::Vector;
+
+/// Zips two arrays (real and imag) to make a new ComplexVector
+///
+/// # Example
+///
+/// ```
+/// use russell_lab::*;
+///
+/// fn main() -> Result<(), StrError> {
+///     let mut v = ComplexVector::new(3);
+///     let real = Vector::from(&[1.0, 2.0, 3.0]);
+///     let imag = Vector::from(&[0.1, 0.2, 0.3]);
+///     complex_vec_zip(&mut v, &real, &imag)?;
+///     assert_eq!(
+///         format!("{}", v),
+///         "┌        ┐\n\
+///          │ 1+0.1i │\n\
+///          │ 2+0.2i │\n\
+///          │ 3+0.3i │\n\
+///          └        ┘"
+///     );
+///     Ok(())
+/// }
+/// ```
+pub fn complex_vec_unzip(real: &mut Vector, imag: &mut Vector, v: &ComplexVector) -> Result<(), StrError> {
+    let dim = v.dim();
+    if real.dim() != dim || imag.dim() != dim {
+        return Err("vectors are incompatible");
+    }
+    for i in 0..dim {
+        real[i] = v[i].re;
+        imag[i] = v[i].im;
+    }
+    Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::complex_vec_unzip;
+    use crate::{cpx, vec_approx_eq, ComplexVector, Vector};
+    use num_complex::Complex64;
+
+    #[test]
+    fn complex_vec_unzip_handles_errors() {
+        let v = ComplexVector::new(2);
+        let mut wrong = Vector::new(1);
+        let mut ok = Vector::new(2);
+        assert_eq!(
+            complex_vec_unzip(&mut wrong, &mut ok, &v).err(),
+            Some("vectors are incompatible")
+        );
+        assert_eq!(
+            complex_vec_unzip(&mut ok, &mut wrong, &v).err(),
+            Some("vectors are incompatible")
+        );
+    }
+
+    #[test]
+    fn complex_vec_unzip_works() {
+        let v = ComplexVector::from(&[cpx!(1.0, 4.0), cpx!(2.0, 5.0), cpx!(3.0, 6.0)]);
+        let mut real = Vector::new(3);
+        let mut imag = Vector::new(3);
+        complex_vec_unzip(&mut real, &mut imag, &v).unwrap();
+        vec_approx_eq(real.as_data(), &[1.0, 2.0, 3.0], 1e-15);
+        vec_approx_eq(imag.as_data(), &[4.0, 5.0, 6.0], 1e-15);
+    }
+}

a5a8a065ba4417fdd5e006be53a6b9052d9c5a2c
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-21 17:32:45 +1000

    [ode] Improve compare script

--- russell_ode/zscripts/compare-fortran-data.bash
+++ russell_ode/zscripts/compare-fortran-data.bash
@@ -7,6 +7,27 @@ test_radau5_van_der_pol_dense \
 test_radau5_van_der_pol \
 "
 
+sum=""
+
 for test in $TESTS; do
-    cargo test --test $test -- --nocapture --quiet > "data/log_$test.txt"
+    correct="data/${test/test_/diff_}.txt"
+    fortran="data/${test/test_/fortran_}.txt"
+    russell="data/${test/test_/russell_}.txt"
+
+    # run the test
+    cargo test --test $test -- --nocapture --quiet > $russell 
+
+    # delete the last two lines (blank line, the cargo output, and a dot)
+    sed -i '$d' $russell
+    sed -i '$d' $russell
+    sed -i '$d' $russell
+
+    # temporary diff (run this just once)
+    # pipe to tail to ignore the first two lines because of the date/time
+    # diff -u $fortran $russell | tail -n +3 > $correct
+
+    res=`diff -u $fortran $russell | tail -n +3 | diff - $correct`
+    sum="${sum}${res}"
 done
+
+echo "this should be empty: [${sum}]"

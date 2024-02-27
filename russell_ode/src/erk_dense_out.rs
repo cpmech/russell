fde6db5868b80e8fc6c4d8679edcd0f85cfb0a46
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-27 11:30:17 +1000

    [ode] Simplify ERK dense output struct

--- russell_ode/src/erk_dense_out.rs
+++ russell_ode/src/erk_dense_out.rs
@@ -1,7 +1,7 @@
 use crate::StrError;
 use crate::{Method, System};
 use crate::{DORMAND_PRINCE_5_D, DORMAND_PRINCE_8_AD, DORMAND_PRINCE_8_CD, DORMAND_PRINCE_8_D};
-use russell_lab::{Matrix, Vector};
+use russell_lab::Vector;
 use russell_sparse::CooMatrix;
 
 /// Handles the dense output of explicit Runge-Kutta methods
@@ -11,15 +11,6 @@ pub(crate) struct ErkDenseOut {
 
     /// System dimension
     ndim: usize,
-
-    /// A coefficients for dense output
-    aad: Matrix,
-
-    /// C coefficients for dense output
-    ccd: Vector,
-
-    /// D coefficients for dense output
-    dd: Matrix,
 
     /// Dense output values (nstage_dense * ndim)
     d: Vec<Vector>,
@@ -38,9 +29,6 @@ impl ErkDenseOut {
             Method::DoPri5 => ErkDenseOut {
                 method,
                 ndim,
-                aad: Matrix::new(0, 0),
-                ccd: Vector::new(0),
-                dd: Matrix::from(&DORMAND_PRINCE_5_D),
                 d: vec![Vector::new(ndim); 5],
                 kd: Vec::new(),
                 yd: Vector::new(0),
@@ -48,9 +36,6 @@ impl ErkDenseOut {
             Method::DoPri8 => ErkDenseOut {
                 method,
                 ndim,
-                aad: Matrix::from(&DORMAND_PRINCE_8_AD),
-                ccd: Vector::from(&DORMAND_PRINCE_8_CD),
-                dd: Matrix::from(&DORMAND_PRINCE_8_D),
                 d: vec![Vector::new(ndim); 8],
                 kd: vec![Vector::new(ndim); 3],
                 yd: Vector::new(ndim),
@@ -58,9 +43,6 @@ impl ErkDenseOut {
             _ => ErkDenseOut {
                 method,
                 ndim,
-                aad: Matrix::new(0, 0),
-                ccd: Vector::new(0),
-                dd: Matrix::new(0, 0),
                 d: Vec::new(),
                 kd: Vec::new(),
                 yd: Vector::new(0),
@@ -85,6 +67,7 @@ impl ErkDenseOut {
     {
         match self.method {
             Method::DoPri5 => {
+                let dd = &DORMAND_PRINCE_5_D;
                 for m in 0..self.ndim {
                     let y_diff = w[m] - y[m];
                     let b_spl = h * k[0][m] - y_diff;
@@ -92,60 +75,64 @@ impl ErkDenseOut {
                     self.d[1][m] = y_diff;
                     self.d[2][m] = b_spl;
                     self.d[3][m] = y_diff - h * k[6][m] - b_spl;
-                    self.d[4][m] = self.dd.get(0, 0) * k[0][m]
-                        + self.dd.get(0, 2) * k[2][m]
-                        + self.dd.get(0, 3) * k[3][m]
-                        + self.dd.get(0, 4) * k[4][m]
-                        + self.dd.get(0, 5) * k[5][m]
-                        + self.dd.get(0, 6) * k[6][m];
+                    self.d[4][m] = dd[0][0] * k[0][m]
+                        + dd[0][2] * k[2][m]
+                        + dd[0][3] * k[3][m]
+                        + dd[0][4] * k[4][m]
+                        + dd[0][5] * k[5][m]
+                        + dd[0][6] * k[6][m];
                     self.d[4][m] *= h;
                 }
                 Ok(0)
             }
             Method::DoPri8 => {
+                let aad = &DORMAND_PRINCE_8_AD;
+                let ccd = &DORMAND_PRINCE_8_CD;
+                let dd = &DORMAND_PRINCE_8_D;
+
                 // first function evaluation
                 for m in 0..self.ndim {
                     self.yd[m] = y[m]
-                        + h * (self.aad.get(0, 0) * k[0][m]
-                            + self.aad.get(0, 6) * k[6][m]
-                            + self.aad.get(0, 7) * k[7][m]
-                            + self.aad.get(0, 8) * k[8][m]
-                            + self.aad.get(0, 9) * k[9][m]
-                            + self.aad.get(0, 10) * k[10][m]
-                            + self.aad.get(0, 11) * k[11][m]
-                            + self.aad.get(0, 12) * k[11][m]);
+                        + h * (aad[0][0] * k[0][m]
+                            + aad[0][6] * k[6][m]
+                            + aad[0][7] * k[7][m]
+                            + aad[0][8] * k[8][m]
+                            + aad[0][9] * k[9][m]
+                            + aad[0][10] * k[10][m]
+                            + aad[0][11] * k[11][m]
+                            + aad[0][12] * k[11][m]);
                 }
-                let u = x + self.ccd[0] * h;
+                let u = x + ccd[0] * h;
                 (system.function)(&mut self.kd[0], u, &self.yd, args)?;
 
                 // second function evaluation
                 for m in 0..self.ndim {
                     self.yd[m] = y[m]
-                        + h * (self.aad.get(1, 0) * k[0][m]
-                            + self.aad.get(1, 5) * k[5][m]
-                            + self.aad.get(1, 6) * k[6][m]
-                            + self.aad.get(1, 7) * k[7][m]
-                            + self.aad.get(1, 10) * k[10][m]
-                            + self.aad.get(1, 11) * k[11][m]
-                            + self.aad.get(1, 12) * k[11][m]
-                            + self.aad.get(1, 13) * self.kd[0][m]);
+                        + h * (aad[1][0] * k[0][m]
+                            + aad[1][5] * k[5][m]
+                            + aad[1][6] * k[6][m]
+                            + aad[1][7] * k[7][m]
+                            + aad[1][10] * k[10][m]
+                            + aad[1][11] * k[11][m]
+                            + aad[1][12] * k[11][m]
+                            + aad[1][13] * self.kd[0][m]);
                 }
-                let u = x + self.ccd[1] * h;
+                let u = x + ccd[1] * h;
                 (system.function)(&mut self.kd[1], u, &self.yd, args)?;
 
                 // next third function evaluation
                 for m in 0..self.ndim {
                     self.yd[m] = y[m]
-                        + h * (self.aad.get(2, 0) * k[0][m]
-                            + self.aad.get(2, 5) * k[5][m]
-                            + self.aad.get(2, 6) * k[6][m]
-                            + self.aad.get(2, 7) * k[7][m]
-                            + self.aad.get(2, 8) * k[8][m]
-                            + self.aad.get(2, 12) * k[11][m]
-                            + self.aad.get(2, 13) * self.kd[0][m]
-                            + self.aad.get(2, 14) * self.kd[1][m]);
+                        + h * (aad[2][0] * k[0][m]
+                            + aad[2][5] * k[5][m]
+                            + aad[2][6] * k[6][m]
+                            + aad[2][7] * k[7][m]
+                            + aad[2][8] * k[8][m]
+                            + aad[2][12] * k[11][m]
+                            + aad[2][13] * self.kd[0][m]
+                            + aad[2][14] * self.kd[1][m]);
                 }
-                let u = x + self.ccd[2] * h;
+                let u = x + ccd[2] * h;
                 (system.function)(&mut self.kd[2], u, &self.yd, args)?;
 
                 // final results
@@ -157,57 +144,57 @@ impl ErkDenseOut {
                     self.d[2][m] = b_spl;
                     self.d[3][m] = y_diff - h * k[11][m] - b_spl;
                     self.d[4][m] = h
-                        * (self.dd.get(0, 0) * k[0][m]
-                            + self.dd.get(0, 5) * k[5][m]
-                            + self.dd.get(0, 6) * k[6][m]
-                            + self.dd.get(0, 7) * k[7][m]
-                            + self.dd.get(0, 8) * k[8][m]
-                            + self.dd.get(0, 9) * k[9][m]
-                            + self.dd.get(0, 10) * k[10][m]
-                            + self.dd.get(0, 11) * k[11][m]
-                            + self.dd.get(0, 12) * k[11][m]
-                            + self.dd.get(0, 13) * self.kd[0][m]
-                            + self.dd.get(0, 14) * self.kd[1][m]
-                            + self.dd.get(0, 15) * self.kd[2][m]);
+                        * (dd[0][0] * k[0][m]
+                            + dd[0][5] * k[5][m]
+                            + dd[0][6] * k[6][m]
+                            + dd[0][7] * k[7][m]
+                            + dd[0][8] * k[8][m]
+                            + dd[0][9] * k[9][m]
+                            + dd[0][10] * k[10][m]
+                            + dd[0][11] * k[11][m]
+                            + dd[0][12] * k[11][m]
+                            + dd[0][13] * self.kd[0][m]
+                            + dd[0][14] * self.kd[1][m]
+                            + dd[0][15] * self.kd[2][m]);
                     self.d[5][m] = h
-                        * (self.dd.get(1, 0) * k[0][m]
-                            + self.dd.get(1, 5) * k[5][m]
-                            + self.dd.get(1, 6) * k[6][m]
-                            + self.dd.get(1, 7) * k[7][m]
-                            + self.dd.get(1, 8) * k[8][m]
-                            + self.dd.get(1, 9) * k[9][m]
-                            + self.dd.get(1, 10) * k[10][m]
-                            + self.dd.get(1, 11) * k[11][m]
-                            + self.dd.get(1, 12) * k[11][m]
-                            + self.dd.get(1, 13) * self.kd[0][m]
-                            + self.dd.get(1, 14) * self.kd[1][m]
-                            + self.dd.get(1, 15) * self.kd[2][m]);
+                        * (dd[1][0] * k[0][m]
+                            + dd[1][5] * k[5][m]
+                            + dd[1][6] * k[6][m]
+                            + dd[1][7] * k[7][m]
+                            + dd[1][8] * k[8][m]
+                            + dd[1][9] * k[9][m]
+                            + dd[1][10] * k[10][m]
+                            + dd[1][11] * k[11][m]
+                            + dd[1][12] * k[11][m]
+                            + dd[1][13] * self.kd[0][m]
+                            + dd[1][14] * self.kd[1][m]
+                            + dd[1][15] * self.kd[2][m]);
                     self.d[6][m] = h
-                        * (self.dd.get(2, 0) * k[0][m]
-                            + self.dd.get(2, 5) * k[5][m]
-                            + self.dd.get(2, 6) * k[6][m]
-                            + self.dd.get(2, 7) * k[7][m]
-                            + self.dd.get(2, 8) * k[8][m]
-                            + self.dd.get(2, 9) * k[9][m]
-                            + self.dd.get(2, 10) * k[10][m]
-                            + self.dd.get(2, 11) * k[11][m]
-                            + self.dd.get(2, 12) * k[11][m]
-                            + self.dd.get(2, 13) * self.kd[0][m]
-                            + self.dd.get(2, 14) * self.kd[1][m]
-                            + self.dd.get(2, 15) * self.kd[2][m]);
+                        * (dd[2][0] * k[0][m]
+                            + dd[2][5] * k[5][m]
+                            + dd[2][6] * k[6][m]
+                            + dd[2][7] * k[7][m]
+                            + dd[2][8] * k[8][m]
+                            + dd[2][9] * k[9][m]
+                            + dd[2][10] * k[10][m]
+                            + dd[2][11] * k[11][m]
+                            + dd[2][12] * k[11][m]
+                            + dd[2][13] * self.kd[0][m]
+                            + dd[2][14] * self.kd[1][m]
+                            + dd[2][15] * self.kd[2][m]);
                     self.d[7][m] = h
-                        * (self.dd.get(3, 0) * k[0][m]
-                            + self.dd.get(3, 5) * k[5][m]
-                            + self.dd.get(3, 6) * k[6][m]
-                            + self.dd.get(3, 7) * k[7][m]
-                            + self.dd.get(3, 8) * k[8][m]
-                            + self.dd.get(3, 9) * k[9][m]
-                            + self.dd.get(3, 10) * k[10][m]
-                            + self.dd.get(3, 11) * k[11][m]
-                            + self.dd.get(3, 12) * k[11][m]
-                            + self.dd.get(3, 13) * self.kd[0][m]
-                            + self.dd.get(3, 14) * self.kd[1][m]
-                            + self.dd.get(3, 15) * self.kd[2][m]);
+                        * (dd[3][0] * k[0][m]
+                            + dd[3][5] * k[5][m]
+                            + dd[3][6] * k[6][m]
+                            + dd[3][7] * k[7][m]
+                            + dd[3][8] * k[8][m]
+                            + dd[3][9] * k[9][m]
+                            + dd[3][10] * k[10][m]
+                            + dd[3][11] * k[11][m]
+                            + dd[3][12] * k[11][m]
+                            + dd[3][13] * self.kd[0][m]
+                            + dd[3][14] * self.kd[1][m]
+                            + dd[3][15] * self.kd[2][m]);
                 }
                 Ok(3)
             }

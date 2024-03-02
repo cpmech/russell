0db7d7725a2efb4502b10a75d59ef2f739b57872
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-03-02 12:10:00 +1000

    [ode] Improve test script

--- russell_ode/zscripts/compare-test-results.bash
+++ russell_ode/zscripts/compare-test-results.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# the first argument is 0 or 1 to run with --features intel_mkl
+INTEL_MKL=${1:-""}
+
+# the second argument is 0 or 1 to save the results to the data dir (and not compare them)
+SAVE_DATA=${2:-""}
+
 TESTS="
 test_dopri5_arenstorf_debug \
 test_dopri5_arenstorf \
@@ -22,8 +28,17 @@ for test in $TESTS; do
     temporary="/tmp/${test/test_/russell_}.txt"
     correct="data/${test/test_/russell_}.txt"
 
+    if [ "${SAVE_DATA}" = "1" ]; then
+        temporary=$correct
+    fi
+
     # run the test
-    cargo test --test $test -- --nocapture --quiet > $temporary
+    if [ "${INTEL_MKL}" = "1" ]; then
+        echo "running with:  --features russell_lab/intel_mkl"
+        cargo test --test $test --features russell_lab/intel_mkl -- --nocapture --quiet > $temporary
+    else
+        cargo test --test $test -- --nocapture --quiet > $temporary
+    fi
 
     # delete the last two lines (blank line, the cargo output, and a dot)
     sed -i '$d' $temporary
@@ -31,8 +46,14 @@ for test in $TESTS; do
     sed -i '$d' $temporary
 
     # check the difference
-    res=`diff -u $temporary $correct`
-    sum="${sum}${res}"
+    if [ ! "${SAVE_DATA}" = "1" ]; then
+        res=`diff -u $temporary $correct`
+        sum="${sum}${res}"
+    fi
 done
 
-echo "this should be empty: [${sum}]"
+if [ "${SAVE_DATA}" = "1" ]; then
+    echo "results saved to the data dir"
+else
+    echo "this should be empty: [${sum}]"
+fi

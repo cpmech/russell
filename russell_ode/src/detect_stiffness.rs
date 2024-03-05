d228f9372c2d9f9021bd5809ba4d21ddcb5a9341
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-03-05 18:42:53 +1000

    [ode] Add test

--- russell_ode/src/detect_stiffness.rs
+++ russell_ode/src/detect_stiffness.rs
@@ -24,3 +24,45 @@ pub(crate) fn detect_stiffness(work: &mut Workspace, params: &Params) -> Result<
     }
     Ok(())
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::detect_stiffness;
+    use crate::{Method, Params, Workspace};
+
+    #[test]
+    fn detect_stiffness_works() {
+        let mut params = Params::new(Method::DoPri5);
+        let mut work = Workspace::new(Method::DoPri5);
+
+        params.stiffness.skip_first_n_accepted_step = 10;
+        detect_stiffness(&mut work, &params).unwrap();
+        assert_eq!(work.stiff_detected, false);
+
+        work.bench.n_accepted = params.stiffness.skip_first_n_accepted_step + 1;
+        work.stiff_h_times_lambda = 3.25 + 0.01; // DoPri5
+        work.stiff_n_detection_yes = params.stiffness.ratified_after_nstep - 1; // will add 1
+        assert_eq!(detect_stiffness(&mut work, &params).err(), Some("stiffness detected"));
+
+        params.stiffness.stop_with_error = false;
+        work.stiff_n_detection_yes = params.stiffness.ratified_after_nstep - 1; // will add 1
+        detect_stiffness(&mut work, &params).unwrap();
+        assert_eq!(work.stiff_detected, true);
+        assert_eq!(work.stiff_n_detection_no, 0);
+        assert_eq!(work.stiff_n_detection_yes, params.stiffness.ratified_after_nstep);
+
+        work.stiff_h_times_lambda = 3.25 - 0.01; // DoPri5
+        detect_stiffness(&mut work, &params).unwrap();
+        assert_eq!(work.stiff_detected, false);
+        assert_eq!(work.stiff_n_detection_no, 1);
+        assert_eq!(work.stiff_n_detection_yes, params.stiffness.ratified_after_nstep);
+
+        work.stiff_n_detection_no = params.stiffness.ignored_after_nstep - 1; // will add 1
+        detect_stiffness(&mut work, &params).unwrap();
+        assert_eq!(work.stiff_detected, false);
+        assert_eq!(work.stiff_n_detection_no, params.stiffness.ignored_after_nstep);
+        assert_eq!(work.stiff_n_detection_yes, 0);
+    }
+}

9e3d8b4e038e71a5c7f6b11f9fa3b0d359358bd5
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-03-25 22:03:50 +1000

    [sparse] Impl access to nanoseconds spent on solver

--- russell_sparse/src/complex_lin_solver.rs
+++ russell_sparse/src/complex_lin_solver.rs
@@ -52,6 +52,15 @@ pub trait ComplexLinSolTrait {
 
     /// Updates the stats structure (should be called after solve)
     fn update_stats(&self, stats: &mut StatsLinSol);
+
+    /// Returns the nanoseconds spent on initialize
+    fn get_ns_init(&self) -> u128;
+
+    /// Returns the nanoseconds spent on factorize
+    fn get_ns_fact(&self) -> u128;
+
+    /// Returns the nanoseconds spent on solve
+    fn get_ns_solve(&self) -> u128;
 }
 
 /// Unifies the access to linear system solvers
--- russell_sparse/src/complex_solver_umfpack.rs
+++ russell_sparse/src/complex_solver_umfpack.rs
@@ -401,6 +401,21 @@ impl ComplexLinSolTrait for ComplexSolverUMFPACK {
         stats.time_nanoseconds.factorize = self.time_factorize_ns;
         stats.time_nanoseconds.solve = self.time_solve_ns;
     }
+
+    /// Returns the nanoseconds spent on initialize
+    fn get_ns_init(&self) -> u128 {
+        self.time_initialize_ns
+    }
+
+    /// Returns the nanoseconds spent on factorize
+    fn get_ns_fact(&self) -> u128 {
+        self.time_factorize_ns
+    }
+
+    /// Returns the nanoseconds spent on solve
+    fn get_ns_solve(&self) -> u128 {
+        self.time_solve_ns
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

ea069b09bd19592b15ca9145897ec13e21f82855
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-03-25 14:18:00 +1000

    [sparse] Remove CSR write matrix. Fix CSC write matrix to handle symmetry

--- russell_sparse/src/write_matrix_market.rs
+++ russell_sparse/src/write_matrix_market.rs
@@ -1,4 +1,4 @@
-use super::{CscMatrix, CsrMatrix, Sym};
+use super::{CscMatrix, Sym};
 use crate::StrError;
 use std::ffi::OsStr;
 use std::fmt::Write;
@@ -14,8 +14,13 @@ impl CscMatrix {
     /// * `full_path` -- may be a String, &str, or Path
     /// * `vismatrix` -- generate a SMAT file for Vismatrix instead of a MatrixMarket
     ///
-    /// **Note:** The vismatrix format is is similar to the MatrixMarket format
-    /// without the header, and the indices start at zero.
+    /// # Notes
+    ///
+    /// 1. The vismatrix format is is similar to the MatrixMarket format
+    ///    without the header, and the indices start at zero.
+    /// 2. If the matrix is symmetric, then:
+    ///     * Only the lower triangle + diagonal will be written (standard MatrixMarket format)
+    ///     * For vismatrix, the full matrix will be written
     ///
     /// # References
     ///
@@ -28,9 +33,6 @@ impl CscMatrix {
         // output buffer
         let mut buffer = String::new();
 
-        // handle one-based indexing
-        let d = if vismatrix { 0 } else { 1 };
-
         // write header
         if !vismatrix {
             if self.symmetric == Sym::No {
@@ -40,80 +42,72 @@ impl CscMatrix {
             }
         }
 
-        // write dimensions
-        let nnz = self.col_pointers[self.ncol] as usize;
-        write!(&mut buffer, "{} {} {}\n", self.nrow, self.ncol, nnz).unwrap();
-
-        // write triplets
-        for j in 0..self.ncol {
-            for p in self.col_pointers[j]..self.col_pointers[j + 1] {
-                let i = self.row_indices[p as usize] as usize;
-                let aij = self.values[p as usize];
-                write!(&mut buffer, "{} {} {:?}\n", i + d, j + d, aij).unwrap();
+        // compute the number of non-zeros to be written
+        let nnz = if vismatrix && (self.symmetric == Sym::YesLower || self.symmetric == Sym::YesUpper) {
+            // will mirror off-diagonal elements
+            let mut count = 0;
+            for j in 0..self.ncol {
+                for p in self.col_pointers[j]..self.col_pointers[j + 1] {
+                    let i = self.row_indices[p as usize] as usize;
+                    count += 1;
+                    if i != j {
+                        count += 1;
+                    }
+                }
             }
-        }
-
-        // create directory
-        let path = Path::new(full_path);
-        if let Some(p) = path.parent() {
-            fs::create_dir_all(p).map_err(|_| "cannot create directory")?;
-        }
-
-        // write file
-        let mut file = File::create(path).map_err(|_| "cannot create file")?;
-        file.write_all(buffer.as_bytes()).map_err(|_| "cannot write file")?;
-
-        // force sync
-        file.sync_all().map_err(|_| "cannot sync file")?;
-        Ok(())
-    }
-}
-
-impl CsrMatrix {
-    /// Writes a MatrixMarket file
-    ///
-    /// # Input
-    ///
-    /// * `full_path` -- may be a String, &str, or Path
-    /// * `vismatrix` -- generate a SMAT file for Vismatrix instead of a MatrixMarket
-    ///
-    /// **Note:** The vismatrix format is is similar to the MatrixMarket format
-    /// without the header, and the indices start at zero.
-    ///
-    /// # References
-    ///
-    /// * MatrixMarket: <https://math.nist.gov/MatrixMarket/formats.html>
-    /// * Vismatrix: <https://github.com/cpmech/vismatrix>
-    pub fn write_matrix_market<P>(&self, full_path: &P, vismatrix: bool) -> Result<(), StrError>
-    where
-        P: AsRef<OsStr> + ?Sized,
-    {
-        // output buffer
-        let mut buffer = String::new();
-
-        // handle one-based indexing
-        let d = if vismatrix { 0 } else { 1 };
-
-        // write header
-        if !vismatrix {
-            if self.symmetric == Sym::No {
-                write!(&mut buffer, "%%MatrixMarket matrix coordinate real general\n").unwrap();
-            } else {
-                write!(&mut buffer, "%%MatrixMarket matrix coordinate real symmetric\n").unwrap();
+            count
+        } else if !vismatrix && self.symmetric == Sym::YesFull {
+            // will consider the lower-triangle only
+            let mut count = 0;
+            for j in 0..self.ncol {
+                for p in self.col_pointers[j]..self.col_pointers[j + 1] {
+                    let i = self.row_indices[p as usize] as usize;
+                    if i >= j {
+                        count += 1;
+                    }
+                }
             }
-        }
+            count
+        } else {
+            // will use the default number of non-zeros
+            self.col_pointers[self.ncol] as usize
+        };
 
         // write dimensions
-        let nnz = self.row_pointers[self.nrow] as usize;
         write!(&mut buffer, "{} {} {}\n", self.nrow, self.ncol, nnz).unwrap();
 
-        // write triplets
-        for i in 0..self.nrow {
-            for p in self.row_pointers[i]..self.row_pointers[i + 1] {
-                let j = self.col_indices[p as usize] as usize;
-                let aij = self.values[p as usize];
-                println!("{} {} {:?}", i, j, aij);
-                write!(&mut buffer, "{} {} {:?}\n", i + d, j + d, aij).unwrap();
+        // write data
+        if vismatrix {
+            for j in 0..self.ncol {
+                for p in self.col_pointers[j]..self.col_pointers[j + 1] {
+                    let i = self.row_indices[p as usize] as usize;
+                    let aij = self.values[p as usize];
+                    write!(&mut buffer, "{} {} {:?}\n", i, j, aij).unwrap();
+                    if self.symmetric == Sym::YesLower || self.symmetric == Sym::YesUpper {
+                        if i != j {
+                            // mirror off-diagonal elements
+                            write!(&mut buffer, "{} {} {:?}\n", j, i, aij).unwrap();
+                        }
+                    }
+                }
+            }
+        } else {
+            for j in 0..self.ncol {
+                for p in self.col_pointers[j]..self.col_pointers[j + 1] {
+                    let i = self.row_indices[p as usize] as usize;
+                    let aij = self.values[p as usize];
+                    match self.symmetric {
+                        Sym::No => write!(&mut buffer, "{} {} {:?}\n", i + 1, j + 1, aij).unwrap(),
+                        Sym::YesLower => write!(&mut buffer, "{} {} {:?}\n", i + 1, j + 1, aij).unwrap(),
+                        Sym::YesUpper => write!(&mut buffer, "{} {} {:?}\n", j + 1, i + 1, aij).unwrap(),
+                        Sym::YesFull => {
+                            if i >= j {
+                                // consider the lower-triangle only
+                                write!(&mut buffer, "{} {} {:?}\n", i + 1, j + 1, aij).unwrap()
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -168,69 +162,113 @@ mod tests {
              2 5 6.0\n\
              5 5 1.0\n"
         );
+    }
+
+    #[test]
+    fn csc_write_matrix_market_sym_works() {
+        let correct = "%%MatrixMarket matrix coordinate real symmetric\n\
+                       3 3 5\n\
+                       1 1 2.0\n\
+                       2 1 -1.0\n\
+                       2 2 2.0\n\
+                       3 2 -1.0\n\
+                       3 3 2.0\n";
         //  2  -1              2     sym
         // -1   2  -1    =>   -1   2
         //     -1   2             -1   2
         let (_, csc, _, _) = Samples::positive_definite_3x3_lower();
-        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc.mtx";
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_lower.mtx";
         csc.write_matrix_market(full_path, false).unwrap();
         let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
-        assert_eq!(
-            contents,
-            "%%MatrixMarket matrix coordinate real symmetric\n\
-             3 3 5\n\
-             1 1 2.0\n\
-             2 1 -1.0\n\
-             2 2 2.0\n\
-             3 2 -1.0\n\
-             3 3 2.0\n"
-        );
+        assert_eq!(contents, correct);
+        //  2  -1              2  -1
+        // -1   2  -1    =>        2  -1
+        //     -1   2          sym     2
+        let (_, csc, _, _) = Samples::positive_definite_3x3_upper();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_upper.mtx";
+        csc.write_matrix_market(full_path, false).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(contents, correct);
+        //  2  -1
+        // -1   2  -1
+        //     -1   2
+        let (_, csc, _, _) = Samples::positive_definite_3x3_full();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_full.mtx";
+        csc.write_matrix_market(full_path, false).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(contents, correct);
     }
 
     #[test]
-    fn csr_write_matrix_market_works() {
+    fn csc_write_matrix_market_works_vismatrix() {
         //  2  3  .  .  .
         //  3  .  4  .  6
         //  . -1 -3  2  .
         //  .  .  1  .  .
         //  .  4  2  .  1
-        let (_, _, csr, _) = Samples::umfpack_unsymmetric_5x5();
-        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csr.mtx";
-        csr.write_matrix_market(full_path, false).unwrap();
+        let (_, csc, _, _) = Samples::umfpack_unsymmetric_5x5();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc.smat";
+        csc.write_matrix_market(full_path, true).unwrap();
         let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
         assert_eq!(
             contents,
-            "%%MatrixMarket matrix coordinate real general\n\
-             5 5 12\n\
-             1 1 2.0\n\
-             1 2 3.0\n\
-             2 1 3.0\n\
-             2 3 4.0\n\
-             2 5 6.0\n\
-             3 2 -1.0\n\
-             3 3 -3.0\n\
-             3 4 2.0\n\
-             4 3 1.0\n\
-             5 2 4.0\n\
-             5 3 2.0\n\
-             5 5 1.0\n"
+            "5 5 12\n\
+             0 0 2.0\n\
+             1 0 3.0\n\
+             0 1 3.0\n\
+             2 1 -1.0\n\
+             4 1 4.0\n\
+             1 2 4.0\n\
+             2 2 -3.0\n\
+             3 2 1.0\n\
+             4 2 2.0\n\
+             2 3 2.0\n\
+             1 4 6.0\n\
+             4 4 1.0\n"
         );
+    }
+
+    #[test]
+    fn csc_write_matrix_market_sym_works_vismatrix() {
+        let correct = "3 3 7\n\
+                       0 0 2.0\n\
+                       1 0 -1.0\n\
+                       0 1 -1.0\n\
+                       1 1 2.0\n\
+                       2 1 -1.0\n\
+                       1 2 -1.0\n\
+                       2 2 2.0\n";
+        let correct_upper = "3 3 7\n\
+                             0 0 2.0\n\
+                             0 1 -1.0\n\
+                             1 0 -1.0\n\
+                             1 1 2.0\n\
+                             1 2 -1.0\n\
+                             2 1 -1.0\n\
+                             2 2 2.0\n";
         //  2  -1              2     sym
         // -1   2  -1    =>   -1   2
         //     -1   2             -1   2
-        let (_, _, csr, _) = Samples::positive_definite_3x3_lower();
-        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csr.mtx";
-        csr.write_matrix_market(full_path, false).unwrap();
+        let (_, csc, _, _) = Samples::positive_definite_3x3_lower();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_lower.smat";
+        csc.write_matrix_market(full_path, true).unwrap();
         let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
-        assert_eq!(
-            contents,
-            "%%MatrixMarket matrix coordinate real symmetric\n\
-             3 3 5\n\
-             1 1 2.0\n\
-             2 1 -1.0\n\
-             2 2 2.0\n\
-             3 2 -1.0\n\
-             3 3 2.0\n"
-        );
+        assert_eq!(contents, correct);
+        //  2  -1              2  -1
+        // -1   2  -1    =>        2  -1
+        //     -1   2          sym     2
+        let (_, csc, _, _) = Samples::positive_definite_3x3_upper();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_upper.smat";
+        csc.write_matrix_market(full_path, true).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(contents, correct_upper);
+        //  2  -1
+        // -1   2  -1
+        //     -1   2
+        let (_, csc, _, _) = Samples::positive_definite_3x3_full();
+        let full_path = "/tmp/russell_sparse/test_write_matrix_market_csc_sym_full.smat";
+        csc.write_matrix_market(full_path, true).unwrap();
+        let contents = fs::read_to_string(full_path).map_err(|_| "cannot open file").unwrap();
+        assert_eq!(contents, correct);
     }
 }

7521b216549eb9072c5f9ef9726a567465fe4b8d
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-02 14:27:35 +1000

    Impl Bessel J0 and Y0

--- russell_lab/src/math/mod.rs
+++ russell_lab/src/math/mod.rs
@@ -1,8 +1,10 @@
 //! This module implements some mathematical functions, including wrapping C-code
 
+mod bessel_0;
 mod c_functions;
 mod constants;
 mod functions;
+pub use crate::math::bessel_0::*;
 pub use crate::math::c_functions::*;
 pub use crate::math::constants::*;
 pub use crate::math::functions::*;

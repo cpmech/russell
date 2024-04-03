1b06d69184e206c5b0efd7dd5ec47bea223dff02
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-03 15:55:15 +1000

    Add tests

--- russell_lab/tests/test_bessel_functions.rs
+++ russell_lab/tests/test_bessel_functions.rs
@@ -64,3 +64,99 @@ fn test_bessel_functions_big() {
         }
     }
 }
+
+#[test]
+fn test_modified_bessel_functions_sml() {
+    let dat: HashMap<String, Vec<f64>> = read_table(
+        "data/reference/as-9-modbessel-integer-sml.cmp",
+        Some(&["x", "I0", "I1", "I2", "I3", "K0", "K1", "K2", "K3"]),
+    )
+    .unwrap();
+
+    let xx = dat.get("x").unwrap();
+    let i0 = dat.get("I0").unwrap();
+    let i1 = dat.get("I1").unwrap();
+    let i2 = dat.get("I2").unwrap();
+    let i3 = dat.get("I3").unwrap();
+    let k0 = dat.get("K0").unwrap();
+    let k1 = dat.get("K1").unwrap();
+    let k2 = dat.get("K2").unwrap();
+    let k3 = dat.get("K3").unwrap();
+
+    for (i, x) in xx.into_iter().enumerate() {
+        approx_eq(math::bessel_mod_i0(*x), i0[i], 1e-8);
+        approx_eq(math::bessel_mod_i1(*x), i1[i], 1e-7);
+        approx_eq(math::bessel_mod_in(2, *x), i2[i], 1e-7);
+        approx_eq(math::bessel_mod_in(3, *x), i3[i], 1e-7);
+        if i == 0 {
+            assert_eq!(math::bessel_mod_k0(*x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_k1(*x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_kn(2, *x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_kn(3, *x), f64::INFINITY);
+        } else {
+            approx_eq(math::bessel_mod_k0(*x), k0[i], 1e-15);
+            approx_eq(math::bessel_mod_k1(*x), k1[i], 1e-15);
+            approx_eq(math::bessel_mod_kn(2, *x), k2[i], 1e-15);
+            approx_eq(math::bessel_mod_kn(3, *x), k3[i], 1e-14);
+        }
+    }
+}
+
+#[test]
+fn test_modified_bessel_functions_big() {
+    let dat: HashMap<String, Vec<f64>> = read_table(
+        "data/reference/as-9-modbessel-integer-big.cmp",
+        Some(&["x", "I0", "I1", "I2", "I3", "K0", "K1", "K2", "K3"]),
+    )
+    .unwrap();
+
+    let xx = dat.get("x").unwrap();
+    let i0 = dat.get("I0").unwrap();
+    let i1 = dat.get("I1").unwrap();
+    let i2 = dat.get("I2").unwrap();
+    let i3 = dat.get("I3").unwrap();
+    let k0 = dat.get("K0").unwrap();
+    let k1 = dat.get("K1").unwrap();
+    let k2 = dat.get("K2").unwrap();
+    let k3 = dat.get("K3").unwrap();
+
+    for (i, x) in xx.into_iter().enumerate() {
+        approx_eq(math::bessel_mod_i0(*x), i0[i], 1e-6);
+        approx_eq(math::bessel_mod_i1(*x), i1[i], 1e-6);
+        approx_eq(math::bessel_mod_in(2, *x), i2[i], 1e-6);
+        approx_eq(math::bessel_mod_in(3, *x), i3[i], 1e-6);
+        if i == 0 {
+            assert_eq!(math::bessel_mod_k0(*x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_k1(*x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_kn(2, *x), f64::INFINITY);
+            assert_eq!(math::bessel_mod_kn(3, *x), f64::INFINITY);
+        } else {
+            approx_eq(math::bessel_mod_k0(*x), k0[i], 1e-15);
+            approx_eq(math::bessel_mod_k1(*x), k1[i], 1e-14);
+            approx_eq(math::bessel_mod_kn(2, *x), k2[i], 1e-13);
+            approx_eq(math::bessel_mod_kn(3, *x), k3[i], 1e-12);
+        }
+    }
+}
+
+#[test]
+fn test_modified_bessel_functions_neg() {
+    let dat: HashMap<String, Vec<f64>> = read_table(
+        "data/reference/as-9-modbessel-integer-neg.cmp",
+        Some(&["x", "I0", "I1", "I2", "I3"]),
+    )
+    .unwrap();
+
+    let xx = dat.get("x").unwrap();
+    let i0 = dat.get("I0").unwrap();
+    let i1 = dat.get("I1").unwrap();
+    let i2 = dat.get("I2").unwrap();
+    let i3 = dat.get("I3").unwrap();
+
+    for (i, x) in xx.into_iter().enumerate() {
+        approx_eq(math::bessel_mod_i0(*x), i0[i], 1e-12);
+        approx_eq(math::bessel_mod_i1(*x), i1[i], 1e-12);
+        approx_eq(math::bessel_mod_in(2, *x), i2[i], 1e-11);
+        approx_eq(math::bessel_mod_in(3, *x), i3[i], 1e-12);
+    }
+}

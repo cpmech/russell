55e022b959d7e59e54bb0b9fb8f03bfcecb78229
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-09 17:11:54 +1000

    Impl fdm5_jacobian

--- russell_lab/src/algo/mod.rs
+++ russell_lab/src/algo/mod.rs
@@ -1,4 +1,6 @@
 //! This module implements algorithms built from base, math, and vector-matrix routines
 
+mod fdm5_jacobian;
 mod interp_lagrange;
+pub use crate::algo::fdm5_jacobian::*;
 pub use crate::algo::interp_lagrange::*;

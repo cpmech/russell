f9a98c66b6d653c45a5ba8f6c7ba9b6d8a02ca12
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-14 21:34:13 +1000

    Rename bracket.rs file

--- russell_lab/src/algo/mod.rs
+++ russell_lab/src/algo/mod.rs
@@ -1,9 +1,9 @@
 //! This module implements algorithms built from base, math, and vector-matrix routines
 
+mod bracket;
 mod interp_lagrange;
 mod num_jacobian;
-mod root_solver_bracket;
 mod testing;
+pub use crate::algo::bracket::*;
 pub use crate::algo::interp_lagrange::*;
 pub use crate::algo::num_jacobian::*;
-pub use crate::algo::root_solver_bracket::*;

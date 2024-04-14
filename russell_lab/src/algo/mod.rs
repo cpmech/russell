1dcf0d78521a07fabc60ea0475a796ee99c6e0ba
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-14 21:36:46 +1000

    Rename bracket_min file

--- russell_lab/src/algo/mod.rs
+++ russell_lab/src/algo/mod.rs
@@ -1,9 +1,9 @@
 //! This module implements algorithms built from base, math, and vector-matrix routines
 
-mod bracket;
+mod bracket_min;
 mod interp_lagrange;
 mod num_jacobian;
 mod testing;
-pub use crate::algo::bracket::*;
+pub use crate::algo::bracket_min::*;
 pub use crate::algo::interp_lagrange::*;
 pub use crate::algo::num_jacobian::*;

dacfe96c85c2104c8813dac7ffdd25a1852cd051
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-17 15:47:47 +1000

    Rename min_solver.rs file

--- russell_lab/src/algo/mod.rs
+++ russell_lab/src/algo/mod.rs
@@ -3,7 +3,7 @@
 mod common;
 mod interp_lagrange;
 mod min_bracketing;
-mod min_solver_brent;
+mod min_solver;
 mod num_jacobian;
 mod quadrature;
 mod root_solver_brent;
@@ -11,7 +11,7 @@ mod testing;
 pub use crate::algo::common::*;
 pub use crate::algo::interp_lagrange::*;
 pub use crate::algo::min_bracketing::*;
-pub use crate::algo::min_solver_brent::*;
+pub use crate::algo::min_solver::*;
 pub use crate::algo::num_jacobian::*;
 pub use crate::algo::quadrature::*;
 pub use crate::algo::root_solver_brent::*;

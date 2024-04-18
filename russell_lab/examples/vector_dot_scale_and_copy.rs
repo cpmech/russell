553a76662309b9636d14869ed3202878120e8f62
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-19 09:04:11 +1000

    Improve examples (vector)

--- russell_lab/examples/vector_dot_scale_and_copy.rs
+++ russell_lab/examples/vector_dot_scale_and_copy.rs
@@ -1,5 +1,19 @@
 use russell_lab::*;
 
 fn main() -> Result<(), StrError> {
+    // scale
+    let mut u = Vector::from(&[1.0, 2.0, 3.0]);
+    vec_scale(&mut u, 0.5);
+    let correct = "┌     ┐\n\
+                   │ 0.5 │\n\
+                   │   1 │\n\
+                   │ 1.5 │\n\
+                   └     ┘";
+    assert_eq!(format!("{}", u), correct);
+
+    // copy
+    let mut v = Vector::from(&[-1.0, -2.0, -3.0]);
+    vec_copy(&mut v, &u)?;
+    assert_eq!(format!("{}", v), correct);
     Ok(())
 }

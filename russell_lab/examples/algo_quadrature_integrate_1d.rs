e8ce392d3ae8a8877a7f98742e5b93867c0d243e
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-21 10:02:05 +1000

    Improve example

--- russell_lab/examples/algo_quadrature_integrate_1d.rs
+++ russell_lab/examples/algo_quadrature_integrate_1d.rs
@@ -1,6 +1,24 @@
-use russell_lab::*;
+use russell_lab::algo::Quadrature;
+use russell_lab::math::{elliptic_e, PI};
+use russell_lab::{approx_eq, StrError};
 
 fn main() -> Result<(), StrError> {
-    println!("TODO: EXAMPLE");
+    //  Determine the perimeter of an ellipse of length 2 and width 1
+    let mut quad = Quadrature::new();
+    let args = &mut 0;
+    let (perimeter, stats) = quad.integrate(0.0, 2.0 * PI, args, |theta, _| {
+        Ok(f64::sqrt(
+            0.25 * f64::powi(f64::sin(theta), 2) + f64::powi(f64::cos(theta), 2),
+        ))
+    })?;
+    println!("\nperimeter = {}", perimeter);
+    println!("\n{}", stats);
+
+    // complete elliptic integral of the second kind E(0.75)
+    let ee = elliptic_e(PI / 2.0, 0.75)?;
+
+    // reference solution
+    let ref_perimeter = 4.0 * ee;
+    approx_eq(perimeter, ref_perimeter, 1e-14);
     Ok(())
 }

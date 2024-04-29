898c77f41dde59f32e1e708979524c715b9c20be
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-29 20:53:34 +1000

    [wip] Prepare Docker file for Rocky Linux

--- zscripts/docker-build-image.bash
+++ zscripts/docker-build-image.bash
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# the first argument is "rocky" to build Rocky linux instead of Ubuntu
+ROCKY=${1:-""}
+
 # image name
 NAME="cpmech/russell"
+DKFILE="Dockerfile.Ubuntu"
+if [ "${ROCKY}" = "rocky" ]; then
+    NAME="cpmech/russell_rocky"
+    DKFILE="Dockerfile.Rocky"
+fi
 
 # build Docker image
-docker build --no-cache -t $NAME .
+docker build --no-cache -f $DKFILE -t $NAME .
 
 echo
 echo

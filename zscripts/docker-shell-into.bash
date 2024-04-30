737278d2cf0abfcf9d2ddd6340318c9d54822bc1
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-04-30 19:08:01 +1000

    Improve Dockerfiles

--- zscripts/docker-shell-into.bash
+++ zscripts/docker-shell-into.bash
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# the first argument is "rocky" to use Rocky linux instead of Ubuntu
-ROCKY=${1:-""}
+# the first argument is the distro: "arch" or "rocky"
+# the second argument is "1" to enable MUMPS
+DISTRO=${1:-""}
+WITH_MUMPS=${2:-""}
 
 # image name
-NAME="cpmech/russell"
-if [ "${ROCKY}" = "rocky" ]; then
+NAME="cpmech/russell_ubuntu"
+if [ "${DISTRO}" = "arch" ]; then
+    NAME="cpmech/russell_arch"
+fi
+if [ "${DISTRO}" = "rocky" ]; then
     NAME="cpmech/russell_rocky"
+fi
+if [ "${WITH_MUMPS}" = "1" ]; then
+    NAME="${NAME}_mumps"
 fi
 
 VERSION="latest"

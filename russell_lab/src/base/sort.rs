6a6064150ae6a5896856e6df88fd1ea8f3e78712
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-05 16:03:19 +1000

    Impl argsort_f64

--- russell_lab/src/base/sort.rs
+++ russell_lab/src/base/sort.rs
@@ -81,11 +81,33 @@ where
     }
 }
 
+/// Returns the indices that would sort an array of f64
+///
+/// **Note:** NaN values are handled such that they will be placed at the end of the array.
+pub fn argsort_f64(data: &[f64]) -> Vec<usize> {
+    // create a vector of (index, value) pairs
+    let mut indexed_data: Vec<_> = data.iter().enumerate().collect();
+
+    // sort the pairs by the value, handling NaN values
+    indexed_data.sort_by(|a, b| {
+        match (a.1.is_nan(), b.1.is_nan()) {
+            (true, true) => std::cmp::Ordering::Equal,    // both are NaN
+            (true, false) => std::cmp::Ordering::Greater, // place NaN after
+            (false, true) => std::cmp::Ordering::Less,    // place NaN before
+            _ => a.1.partial_cmp(b.1).unwrap(),           // compare normally
+        }
+    });
+
+    // extract and return the indices from the sorted pairs
+    let sorted_indices = indexed_data.iter().map(|(idx, _)| *idx).collect();
+    sorted_indices
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
-    use super::{sort2, sort3, sort4};
+    use super::{argsort_f64, sort2, sort3, sort4};
 
     #[test]
     fn sort2_works() {
@@ -162,5 +184,14 @@ mod tests {
         let mut x = (4, 3, 2, 1);
         sort4(&mut x);
         assert_eq!(x, (1, 2, 3, 4));
+    }
+
+    #[test]
+    fn argsort_f64_works() {
+        let data = [3.0, 1.0, 4.0, 1.0, 5.0, 9.0, 2.0, 6.0, 5.0, 3.0];
+        assert_eq!(argsort_f64(&data), &[1, 3, 6, 0, 9, 2, 4, 8, 7, 5]);
+
+        let data = [3.0, 1.0, f64::NAN, 8.0, -5.0];
+        assert_eq!(argsort_f64(&data), &[4, 1, 0, 3, 2]);
     }
 }

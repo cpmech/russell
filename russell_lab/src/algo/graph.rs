1d1d763b899296f6920ce50e65af185691b8009f
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-12 16:43:50 +1000

    [wip] Improve Graph

--- russell_lab/src/algo/graph.rs
+++ russell_lab/src/algo/graph.rs
@@ -1,5 +1,7 @@
-use crate::{AsArray2D, StrError};
-use crate::{Matrix, NumMatrix, Vector};
+use crate::AsArray2D;
+use crate::{Matrix, NumMatrix};
+use std::cmp;
+use std::fmt::Write;
 use std::{collections::HashMap, vec};
 
 /// Graph defines a (directed) graph structure
@@ -38,14 +40,17 @@ pub struct Graph {
     /// multiplied by the weight of the edge. Otherwise, the distance is simply the weight of the edge.
     ///
     /// Size: (nnode, nnode)
-    distances: Matrix,
+    dist: Matrix,
 
-    /// Holds the next tree connection
+    /// Holds the next tree connection (next-hop matrix)
     ///
     /// The value of usize::MAX is used to indicate "no connection".
     ///
     /// Size: (nnode, nnode)
     next: NumMatrix<usize>,
+
+    /// Indicates whether the distances are already calculated and next has been initialized
+    ready: bool,
 }
 
 impl Graph {
@@ -66,18 +71,24 @@ impl Graph {
             shares.entry(b).or_insert(Vec::new()).push(e);
         }
         let nnode = shares.len();
+        let mut distances = Matrix::filled(nnode, nnode, f64::MAX);
+        for i in 0..nnode {
+            distances.set(i, i, 0.0);
+        }
         Graph {
             edges: NumMatrix::from(edges),
             weights: vec![1.0; nnode],
             shares,
             coordinates: Vec::new(),
-            distances: Matrix::new(nnode, nnode),
+            dist: distances,
             next: NumMatrix::filled(nnode, nnode, usize::MAX),
+            ready: false,
         }
     }
 
     /// Sets the weight of an edge
     pub fn set_weight(&mut self, edge: usize, value: f64) -> &mut Self {
+        assert!(value >= 0.0, "edge weight must be ≥ 0");
         self.weights[edge] = value;
         self
     }
@@ -99,6 +110,85 @@ impl Graph {
             self.coordinates[node][i] = coordinates[i];
         }
         self
+    }
+
+    /// Calculates the distance matrix and initializes the next-hop matrix
+    fn calc_dist_and_next(&mut self) {
+        let nedge = self.edges.nrow();
+        let nnode = self.shares.len();
+        for e in 0..nedge {
+            // Get edge nodes
+            let (i, j) = (self.edges.get(e, 0), self.edges.get(e, 1));
+
+            // Initialize distance
+            let mut d = 1.0;
+
+            // Compute Euclidean distance if vertex coordinates are provided
+            if self.coordinates.len() == nnode {
+                d = 0.0;
+                let xa = &self.coordinates[i];
+                let xb = &self.coordinates[j];
+                for dim in 0..xa.len() {
+                    d += (xa[dim] - xb[dim]).powi(2);
+                }
+                d = d.sqrt();
+            }
+
+            // Apply edge weight
+            d *= self.weights[e];
+
+            // Update distance and next-hop matrices
+            self.dist.set(i, j, d);
+            self.next.set(i, j, j);
+            assert!(self.dist.get(i, j) >= 0.0, "distance must be ≥ 0");
+        }
+    }
+
+    /// Returns a string representation of the distance matrix
+    pub fn str_dist_matrix(&self) -> String {
+        // handle empty matrix
+        let (nrow, ncol) = self.dist.dims();
+        if nrow == 0 || ncol == 0 {
+            print!("[]");
+        }
+        // find largest width
+        let mut width = 0;
+        let mut buf = String::new();
+        for i in 0..nrow {
+            for j in 0..ncol {
+                let val = self.dist.get(i, j);
+                if val == f64::MAX {
+                    write!(&mut buf, "∞").unwrap();
+                } else {
+                    write!(&mut buf, "{}", val).unwrap();
+                }
+                width = cmp::max(buf.chars().count(), width);
+                buf.clear();
+            }
+        }
+        // draw matrix
+        width += 1;
+        buf.clear();
+        write!(&mut buf, "┌{:1$}┐\n", " ", width * ncol + 1).unwrap();
+        for i in 0..nrow {
+            if i > 0 {
+                write!(&mut buf, " │\n");
+            }
+            for j in 0..ncol {
+                if j == 0 {
+                    write!(&mut buf, "│").unwrap();
+                }
+                let val = self.dist.get(i, j);
+                if val == f64::MAX {
+                    write!(&mut buf, "{:>1$}", "∞", width).unwrap();
+                } else {
+                    write!(&mut buf, "{:>1$}", val, width).unwrap();
+                }
+            }
+        }
+        write!(&mut buf, " │\n").unwrap();
+        write!(&mut buf, "└{:1$}┘", " ", width * ncol + 1).unwrap();
+        buf
     }
 }
 
@@ -134,7 +224,7 @@ mod tests {
         assert_eq!(&graph.weights, &[1.0, 1.0, 1.0, 1.0]);
 
         // check shares
-        let mut entries: Vec<_> = graph.shares.into_iter().collect();
+        let mut entries: Vec<_> = graph.shares.iter().collect();
         entries.sort_by(|a, b| a.0.cmp(&b.0));
         assert_eq!(
             format!("{:?}", entries),
@@ -145,7 +235,15 @@ mod tests {
         assert_eq!(graph.coordinates.len(), 0);
 
         // check distances
-        assert_eq!(graph.distances.dims(), (4, 4));
+        assert_eq!(
+            format!("{}", graph.str_dist_matrix()),
+            "┌         ┐\n\
+             │ 0 ∞ ∞ ∞ │\n\
+             │ ∞ 0 ∞ ∞ │\n\
+             │ ∞ ∞ 0 ∞ │\n\
+             │ ∞ ∞ ∞ 0 │\n\
+             └         ┘"
+        );
 
         // check 'next'
         assert_eq!(graph.next.dims(), (4, 4));

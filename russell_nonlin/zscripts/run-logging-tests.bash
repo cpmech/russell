2ce252c431d87e0af8df2c1fa600bcb72bef6ea4
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-04-22 17:50:42 +1000

    Add test logs

---
+++ russell_nonlin/zscripts/run-logging-tests.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cargo test --features intel_mkl,local_suitesparse --test test_newton_problems -- --test-threads=1 --nocapture > data/logs/test_newton_problems.txt
+cargo test --features intel_mkl,local_suitesparse --test test_newton_problems_auto -- --test-threads=1 --nocapture > data/logs/test_newton_problems_auto.txt

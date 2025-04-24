6b93cf4b44dc720f6b5c70bdd39583767d645a19
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-04-24 11:39:47 +1000

    Improve test

--- russell_nonlin/tests/test_arc_single_eq_with_fold.rs
+++ russell_nonlin/tests/test_arc_single_eq_with_fold.rs
@@ -1,9 +1,5 @@
-#![allow(unused)]
-
-use russell_lab::{array_approx_eq, mat_approx_eq, num_jacobian, Vector};
-use russell_nonlin::{AutoStep, Config, Direction, Method, NoArgs, Samples, Solver, Stop, System};
-use russell_pde::FdmLaplacian2d;
-use russell_sparse::{CooMatrix, Sym};
+use russell_lab::array_approx_eq;
+use russell_nonlin::{AutoStep, Config, Direction, Method, Output, Samples, Solver, Stop};
 
 #[test]
 fn test_arc_single_eq_with_fold() {
@@ -17,6 +13,10 @@ fn test_arc_single_eq_with_fold() {
     // solver
     let mut solver = Solver::new(config, system).unwrap();
 
+    // output data
+    let out = &mut Output::new();
+    out.set_recording(true, &[0], &[0]);
+
     // numerical continuation
     solver
         .solve(
@@ -25,7 +25,29 @@ fn test_arc_single_eq_with_fold() {
             Direction::Pos,
             Stop::Steps(5),
             AutoStep::No(0.5),
-            None,
+            Some(out),
         )
         .unwrap();
+
+    // check results
+    let uu = out.get_u_values(0);
+    let ll = out.get_l_values();
+    let uu_mathematica = &[
+        0.0,
+        0.428095787401572,
+        0.928938368657503,
+        1.42982786403821,
+        1.92613250768946,
+        2.42179932045308,
+    ];
+    let ll_mathematica = &[
+        0.0,
+        0.279010993784976,
+        0.366905391632565,
+        0.342229470016786,
+        0.280658009972863,
+        0.214963176909604,
+    ];
+    array_approx_eq(&uu, uu_mathematica, 1e-6);
+    array_approx_eq(&ll, ll_mathematica, 1e-7);
 }

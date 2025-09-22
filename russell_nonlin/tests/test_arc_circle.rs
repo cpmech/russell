61d61e65fe4d95f2a739b767048e559dff0475ca
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-09-23 07:53:13 +1000

    Add test

--- russell_nonlin/tests/test_arc_circle.rs
+++ russell_nonlin/tests/test_arc_circle.rs
@@ -2,7 +2,7 @@
 
 use plotpy::{linspace, Canvas, Curve, Plot, RayEndpoint};
 use russell_lab::{approx_eq, array_approx_eq, math::SQRT_2};
-use russell_nonlin::{AutoStep, Config, Direction, Status, Method, Output, Samples, Solver, Stop};
+use russell_nonlin::{AutoStep, Config, Direction, Method, Output, Samples, Solver, Status, Stop};
 
 const RADIUS: f64 = SQRT_2;
 
@@ -92,7 +92,7 @@ fn do_plot_stepsizes(name: &str, stepsizes: &[f64]) {
 }
 
 #[test]
-fn test_arc_circle_1() {
+fn test_arc_circle_max_lambda() {
     // system
     let (system, mut state, mut args) = Samples::circle_ul(RADIUS);
 
@@ -143,13 +143,13 @@ fn test_arc_circle_1() {
         let hh = out.get_h_values();
         let duds = out.get_duds_values(0);
         let dlds = out.get_dlds_values();
-        do_plot("test_arc_circle_1", uu, ll, duds, dlds, hh);
-        do_plot_stepsizes("test_arc_circle_1", hh);
+        do_plot("test_arc_circle_max_lambda", uu, ll, duds, dlds, hh);
+        do_plot_stepsizes("test_arc_circle_max_lambda", hh);
     }
 }
 
 #[test]
-fn test_arc_circle_2() {
+fn test_arc_circle_min_lambda() {
     // system
     let (system, mut state, mut args) = Samples::circle_ul(RADIUS);
 
@@ -200,7 +200,121 @@ fn test_arc_circle_2() {
         let hh = out.get_h_values();
         let duds = out.get_duds_values(0);
         let dlds = out.get_dlds_values();
-        do_plot("test_arc_circle_2", uu, ll, duds, dlds, hh);
-        do_plot_stepsizes("test_arc_circle_2", hh);
+        do_plot("test_arc_circle_min_lambda", uu, ll, duds, dlds, hh);
+        do_plot_stepsizes("test_arc_circle_min_lambda", hh);
+    }
+}
+
+#[test]
+fn test_arc_circle_max_u() {
+    // system
+    let (system, mut state, mut args) = Samples::circle_ul(RADIUS);
+
+    // configuration
+    let mut config = Config::new(Method::Arclength);
+    config
+        .set_verbose(true, true, true)
+        .set_hide_timings(true)
+        .set_h_ini(0.3)
+        .set_record_iterations_residuals(true);
+
+    // define solver
+    let mut solver = Solver::new(config, system).unwrap();
+
+    // output
+    let out = &mut Output::new();
+    out.set_recording(true, &[0], &[0]);
+
+    // numerical continuation
+    let status = solver
+        .solve(
+            &mut args,
+            &mut state,
+            Direction::Neg,
+            Stop::MaxCompU(0, 1.3),
+            AutoStep::Yes,
+            Some(out),
+        )
+        .unwrap();
+    assert_eq!(status, Status::Success);
+
+    // check the results
+    let uu = out.get_u_values(0);
+    let ll = out.get_l_values();
+    println!("u final = {}", state.u[0]);
+    println!("λ final = {}, err = {}", state.l, f64::abs(state.l - RADIUS));
+    approx_eq(state.u[0], 1.3, 1e-5);
+    approx_eq(state.u[0] * state.u[0] + state.l * state.l, RADIUS * RADIUS, 1e-10);
+
+    // check stats
+    let stats = solver.get_stats();
+    assert_eq!(stats.n_accepted, 5);
+    assert_eq!(stats.n_rejected, 0);
+    assert_eq!(stats.n_steps, 5);
+
+    // plot
+    if SAVE_FIGURE {
+        let hh = out.get_h_values();
+        let duds = out.get_duds_values(0);
+        let dlds = out.get_dlds_values();
+        do_plot("test_arc_circle_max_u", uu, ll, duds, dlds, hh);
+        do_plot_stepsizes("test_arc_circle_max_u", hh);
+    }
+}
+
+#[test]
+fn test_arc_circle_min_u() {
+    // system
+    let (system, mut state, mut args) = Samples::circle_ul(RADIUS);
+
+    // configuration
+    let mut config = Config::new(Method::Arclength);
+    config
+        .set_verbose(true, true, true)
+        .set_hide_timings(true)
+        .set_h_ini(0.3)
+        .set_record_iterations_residuals(true);
+
+    // define solver
+    let mut solver = Solver::new(config, system).unwrap();
+
+    // output
+    let out = &mut Output::new();
+    out.set_recording(true, &[0], &[0]);
+
+    // numerical continuation
+    let status = solver
+        .solve(
+            &mut args,
+            &mut state,
+            Direction::Pos,
+            Stop::MinCompU(0, 0.4),
+            AutoStep::Yes,
+            Some(out),
+        )
+        .unwrap();
+    assert_eq!(status, Status::Success);
+
+    // check the results
+    let uu = out.get_u_values(0);
+    let ll = out.get_l_values();
+    println!("u final = {}", state.u[0]);
+    println!("λ final = {:.7e}", state.l);
+    assert!(state.u[0] > 0.0 && state.u[0] < 0.4);
+    approx_eq(state.u[0] * state.u[0] + state.l * state.l, RADIUS * RADIUS, 1e-9);
+
+    // check stats
+    let stats = solver.get_stats();
+    assert_eq!(stats.n_accepted, 3);
+    assert_eq!(stats.n_rejected, 0);
+    assert_eq!(stats.n_steps, 3);
+
+    // plot
+    if SAVE_FIGURE {
+        let hh = out.get_h_values();
+        let duds = out.get_duds_values(0);
+        let dlds = out.get_dlds_values();
+        do_plot("test_arc_circle_min_u", uu, ll, duds, dlds, hh);
+        do_plot_stepsizes("test_arc_circle_min_u", hh);
     }
 }

9467e8736722f6b8e835e8359c074ded47370a87
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-09-22 22:08:28 +1000

    Add test

--- russell_nonlin/tests/test_circle.rs
+++ russell_nonlin/tests/test_circle.rs
@@ -2,7 +2,7 @@
 
 use plotpy::{linspace, Canvas, Curve, Plot, RayEndpoint};
 use russell_lab::{approx_eq, array_approx_eq, math::SQRT_2};
-use russell_nonlin::{AutoStep, Config, Direction, Status, Method, Output, Samples, Solver, Stop};
+use russell_nonlin::{AutoStep, Config, Direction, Method, Output, Samples, Solver, Status, Stop};
 
 const RADIUS: f64 = SQRT_2;
 
@@ -101,5 +101,62 @@ fn test_circle_1() {
         let hh = out.get_h_values();
         do_plot("test_circle_1", uu, ll, hh);
         do_plot_stepsizes("test_circle_1", hh);
+    }
+}
+
+#[test]
+fn test_circle_2() {
+    // system
+    let (system, mut state, mut args) = Samples::circle_ul(RADIUS);
+
+    // configuration
+    let mut config = Config::new(Method::Natural);
+    config
+        .set_verbose(true, true, true)
+        .set_hide_timings(true)
+        .set_h_ini(0.3)
+        .set_record_iterations_residuals(true);
+
+    // define solver
+    let mut solver = Solver::new(config, system).unwrap();
+
+    // output
+    let out = &mut Output::new();
+    out.set_recording(true, &[0], &[0]);
+
+    // numerical continuation
+    let status = solver
+        .solve(
+            &mut args,
+            &mut state,
+            Direction::Neg,
+            Stop::MinLambda(0.0),
+            AutoStep::Yes,
+            Some(out),
+        )
+        .unwrap();
+    assert_eq!(status, Status::Success);
+
+    // check the results
+    let uu = out.get_u_values(0);
+    let ll = out.get_l_values();
+    println!("u final = {}", state.u[0]);
+    println!("λ final = {:.7e}", state.l);
+    approx_eq(state.u[0], RADIUS, 1e-12);
+    approx_eq(state.l, 0.0, 1e-14);
+
+    // check stats
+    let stats = solver.get_stats();
+    assert_eq!(stats.n_accepted, 7);
+    assert_eq!(stats.n_rejected, 2);
+    assert_eq!(stats.n_steps, 9);
+
+    // plot
+    if SAVE_FIGURE {
+        let hh = out.get_h_values();
+        let duds = out.get_duds_values(0);
+        let dlds = out.get_dlds_values();
+        do_plot("test_circle_2", uu, ll, hh);
+        do_plot_stepsizes("test_circle_2", hh);
     }
 }

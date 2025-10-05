6097e3eb5a82138efea73eeaa9f729d80146da29
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-10-06 09:00:18 +1000

    [nonlin] Allow specification of norm type in MaxNormU

--- russell_nonlin/src/enums.rs
+++ russell_nonlin/src/enums.rs
@@ -61,8 +61,8 @@ pub enum Stop {
     /// Holds `(index, max_value)`.
     MaxCompU(usize, f64),
 
-    /// Stops when the Euclidean norm of `u` (‖u‖₂) reaches a maximum value.
-    MaxNormU(f64),
+    /// Stops when the norm of `u` reaches a maximum value.
+    MaxNormU(f64, Norm),
 
     /// Stops when lambda reaches a minimum value.
     MinLambda(f64),
@@ -94,7 +94,7 @@ impl Stop {
                     return Err("Stop enum error: MaxCompU value must be greater than the initial u value");
                 }
             }
-            Stop::MaxNormU(norm_u1) => {
+            Stop::MaxNormU(norm_u1, _) => {
                 if *norm_u1 <= 0.0 {
                     return Err("Stop enum error: MaxNormU value must be greater than 0.0");
                 }
@@ -125,7 +125,7 @@ impl Stop {
         match self {
             Stop::MinCompU(_, _) => None,
             Stop::MaxCompU(_, _) => None,
-            Stop::MaxNormU(_) => None,
+            Stop::MaxNormU(_, _) => None,
             Stop::MinLambda(l1) => Some((*l1, true)),
             Stop::MaxLambda(l1) => Some((*l1, false)),
             Stop::Steps(_) => None,
@@ -139,7 +139,7 @@ impl Stop {
         match self {
             Stop::MinCompU(i, u1) => Some((*i, *u1, true)),
             Stop::MaxCompU(i, u1) => Some((*i, *u1, false)),
-            Stop::MaxNormU(_) => None,
+            Stop::MaxNormU(_, _) => None,
             Stop::MinLambda(_) => None,
             Stop::MaxLambda(_) => None,
             Stop::Steps(_) => None,
@@ -151,8 +151,8 @@ impl Stop {
         match self {
             Stop::MinCompU(i, u1) => state.u[*i] < *u1 || f64::abs(state.u[*i] - *u1) < CONFIG_H_MIN,
             Stop::MaxCompU(i, u1) => state.u[*i] > *u1 || f64::abs(*u1 - state.u[*i]) < CONFIG_H_MIN,
-            Stop::MaxNormU(norm_u1) => {
-                let norm_u = vec_norm(&state.u, Norm::Euc);
+            Stop::MaxNormU(norm_u1, norm_type) => {
+                let norm_u = vec_norm(&state.u, *norm_type);
                 norm_u > *norm_u1 || f64::abs(norm_u - *norm_u1) < CONFIG_H_MIN
             }
             Stop::MinLambda(l1) => state.l < *l1 || f64::abs(*l1 - state.l) < CONFIG_H_MIN,
@@ -166,7 +166,7 @@ impl Stop {
         match self {
             Stop::MinCompU(_, _) => h_ini_default,
             Stop::MaxCompU(_, _) => h_ini_default,
-            Stop::MaxNormU(_) => h_ini_default,
+            Stop::MaxNormU(_, _) => h_ini_default,
             Stop::MinLambda(l1) => f64::min(h_ini_default, f64::abs(state.l - *l1)),
             Stop::MaxLambda(l1) => f64::min(h_ini_default, f64::abs(*l1 - state.l)),
             Stop::Steps(_) => h_ini_default,
@@ -178,7 +178,7 @@ impl Stop {
         match self {
             Stop::MinCompU(_, _) => h_eq_default,
             Stop::MaxCompU(_, _) => h_eq_default,
-            Stop::MaxNormU(_) => h_eq_default,
+            Stop::MaxNormU(_, _) => h_eq_default,
             Stop::MinLambda(l1) => {
                 let n = f64::ceil(f64::abs(state.l - *l1) / h_eq_default) as usize;
                 (state.l - *l1) / (n as f64)
--- russell_nonlin/tests/test_reaction_diffusion_1d.rs
+++ russell_nonlin/tests/test_reaction_diffusion_1d.rs
@@ -1,70 +1,59 @@
 use plotpy::{Curve, Plot};
 use russell_lab::{mat_approx_eq, num_jacobian, Norm, Vector};
 use russell_nonlin::{AutoStep, Config, IniDir, Method, NoArgs, Output, Solver, State, Stop, System};
-use russell_pde::FdmLaplacian2d;
+use russell_pde::FdmLaplacian1d;
 use russell_sparse::{CooMatrix, Sym};
 
 const CHECK_JACOBIAN: bool = true;
 const SAVE_FIGURE: bool = true;
 
-#[test]
-fn test_reaction_diffusion_2d() {
+// #[test]
+fn test_reaction_diffusion_1d() {
     // The nonlinear problem originates from the FDM discretization of the following equation:
     //
-    // ∂²ϕ   ∂²ϕ
-    // ——— + ——— + λ exp(ϕ/(1 + α ϕ)) = 0
-    // ∂x²   ∂y²
+    // ∂²ϕ
+    // ——— + λ exp(ϕ) = 0
+    // ∂x²
     //
-    // on the unit square (1.0 × 1.0) with homogeneous boundary conditions.
+    // on the unit segment with homogeneous boundary conditions.
     //
-    // Below, ϕ is a vector, i.e., ϕ = [ϕ₁, ϕ₂, ..., ϕₙ]ᵀ, where n is the number of grid points.
-    // The prescribed values are collected in the vector c = [c₁, c₂, ..., cₘ]ᵀ, where m is the number
-    // of boundary points. The Laplacian operator is represented by the matrix K, thus
-    // Kϕ is the discretization of the Laplacian operator applied to ϕ(x,y).
+    // Below, ϕ is a vector, i.e., ϕ = [ϕ₀, ϕ₁, ϕ₂, ..., ϕₙ₋₁]ᵀ, where n is the number of grid points.
+    // The prescribed values are collected in the vector c = [c₀, cₙ₋₁]ᵀ. The Laplacian operator is
+    // represented by the matrix K, thus Kϕ is the discretization of the Laplacian operator applied to ϕ(x,y).
     //
-    // The boundary conditions are enforced via Lagrange multipliers ψ = [ψ₁, ψ₂, ..., ψₘ]ᵀ,
-    // where m is the number of prescribed values (boundary points). The prescribed
-    // values are all zero (homogeneous boundary conditions), thus c = [0, 0, ..., 0]ᵀ.
+    // The boundary conditions are enforced via Lagrange multipliers ψ = [ψ₀, ψ₁]ᵀ. The prescribed
+    // values are both zero (homogeneous boundary conditions), thus c = [0, 0]ᵀ. The constraints matrix
+    // for the Lagrange multipliers method is E, thus Eϕ = c.
     //
     // The vector of unknowns is expressed by u = [ϕ, ψ]ᵀ and the discretized system is
-    // expressed by G = [R, S]ᵀ, where:
+    // expressed by G(u, λ) = [R(u, λ), S(u, λ)]ᵀ, where:
     //
-    // R = K ϕ + λ b + Eᵀψ = 0
-    // S = E ϕ - c = 0
+    // R(u, λ) = K ϕ + λ b + Eᵀψ = 0
+    // S(u, λ) = E ϕ - c = 0
     //
-    // with: bₘ = exp(ϕₘ/(1 + α ϕₘ))  (no sum on m)
+    // With bₘ = exp(ϕₘ), the derivatives are:
     //
-    // The derivatives are:
-    //
-    //                  ⎧ bₘ/(1 + α ϕₘ)²  if m = n
+    //                  ⎧ bₘ  if m = n
     // Bₘₙ := ∂bₘ/∂ϕₙ = ⎨
-    //                  ⎩ 0  otherwise
+    //                  ⎩ 0   otherwise
     //
-    // ∂R/∂ϕ = K + λ B    ∂R/∂ψ = Eᵀ
-    // ∂S/∂ϕ = E          ∂S/∂ψ = 0
+    //      ┌              ┐   ┌              ┐
+    //      │ ∂R/∂ϕ  ∂R/∂ψ │   │ K + λ B   Eᵀ │
+    // Gu = │              │ = │              │
+    //      │ ∂S/∂ϕ  ∂S/∂ψ │   │ E         0  │
+    //      └              ┘   └              ┘
     //
-    // ∂R/∂λ = b
-    // ∂S/∂λ = 0
-    //
-    // Thus:
-    //      ┌              ┐
-    //      │ K + λ B   Eᵀ │
-    // Gu = │              │
-    //      │ E         0  │
-    //      └              ┘
-    // And:
-    //      ┌   ┐
-    //      │ b │
-    // Gλ = │   │
-    //      │ 0 │
-    //      └   ┘
+    //      ┌       ┐   ┌   ┐
+    //      │ ∂R/∂λ │   │ b │
+    // Gλ = │       │ = │   │
+    //      │ ∂S/∂λ │   │ 0 │
+    //      └       ┘   └   ┘
 
     // constants
-    const ALPHA: f64 = 0.0;
     const NDIV: usize = 5; // number of divisions along each axis of the FDM grid
 
     // allocate the Laplacian operator
-    let mut fdm = FdmLaplacian2d::new(1.0, 1.0, 0.0, 1.0, 0.0, 1.0, NDIV, NDIV).unwrap();
+    let mut fdm = FdmLaplacian1d::new(1.0, 0.0, 1.0, NDIV, None).unwrap();
     fdm.set_homogeneous_boundary_conditions();
 
     // auxiliary variables
@@ -82,20 +71,19 @@ fn test_reaction_diffusion_2d() {
 
     // function to calculate G(u, λ)
     let calc_gg = |gg: &mut Vector, l: f64, u: &Vector, _args: &mut NoArgs| {
-        // ┌   ┐   ┌       ┐ ┌   ┐   ┌    ┐
-        // │ R │   │ K  Eᵀ │ │ ϕ │   │ λb │
-        // │   │ = │       │ │   │ + │    │
-        // │ S │   │ E  0  │ │ ψ │   │ -c │
-        // └   ┘   └       ┘ └   ┘   └    ┘
+        // ┌   ┐   ┌       ┐ ┌   ┐   ┌     ┐
+        // │ R │   │ K  Eᵀ │ │ ϕ │   │ λ b │
+        // │   │ = │       │ │   │ + │     │
+        // │ S │   │ E  0  │ │ ψ │   │ -c  │
+        // └   ┘   └       ┘ └   ┘   └     ┘
         //   G         A       u
         aa.mat_vec_mul(gg, 1.0, u).unwrap();
         // update R += λ b
         for m in 0..n_phi {
-            let dm = 1.0 + ALPHA * u[m];
-            let bm = f64::exp(u[m] / dm);
+            let bm = f64::exp(u[m]);
             gg[m] += l * bm;
         }
-        // update S -= c (not really needed since c = 0)
+        // update S -= c (not needed since c = 0)
         Ok(())
     };
 
@@ -105,10 +93,17 @@ fn test_reaction_diffusion_2d() {
         ggu.add(1.0, &aa).unwrap();
         // add λ B to the K term
         for m in 0..n_phi {
-            let dm = 1.0 + ALPHA * u[m];
-            let bm = f64::exp(u[m] / dm);
-            let bmm = bm / (dm * dm); // no sum on m
-            ggu.put(m, m, l * bmm).unwrap();
+            let bm = f64::exp(u[m]);
+            ggu.put(m, m, l * bm).unwrap();
+        }
+        if CHECK_JACOBIAN {
+            let ana = ggu.as_dense();
+            let num = num_jacobian(ndim, l, u, 1.0, &mut 0, calc_gg).unwrap();
+            if NDIV <= 3 {
+                println!("ana =\n{:.3}", ana);
+                println!("num =\n{:.3}", num);
+            }
+            mat_approx_eq(&ana, &num, 1e-6);
         }
         Ok(())
     };
@@ -116,8 +111,7 @@ fn test_reaction_diffusion_2d() {
     // function to calculate Gl = ∂G/∂λ
     let calc_ggl = |ggl: &mut Vector, _l: f64, u: &Vector, _args: &mut NoArgs| {
         for m in 0..n_phi {
-            let dm = 1.0 + ALPHA * u[m];
-            let bm = f64::exp(u[m] / dm);
+            let bm = f64::exp(u[m]);
             ggl[m] = bm;
         }
         Ok(())
@@ -136,28 +130,15 @@ fn test_reaction_diffusion_2d() {
     system.set_calc_ggl(calc_ggl);
 
     // check Jacobian matrix
-    if CHECK_JACOBIAN {
-        let mut u0 = Vector::new(ndim);
-        let l0 = 0.01;
-        u0.fill(0.01);
-        let mut ggu = CooMatrix::new(ndim, ndim, nnz, sym).unwrap();
-        calc_ggu(&mut ggu, l0, &u0, &mut 0).unwrap();
-        let ana = ggu.as_dense();
-        let num = num_jacobian(ndim, l0, &u0, 1.0, &mut 0, calc_gg).unwrap();
-        if NDIV <= 3 {
-            println!("ana =\n{:.3}", ana);
-            println!("num =\n{:.3}", num);
-        }
-        mat_approx_eq(&ana, &num, 1e-9);
-    }
 
     // configuration
     let mut config = Config::new(Method::Arclength);
     config
+        // .set_n_cont_reject_allowed(100)
         .set_verbose(true, true, true)
         .set_hide_timings(true)
         .set_debug_predictor(true)
-        .set_bordering(false);
+        .set_bordering(true);
 
     // define the solver
     let mut solver = Solver::new(config, system).unwrap();
@@ -170,12 +151,14 @@ fn test_reaction_diffusion_2d() {
     let mut state = State::new(ndim);
 
     // numerical continuation
+    let norm_type = Norm::Max;
     let status = solver
         .solve(
             &mut 0,
             &mut state,
             IniDir::Pos,
-            Stop::MaxNormU(100.0, Norm::Max),
+            // Stop::MaxNormU(200.0, Norm::Max),
+            Stop::MaxLambda(3.51),
             AutoStep::Yes,
             Some(out),
         )
@@ -187,9 +170,14 @@ fn test_reaction_diffusion_2d() {
         let mut curve = Curve::new();
         curve.draw(out.get_l_values(), out.get_norm_u_values());
         let mut plot = Plot::new();
+        let y_lbl = if norm_type == Norm::Max {
+            "‖u‖∞"
+        } else {
+            "‖u‖₂"
+        };
         plot.add(&curve)
-            .grid_and_labels("λ", "‖u‖₂")
-            .save(&format!("/tmp/russell_nonlin/test_reaction_diffusion_2d.svg"))
+            .grid_and_labels("λ", y_lbl)
+            .save(&format!("/tmp/russell_nonlin/test_reaction_diffusion_1d.svg"))
             .unwrap();
     }
 }

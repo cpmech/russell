f816ccac14291f9ebbd961f7e64cbf236c4c3d9e
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-10-14 13:15:31 +1000

    [pde/wip] Update test

--- russell_pde/tests/test_laplace2d_1_lag.rs
+++ russell_pde/tests/test_laplace2d_1_lag.rs
@@ -1,5 +1,5 @@
 use russell_lab::{array_approx_eq, Vector};
-use russell_pde::{FdmLaplacian2d, Side};
+use russell_pde::{EssentialBcs2d, FdmLaplacian2dNew, Grid2d, Side};
 use russell_sparse::{Genie, LinSolver};
 
 #[test]
@@ -18,43 +18,60 @@ fn test_laplace2d_1_lag() {
     // bottom:  ϕ(x, 0.0) = 1.0
     // top:     ϕ(x, 3.0) = 2.0
 
+    // allocate the grid
+    let grid = Grid2d::new_uniform(0.0, 3.0, 0.0, 3.0, 4, 4).unwrap();
+
+    // set the essential boundary conditions
+    let mut ebcs = EssentialBcs2d::new(&grid);
+    ebcs.set(Side::Xmin, |_, _| 1.0);
+    ebcs.set(Side::Xmax, |_, _| 2.0);
+    ebcs.set(Side::Ymin, |_, _| 1.0);
+    ebcs.set(Side::Ymax, |_, _| 2.0);
+
     // allocate the Laplacian operator
-    let mut fdm = FdmLaplacian2d::new(1.0, 1.0, 0.0, 3.0, 0.0, 3.0, 4, 4).unwrap();
+    let (kx, ky) = (1.0, 1.0);
+    let fdm = FdmLaplacian2dNew::new(&ebcs, kx, ky).unwrap();
 
-    // set essential boundary conditions
-    fdm.set_essential_boundary_condition(Side::Xmin, |_, _| 1.0);
-    fdm.set_essential_boundary_condition(Side::Xmax, |_, _| 2.0);
-    fdm.set_essential_boundary_condition(Side::Ymin, |_, _| 1.0);
-    fdm.set_essential_boundary_condition(Side::Ymax, |_, _| 2.0);
-
-    // compute the augmented coefficient matrix for the Lagrange multipliers method
+    // Solving:
     // ┌       ┐ ┌   ┐   ┌   ┐
-    // │ K  Eᵀ │ │ u │   │ f │
+    // │ M  Eᵀ │ │ a │   │ r │
     // │       │ │   │ = │   │
     // │ E  0  │ │ w │   │ ū │
     // └       ┘ └   ┘   └   ┘
-    //     A      lhs     rhs
-    let aa = fdm.augmented_coefficient_matrix(0).unwrap();
+    //     A       x       b
+    // where a = (u, p) and w are the Lagrange multipliers
+
+    // auxiliary variables
+    let nu = ebcs.num_unknown();
+    let np = ebcs.num_prescribed();
+    let na = nu + np; // dimension of a = (u, p)
+    let nw = np; // number of Lagrange multipliers
+    let nx = na + nw; // dimension of x = (u, p, w)
+
+    // assemble the coefficient matrix
+    let (aa, _) = fdm.get_aa_matrix(0, true);
 
     // allocate the left- and right-hand side vectors
-    let np = fdm.num_prescribed();
-    let dim = fdm.dim();
-    let mut lhs = Vector::new(dim + np);
-    let mut rhs = Vector::new(dim + np);
+    let mut x = Vector::new(nx);
+    let mut b = Vector::new(nx);
 
     // add the prescribed values to the right-hand side vector
-    fdm.loop_over_prescribed_values(|ip, _, value| {
-        rhs[dim + ip] = value;
+    ebcs.for_each_prescribed_node(|ip, _, _, _, u_bar| {
+        b[na + ip] = u_bar;
     });
 
     // solve the linear system
     let mut solver = LinSolver::new(Genie::Umfpack).unwrap();
     solver.actual.factorize(&aa, None).unwrap();
-    solver.actual.solve(&mut lhs, &rhs, false).unwrap();
+    solver.actual.solve(&mut x, &b, false).unwrap();
+
+    // results
+    let a = &x.as_data()[..na];
+    println!("a = {:?}", a);
 
     // check
-    let x_correct = [
+    let a_correct = [
         1.0, 1.0, 1.0, 1.0, 1.0, 1.25, 1.5, 2.0, 1.0, 1.5, 1.75, 2.0, 2.0, 2.0, 2.0, 2.0,
     ];
-    array_approx_eq(&lhs.as_data()[..dim], &x_correct, 1e-15);
+    array_approx_eq(a, &a_correct, 1e-15);
 }

63a5d8e2dcc0d98d114d4455ef36d0b0ef0f5437
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-12-25 15:18:53 +1000

    [wip] Improve test

--- russell_pde/tests/test_1d_prob04_spc.rs
+++ russell_pde/tests/test_1d_prob04_spc.rs
@@ -1,13 +1,19 @@
+use plotpy::{linspace, Curve, Plot};
 use russell_lab::approx_eq;
 use russell_pde::{ProblemSamples, Spc1d, StrError};
 
+const SAVE_FIGURE: bool = false;
+
 #[test]
 fn test_1d_prob04_spc_sps() -> Result<(), StrError> {
+    let (nn, tol) = (7, 2.26e-2);
+    // let (nn, tol) = (16, 3.0965e-9); // Trefethen, page 138, Output 33
+
     // problem setup
     let (xmin, xmax, kx, ebcs, nbcs, source, analytical) = ProblemSamples::d1_problem_04();
 
     // allocate the solver
-    let nx = 7;
+    let nx = nn + 1;
     let spc = Spc1d::new(xmin, xmax, nx, ebcs, nbcs, kx)?;
 
     // solve the problem
@@ -20,8 +26,33 @@ fn test_1d_prob04_spc_sps() -> Result<(), StrError> {
         if err > err_max {
             err_max = err;
         }
-        approx_eq(a[m], analytical(x), 8.48e-2);
+        approx_eq(a[m], analytical(x), tol);
     });
-    println!("N = {}, max(err) = {:>10.5e}", nx - 1, err_max);
+    println!("N = {}, max(err) = {:>10.5e}", nn, err_max);
+
+    // plot
+    if SAVE_FIGURE {
+        let mut curve_ana = Curve::new();
+        let mut curve_num = Curve::new();
+        curve_ana.set_label("Analytical");
+        curve_num
+            .set_label("Numerical")
+            .set_marker_style("o")
+            .set_line_style("None");
+        let xx_ana = linspace(xmin, xmax, 101);
+        let uu_ana = xx_ana.iter().map(|&x| analytical(x)).collect::<Vec<_>>();
+        let mut xx_num = vec![0.0; nx];
+        spc.for_each_coord(|i, x| {
+            xx_num[i] = x;
+        });
+        let uu_num = a.as_data();
+        curve_ana.draw(&xx_ana, &uu_ana);
+        curve_num.draw(&xx_num, &uu_num);
+        let mut plot = Plot::new();
+        plot.add(&curve_ana)
+            .add(&curve_num)
+            .grid_labels_legend("$x$", "$\\phi$")
+            .save("/tmp/russell_pde/test_1d_prob04_spc.svg")?;
+    }
     Ok(())
 }

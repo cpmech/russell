c7d1b42f58d73dac510bb38feee564f3cf32098b
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-12-28 09:27:21 +1000

    [pde] Add unit tests

--- russell_pde/src/util.rs
+++ russell_pde/src/util.rs
@@ -7,3 +7,20 @@ pub(crate) fn delta(i: usize, j: usize) -> f64 {
         0.0
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::delta;
+
+    #[test]
+    fn delta_works() {
+        assert_eq!(delta(0, 0), 1.0);
+        assert_eq!(delta(1, 1), 1.0);
+        assert_eq!(delta(10, 10), 1.0);
+        assert_eq!(delta(0, 1), 0.0);
+        assert_eq!(delta(1, 0), 0.0);
+        assert_eq!(delta(1, 2), 0.0);
+    }
+}

e15e87e61b4997c1458991e03fa340cc1f9b799d
zhuhui1990 <zhuhui.last@foxmail.com>
2026-04-23 10:52:15 +0800

    fix(solver_mumps):fix call of mumps

--- russell_sparse/src/solver_mumps.rs
+++ russell_sparse/src/solver_mumps.rs
@@ -202,13 +202,12 @@ impl LinSolTrait for SolverMUMPS {
             if coo.nnz != self.initialized_nnz {
                 return Err("subsequent factorizations must use the same matrix (nnz differs)");
             }
-            self.fortran_indices_i = vec![0; coo.nnz];
-            self.fortran_indices_j = vec![0; coo.nnz];
-            for k in 0..coo.nnz {
-                self.fortran_indices_i[k] = coo.indices_i[k] + 1;
-                self.fortran_indices_j[k] = coo.indices_j[k] + 1;
+            unsafe {
+                solver_mumps_drop(self.solver);
             }
+            self.solver = unsafe { solver_mumps_new() };
             self.initialized = false;
+            self.factorized = false;
         } else {
             if coo.nrow != coo.ncol {
                 return Err("the COO matrix must be square");

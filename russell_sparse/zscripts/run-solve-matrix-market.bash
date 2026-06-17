d8f7c1427c083c2fbf5b6b0d270d6c52d1eff411
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-06-17 21:44:38 +1000

    Improve script to run matrix market solutions

--- russell_sparse/zscripts/run-solve-matrix-market.bash
+++ russell_sparse/zscripts/run-solve-matrix-market.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 GENIE=${1:-"umfpack"}
+MATRIX=${2:-"data/matrix_market/bfwb62.mtx"}
 
 FEATURES="intel_mkl,local_sparse,cudss"
 
 cargo run --release --features "$FEATURES" --bin solve_matrix_market -- \
-    data/matrix_market/bfwb62.mtx \
-    --genie $GENIE
+    "$MATRIX" \
+    --genie "$GENIE"

e49676f2b209cbe12c14491dd4b89957c9f4abb7
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-08-23 11:25:47 +1000

    Organize functions

--- russell_tensor/src/operations_t2x.rs
+++ russell_tensor/src/operations_t2x.rs
@@ -1,35 +1,6 @@
 use super::Tensor2;
 use crate::{Rep, SQRT_2};
 
-/// Performs the transpose(Tensor2) dot Tensor2 operation: C = Aᵀ · A
-///
-/// The result C is symmetric.
-///
-/// # Output
-///
-/// * `cc` -- the resulting symmetric tensor C = Aᵀ · A; must be [Rep::Symmetric]
-///
-/// # Input
-///
-/// * `aa` -- the tensor A; must be [Rep::General]
-///
-/// # Panics
-///
-/// A panic will occur if the required [Rep] enums are incorrect.
-#[rustfmt::skip]
-pub fn t2_gen_tra_dot_gen(cc: &mut Tensor2, aa: &Tensor2) {
-    assert_eq!(aa.rep(), Rep::General);
-    assert_eq!(cc.rep(), Rep::Symmetric);
-    let a = &aa.vec;
-    let c = &mut cc.vec;
-    c[0] = 0.5 * (2.0 * a[0] * a[0] + (a[3] - a[6]) * (a[3] - a[6]) + (a[5] - a[8]) * (a[5] - a[8]));
-    c[1] = 0.5 * (2.0 * a[1] * a[1] + (a[3] + a[6]) * (a[3] + a[6]) + (a[4] - a[7]) * (a[4] - a[7]));
-    c[2] = 0.5 * (2.0 * a[2] * a[2] + (a[4] + a[7]) * (a[4] + a[7]) + (a[5] + a[8]) * (a[5] + a[8]));
-    c[3] = a[1] * (a[3] - a[6]) + a[0] * (a[3] + a[6]) + ((a[4] - a[7]) * (a[5] - a[8])) / SQRT_2;
-    c[4] = a[2] * (a[4] - a[7]) + a[1] * (a[4] + a[7]) + ((a[3] + a[6]) * (a[5] + a[8])) / SQRT_2;
-    c[5] = ((a[3] - a[6]) * (a[4] + a[7])) / SQRT_2 + a[2] * (a[5] - a[8]) + a[0] * (a[5] + a[8]);
-}
-
 /// Performs the general tensor dot general tensor operation: C = A · B
 /// 
 /// Computes:
@@ -184,6 +155,35 @@ pub fn t2_sym_dot_sym(cc: &mut Tensor2, aa: &Tensor2, bb: &Tensor2) {
     c[6] = (SQRT_2 * a[3] * (-b[0] + b[1]) + SQRT_2 * a[0] * b[3] - SQRT_2 * a[1] * b[3] + a[5] * b[4] - a[4] * b[5]) / (2.0 * SQRT_2);
     c[7] = (SQRT_2 * a[4] * (-b[1] + b[2]) - a[5] * b[3] + SQRT_2 * a[1] * b[4] - SQRT_2 * a[2] * b[4] + a[3] * b[5]) / (2.0 * SQRT_2);
     c[8] = (SQRT_2 * a[5] * (-b[0] + b[2]) - a[4] * b[3] + a[3] * b[4] + SQRT_2 * a[0] * b[5] - SQRT_2 * a[2] * b[5]) / (2.0 * SQRT_2);
+}
+
+/// Performs the transpose(Tensor2) dot Tensor2 operation: C = Aᵀ · A
+///
+/// The result C is symmetric.
+///
+/// # Output
+///
+/// * `cc` -- the resulting symmetric tensor C = Aᵀ · A; must be [Rep::Symmetric]
+///
+/// # Input
+///
+/// * `aa` -- the tensor A; must be [Rep::General]
+///
+/// # Panics
+///
+/// A panic will occur if the required [Rep] enums are incorrect.
+#[rustfmt::skip]
+pub fn t2_gen_tra_dot_self(cc: &mut Tensor2, aa: &Tensor2) {
+    assert_eq!(aa.rep(), Rep::General);
+    assert_eq!(cc.rep(), Rep::Symmetric);
+    let a = &aa.vec;
+    let c = &mut cc.vec;
+    c[0] = 0.5 * (2.0 * a[0] * a[0] + (a[3] - a[6]) * (a[3] - a[6]) + (a[5] - a[8]) * (a[5] - a[8]));
+    c[1] = 0.5 * (2.0 * a[1] * a[1] + (a[3] + a[6]) * (a[3] + a[6]) + (a[4] - a[7]) * (a[4] - a[7]));
+    c[2] = 0.5 * (2.0 * a[2] * a[2] + (a[4] + a[7]) * (a[4] + a[7]) + (a[5] + a[8]) * (a[5] + a[8]));
+    c[3] = a[1] * (a[3] - a[6]) + a[0] * (a[3] + a[6]) + ((a[4] - a[7]) * (a[5] - a[8])) / SQRT_2;
+    c[4] = a[2] * (a[4] - a[7]) + a[1] * (a[4] + a[7]) + ((a[3] + a[6]) * (a[5] + a[8])) / SQRT_2;
+    c[5] = ((a[3] - a[6]) * (a[4] + a[7])) / SQRT_2 + a[2] * (a[5] - a[8]) + a[0] * (a[5] + a[8]);
 }
 
 /// Performs the transpose(general tensor) dot general tensor operation: C = Aᵀ · B

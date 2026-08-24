63ed1cbc42ad1cc01c8c9a4bb7e31f320c07ea8c
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-08-24 13:33:31 +1000

    Add benchmark

--- russell_tensor/benchmarks/tensor_benchmark.rs
+++ russell_tensor/benchmarks/tensor_benchmark.rs
@@ -16,10 +16,13 @@
 
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use russell_tensor::{AuxDeriv2InvariantJ3, AuxDeriv2InvariantLode, Rep, Tensor2, Tensor4};
-use russell_tensor::{deriv2_invariant_jj3, deriv2_invariant_lode, t2_qsd_t2, t2_ssd};
+use russell_tensor::{deriv_squared_tensor, deriv2_invariant_jj3, deriv2_invariant_lode, t2_qsd_t2, t2_ssd};
 
 /// Fixed symmetric 3×3 matrix used to build the input tensors
 const SYMMETRIC: [[f64; 3]; 3] = [[1.0, 2.0, 3.0], [2.0, 4.0, 5.0], [3.0, 5.0, 6.0]];
+
+/// Fixed general (non-symmetric) 3×3 matrix used to build the input tensors
+const GENERAL: [[f64; 3]; 3] = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
 
 /// Benchmarks `t2_ssd` (self-sum-dyadic)
 fn bench_t2_ssd(crit: &mut Criterion) {
@@ -135,11 +138,29 @@ fn bench_deriv2_invariant_lode(crit: &mut Criterion) {
     group.finish();
 }
 
+/// Benchmarks `deriv_squared_tensor` (derivative of the squared tensor, general)
+fn bench_deriv_squared_tensor(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("deriv_squared_tensor");
+
+    group.bench_with_input(BenchmarkId::new("general", ""), &(), |b, _| {
+        let aa = Tensor2::from_std_matrix(&GENERAL, Rep::General).unwrap();
+        let mut da2_da = Tensor4::new(Rep::General);
+        let mut ii = Tensor2::new(Rep::General);
+        b.iter(|| {
+            deriv_squared_tensor(&mut da2_da, &mut ii, &aa);
+            std::hint::black_box(da2_da.get(0, 0));
+        });
+    });
+
+    group.finish();
+}
+
 criterion_group!(
     benches,
     bench_t2_ssd,
     bench_t2_qsd_t2,
     bench_deriv2_invariant_jj3,
-    bench_deriv2_invariant_lode
+    bench_deriv2_invariant_lode,
+    bench_deriv_squared_tensor
 );
 criterion_main!(benches);

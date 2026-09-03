78c3327f8e2b1cdaf49b0e90de4ec9c9711e4806
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-09-03 16:55:04 +1000

    wip: Add expected output to example

--- russell_tensor/examples/piezoelectric2.rs
+++ russell_tensor/examples/piezoelectric2.rs
@@ -4,6 +4,68 @@ use russell_tensor::{t1_dot_t3, t2_dot_t1, t3_ddot_t2, t4_ddot_t2};
 use std::env;
 use std::path::PathBuf;
 
+// Calculates:
+// σ = C:ε − E·e [Pa = (Pa)(−) − (C/m²)(V/m) = N/m²]
+// D = e:ε + p·E [C/m² = (C/m²)(−) + (F/m)(V/m) = C/m²]
+//
+// Expected output:
+//
+// Ti3TeO8 (mp-774922) : Crystal System Triclinic : Space Group P1
+// Permittivity [F/m] p =
+// ┌           ┐
+// │  1.05E-10 │
+// │  8.69E-11 │
+// │  1.09E-10 │
+// │  4.70E-11 │
+// │  3.98E-11 │
+// │  5.48E-11 │
+// └           ┘
+// Piezoelectric tensor [C/m^2] e =
+// ┌                                                             ┐
+// │ -3.94E-01 -1.31E-01 -2.00E-01  2.99E-01 -2.73E-01  1.57E-01 │
+// │  1.39E-01  1.60E-01  2.52E-01  1.89E-02 -7.70E-02  2.27E-01 │
+// │ -4.07E-02 -5.02E-03 -1.32E-02  2.14E-02  2.10E-01  3.00E-02 │
+// └                                                             ┘
+// Stiffness tensor [GPa] C =
+// ┌                                                             ┐
+// │  2.50E+01  3.00E+00 -3.00E+00  7.07E+00  1.41E+00  1.41E+00 │
+// │  3.00E+00  2.30E+01 -1.00E+00  8.49E+00  4.24E+00 -0.00E+00 │
+// │ -3.00E+00 -1.00E+00  1.70E+01  2.83E+00  4.24E+00  2.83E+00 │
+// │  7.07E+00  8.49E+00  2.83E+00  2.60E+01  6.00E+00  6.00E+00 │
+// │  1.41E+00  4.24E+00  4.24E+00  6.00E+00  4.00E+01  1.60E+01 │
+// │  1.41E+00 -0.00E+00  2.83E+00  6.00E+00  1.60E+01  4.40E+01 │
+// └                                                             ┘
+// Small strain tensor [-] eps =
+// ┌           ┐
+// │  0.00E+00 │
+// │  0.00E+00 │
+// │  1.25E-04 │
+// │  0.00E+00 │
+// │  0.00E+00 │
+// │  0.00E+00 │
+// └           ┘
+// Electric field [V/m] E =
+// ┌           ┐
+// │  0.00E+00 │
+// │  0.00E+00 │
+// │  1.00E+06 │
+// └           ┘
+// Stress [kPa] sig =
+// ┌           ┐
+// │ -3.34E+02 │
+// │ -1.20E+02 │
+// │  2.14E+03 │
+// │  3.32E+02 │
+// │  3.20E+02 │
+// │  3.24E+02 │
+// └           ┘
+// Electric displacement [C/m^2] D =
+// ┌           ┐
+// │  1.38E-05 │
+// │  5.97E-05 │
+// │  1.07E-04 │
+// └           ┘
+
 fn main() -> Result<(), StrError> {
     // Get the asset's full path (the JSON file is in the crate's data/ directory)
     let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());

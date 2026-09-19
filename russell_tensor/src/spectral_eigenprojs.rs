f7448b70980a5afc38b05d343d10b5e1a4f1d927
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-09-19 18:19:04 +1000

    wip (broken): Trying to fix eigenprojectors

--- russell_tensor/src/spectral_eigenprojs.rs
+++ russell_tensor/src/spectral_eigenprojs.rs
@@ -3,15 +3,10 @@ use crate::{EigenMethod, EigenValuesT2, Tensor2};
 use crate::{IDENTITY2, SQRT_2};
 use russell_lab::small_mat_eigen_sym_jacobi;
 
-/// Tolerance to assume repeated eigenvalues
-const TOL_REPEATED: f64 = 1e-8;
-
-/// Holds indices for permutation by looping in 0..3
-const INDICES: [usize; 5] = [0, 1, 2, 0, 1];
-
 pub struct EigenProjsT2 {
-    /// Eigenvalues struct
     eig: EigenValuesT2,
+    bs: Tensor2<6>,
+    bt: Tensor2<6>,
 }
 
 impl EigenProjsT2 {
@@ -19,6 +14,8 @@ impl EigenProjsT2 {
     pub fn new() -> Self {
         EigenProjsT2 {
             eig: EigenValuesT2::new(),
+            bs: Tensor2::new(),
+            bt: Tensor2::new(),
         }
     }
 
@@ -59,8 +56,17 @@ impl EigenProjsT2 {
         // calculate the eigenvalues (sorted in descending order)
         let spherical = self.eig.calculate_mx(ll, aa, method)?;
 
+        // calculate differences between the SORTED eigenvalues
+        let scale = aa.norm();
+        let tol = 1e3 * f64::EPSILON * f64::EPSILON * scale * scale;
+        let d01 = f64::abs(ll[0] - ll[1]);
+        let d12 = f64::abs(ll[1] - ll[2]);
+        let repeated01 = d01 < tol;
+        let repeated12 = d12 < tol;
+        let both_repeated = repeated01 && repeated12;
+
         // handle spherical case → P0=I, P1=0, P2=0
-        if spherical {
+        if spherical || both_repeated {
             for m in 0..6 {
                 projs[0].vec[m] = 0.0;
                 projs[1].vec[m] = 0.0;
@@ -72,30 +78,39 @@ impl EigenProjsT2 {
             return Ok(());
         }
 
-        // calculate differences between the SORTED eigenvalues
-        let scale = ll[0].abs().max(ll[1].abs()).max(ll[2].abs()).max(1.0);
-        let tol = TOL_REPEATED * scale;
-        let d01 = f64::abs(ll[0] - ll[1]);
-        let d12 = f64::abs(ll[1] - ll[2]);
-
         // handle all-distinct case. Use Sylvester's equation
-        if d01 >= tol && d12 >= tol {
-            // P[r] = f * (A - λ[s] I) . (A - λ[t] I)
-            for i in 0..3 {
-                let r = INDICES[i];
-                let s = INDICES[i + 1];
-                let t = INDICES[i + 2];
-                let p = -ll[s];
-                let q = -ll[t];
-                let f = 1.0 / ((ll[r] - ll[s]) * (ll[r] - ll[t]));
-                t2_plus_diag_product(projs[r].as_mut_data(), f, aa.as_data(), p, q);
+        let repeated = repeated01 || repeated12;
+        if !repeated {
+            // r s t → 0 1 2
+            let (r, s, t) = (0, 1, 2);
+            for m in 0..6 {
+                self.bs.vec[m] = (aa.vec[m] - ll[s] * IDENTITY2[m]) / (ll[r] - ll[s]);
+                self.bt.vec[m] = (aa.vec[m] - ll[t] * IDENTITY2[m]) / (ll[r] - ll[t]);
             }
+            bs_times_bt(&mut projs[0], &self.bs, &self.bt);
+            // r s t → 1 2 0
+            let (r, s, t) = (1, 2, 0);
+            for m in 0..6 {
+                self.bs.vec[m] = (aa.vec[m] - ll[s] * IDENTITY2[m]) / (ll[r] - ll[s]);
+                self.bt.vec[m] = (aa.vec[m] - ll[t] * IDENTITY2[m]) / (ll[r] - ll[t]);
+            }
+            bs_times_bt(&mut projs[1], &self.bs, &self.bt);
+            // r s t → 2 0 1 (use completeness identity instead)
+            // for m in 0..6 {
+            //     projs[2].vec[m] = IDENTITY2[m] - projs[0].vec[m] - projs[1].vec[m];
+            // }
+            let (r, s, t) = (2, 0, 1);
+            for m in 0..6 {
+                self.bs.vec[m] = (aa.vec[m] - ll[s] * IDENTITY2[m]) / (ll[r] - ll[s]);
+                self.bt.vec[m] = (aa.vec[m] - ll[t] * IDENTITY2[m]) / (ll[r] - ll[t]);
+            }
+            bs_times_bt(&mut projs[2], &self.bs, &self.bt);
             return Ok(());
         }
 
         // handle two-repeated eigenvalues
         // P_hat=(A-λ1*I)/κ, P1=I-P_hat, P_other=0 with κ=λ_hat-λ1
-        if d01 < tol {
+        if repeated01 {
             // λ0 ≈ λ1 > (λ2) → λ_hat = λ2
             let kappa = ll[2] - ll[1];
             for m in 0..6 {
@@ -116,56 +131,50 @@ impl EigenProjsT2 {
     }
 }
 
-/// Calculates alpha * (A + p I) . (A + q I)
 #[inline]
-pub(crate) fn t2_plus_diag_product(res: &mut [f64], alpha: f64, a: &[f64], p: f64, q: f64) {
-    res[0] = alpha * (2.0 * (p + a[0]) * (q + a[0]) + a[3] * a[3] + a[5] * a[5]) / 2.0;
-    res[1] = alpha * (2.0 * (p + a[1]) * (q + a[1]) + a[3] * a[3] + a[4] * a[4]) / 2.0;
-    res[2] = alpha * (2.0 * (p + a[2]) * (q + a[2]) + a[4] * a[4] + a[5] * a[5]) / 2.0;
-    res[3] = alpha * ((p + q + a[0] + a[1]) * a[3] + a[4] * a[5] / SQRT_2);
-    res[4] = alpha * ((p + q + a[1] + a[2]) * a[4] + a[3] * a[5] / SQRT_2);
-    res[5] = alpha * ((p + q + a[0] + a[2]) * a[5] + a[3] * a[4] / SQRT_2);
+#[rustfmt::skip]
+fn bs_times_bt(pp: &mut Tensor2<6>, bs_ten: &Tensor2<6>, bt_ten: &Tensor2<6>) {
+    const TWO_SQRT_2: f64 = 2.0 * SQRT_2;
+    let bs = bs_ten.as_data();
+    let bt = bt_ten.as_data();
+    pp.vec[0] = (2.0 * bs[0] * bt[0] + bs[3] * bt[3] + bs[5] * bt[5]) / 2.0;
+    pp.vec[1] = (2.0 * bs[1] * bt[1] + bs[3] * bt[3] + bs[4] * bt[4]) / 2.0;
+    pp.vec[2] = (2.0 * bs[2] * bt[2] + bs[4] * bt[4] + bs[5] * bt[5]) / 2.0;
+    pp.vec[3] = (SQRT_2 * bs[3] * (bt[0] + bt[1]) + SQRT_2 * bs[0] * bt[3] + SQRT_2 * bs[1] * bt[3] + bs[5] * bt[4] + bs[4] * bt[5]) / TWO_SQRT_2;
+    pp.vec[4] = (SQRT_2 * bs[4] * (bt[1] + bt[2]) + bs[5] * bt[3] + SQRT_2 * bs[1] * bt[4] + SQRT_2 * bs[2] * bt[4] + bs[3] * bt[5]) / TWO_SQRT_2;
+    pp.vec[5] = (SQRT_2 * bs[5] * (bt[0] + bt[2]) + bs[4] * bt[3] + bs[3] * bt[4] + SQRT_2 * bs[0] * bt[5] + SQRT_2 * bs[2] * bt[5]) / TWO_SQRT_2;
+    let res_6 = (SQRT_2 * bs[3] * (bt[1] - bt[0]) + SQRT_2 * bs[0] * bt[3] - SQRT_2 * bs[1] * bt[3] + bs[5] * bt[4] - bs[4] * bt[5]) / TWO_SQRT_2;
+    let res_7 = (SQRT_2 * bs[4] * (bt[2] - bt[1]) - bs[5] * bt[3] + SQRT_2 * bs[1] * bt[4] - SQRT_2 * bs[2] * bt[4] + bs[3] * bt[5]) / TWO_SQRT_2;
+    let res_8 = (SQRT_2 * bs[5] * (bt[2] - bt[0]) - bs[4] * bt[3] + bs[3] * bt[4] + SQRT_2 * bs[0] * bt[5] - SQRT_2 * bs[2] * bt[5]) / TWO_SQRT_2;
+    debug_assert!(f64::abs(res_6)<1e-15);
+    debug_assert!(f64::abs(res_7)<1e-15);
+    debug_assert!(f64::abs(res_8)<1e-15);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
-    use super::{EigenProjsT2, t2_plus_diag_product};
+    use super::EigenProjsT2;
     use crate::OK_EIGENPROJ_RULES;
-    use crate::testing::generate_tensors2;
+    use crate::testing::{HaberaZilian, generate_tensors2};
     use crate::{EigenMethod, SamplesTensor2, Tensor2, eigenprojector_rules};
-    use russell_lab::{approx_eq, array_approx_eq, sort3};
+    use russell_lab::{approx_eq, sort3};
 
-    #[test]
-    fn t2_plus_diag_product_works() {
-        let aa = Tensor2::<6>::from_std_matrix(&SamplesTensor2::TENSOR_U.matrix).unwrap();
-        let mut res = [0.0; 6];
-        let (alpha, p, q) = (0.5, -1.5, 2.0);
-        t2_plus_diag_product(&mut res, alpha, aa.as_data(), p, q);
-        let mut aa_plus_p_times_ii = [[0.0; 3]; 3];
-        let mut aa_plus_q_times_ii = [[0.0; 3]; 3];
-        let mut expected_mat = [[0.0; 3]; 3];
-        let ii_mat = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]];
-        let aa_mat = aa.as_std_matrix();
-        for i in 0..3 {
-            for j in 0..3 {
-                aa_plus_p_times_ii[i][j] = aa_mat[(i, j)] + p * ii_mat[i][j];
-                aa_plus_q_times_ii[i][j] = aa_mat[(i, j)] + q * ii_mat[i][j];
+    fn check_reconstruct(aa: &Tensor2<6>, ll: &[f64; 3], projs: &[Tensor2<6>], tol: f64, verbose: bool) {
+        if verbose {
+            let mut aa_rec = Tensor2::<6>::new();
+            for m in 0..6 {
+                aa_rec.vec[m] = ll[0] * projs[0].vec[m] + ll[1] * projs[1].vec[m] + ll[2] * projs[2].vec[m];
             }
+            println!(
+                "trace(A) = {}, trace(A_rec) = {}, |trace(A) - trace(A_rec)| = {:.5e}",
+                aa.trace(),
+                aa_rec.trace(),
+                f64::abs(aa.trace() - aa_rec.trace())
+            );
+            println!("A (reconstructed) =\n{}", aa_rec.as_std_matrix());
         }
-        for i in 0..3 {
-            for j in 0..3 {
-                for k in 0..3 {
-                    expected_mat[i][j] += alpha * aa_plus_p_times_ii[i][k] * aa_plus_q_times_ii[k][j];
-                }
-            }
-        }
-        let expected = Tensor2::<6>::from_std_matrix(&expected_mat).unwrap();
-        array_approx_eq(expected.as_data(), &res, 1e-14);
-    }
-
-    fn check_compose(aa: &Tensor2<6>, ll: &[f64; 3], projs: &[Tensor2<6>], tol: f64) {
         for m in 0..6 {
             let aa_m = ll[0] * projs[0].vec[m] + ll[1] * projs[1].vec[m] + ll[2] * projs[2].vec[m];
             approx_eq(aa.vec[m], aa_m, tol);
@@ -175,6 +184,7 @@ mod tests {
     #[test]
     fn calculate_mx_works_with_samples() {
         const VERBOSE: bool = true;
+        const VERB_RECONSTRUCT: bool = true;
         const TOL_VALS: f64 = 1e-13;
         const TOL_IDEM: f64 = 1e-14;
         const TOL_ORTH: f64 = 1e-14;
@@ -222,7 +232,7 @@ mod tests {
                 assert_eq!(status, OK_EIGENPROJ_RULES);
 
                 // check the spectral composition
-                check_compose(&aa, &ll, &projs, TOL_SPECTRAL);
+                check_reconstruct(&aa, &ll, &projs, TOL_SPECTRAL, VERB_RECONSTRUCT);
             }
         }
     }
@@ -230,6 +240,7 @@ mod tests {
     #[test]
     fn general_tensors2_works() {
         const VERBOSE: bool = true;
+        const VERB_RECONSTRUCT: bool = false;
         const TOL_IDEM: f64 = 1e-15;
         const TOL_ORTH: f64 = 1e-15;
         const TOL_COMP: f64 = 1e-15;
@@ -287,7 +298,84 @@ mod tests {
                 assert_eq!(status, OK_EIGENPROJ_RULES);
 
                 // check the spectral composition
-                check_compose(&aa, &ll, &projs, tol_spec);
+                check_reconstruct(&aa, &ll, &projs, tol_spec, VERB_RECONSTRUCT);
+            }
+        }
+    }
+
+    #[test]
+    fn habera_zilian_cases_work_works() {
+        const VERBOSE: bool = true;
+        const VERBOSE_PROJ: bool = false;
+        const VERB_RECONSTRUCT: bool = true;
+        const TOL_IDEM: f64 = 1e-13;
+        const TOL_ORTH: f64 = 1e-13;
+        const TOL_COMP: f64 = 1e-15;
+        const TOL_SPEC: f64 = 1e-13;
+        let mut ll = [0.0; 3];
+        let mut eig = EigenProjsT2::new();
+        let mut projs = [Tensor2::<6>::new(), Tensor2::<6>::new(), Tensor2::<6>::new()];
+        let hz = HaberaZilian::new();
+        for method in [
+            EigenMethod::AnalyticalHZ,
+            // EigenMethod::AnalyticalHA22,
+            // EigenMethod::AnalyticalHA23,
+            // EigenMethod::Iterative,
+        ] {
+            for name in hz.names {
+                for &delta in &hz.deltas {
+                    // tricky problem // if !(name == "single_lim_J3J2" && delta == 1e-12) { continue; }
+                    if !(name == "single_lim_disc_t" && delta == 1e-12) {
+                        continue;
+                    }
+                    if VERBOSE {
+                        println!("\n{}", "=".repeat(80));
+                        println!("{:?}", method);
+                    }
+                    // calculate the eigenvalues and eigenprojectors
+                    let aa = hz.tensor(name, delta);
+                    if VERBOSE {
+                        println!("{} : {:.2e} : A = \n{}", name, delta, aa.as_std_matrix());
+                    }
+                    eig.calculate_mx(&mut ll, &mut projs, &aa, method).unwrap();
+
+                    // check whether the eigenprojectors satisfy the eigenprojector rules
+                    let (mut tol_idem, mut tol_orth, mut tol_comp, mut tol_spec) =
+                        (TOL_IDEM, TOL_ORTH, TOL_COMP, TOL_SPEC);
+                    if name == "single_lim_disc_t" && delta == 1e-12 {
+                        tol_idem = 1e-3;
+                        tol_orth = 1e-3;
+                        tol_comp = 1e-3;
+                    }
+                    if name == "single_lim_disc_t" && delta == 1e-8 {
+                        tol_idem = 1e-7;
+                        tol_orth = 1e-7;
+                        tol_comp = 1e-8;
+                        tol_spec = 1e-8;
+                    }
+                    if (name == "single_lim_disc_t" || name == "single_lim_disc_n") && delta == 1e-6 {
+                        tol_idem = 1e-9;
+                        tol_orth = 1e-9;
+                    }
+                    if name == "single_lim_disc_n" && delta == 1e-8 {
+                        tol_idem = 1e-8;
+                        tol_orth = 1e-8;
+                    }
+                    if name == "single_lim_J3J2" && delta == 1e-12 {
+                        tol_idem = 1e-3;
+                        tol_orth = 1e-3;
+                    }
+                    if VERBOSE_PROJ {
+                        println!("P0 =\n{}", projs[0].as_std_matrix());
+                        println!("P1 =\n{}", projs[1].as_std_matrix());
+                        println!("P2 =\n{}", projs[2].as_std_matrix());
+                    }
+                    let status = eigenprojector_rules(&projs, tol_idem, tol_orth, tol_comp, VERBOSE);
+                    assert_eq!(status, OK_EIGENPROJ_RULES);
+
+                    // check the reconstructed matrix
+                    check_reconstruct(&aa, &ll, &projs, tol_spec, VERB_RECONSTRUCT);
+                }
             }
         }
     }
